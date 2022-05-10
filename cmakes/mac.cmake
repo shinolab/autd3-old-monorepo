bf34bde2930e6ca760a44a3a066d9d2c6d03f68b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-10 09:56:43 +0900

    add cmake

---
+++ cmakes/mac.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_C_FLAGS "-Wall -Wextra -Wno-unknown-pragmas -arch arm64 -arch x86_64")
+set(CMAKE_C_FLAGS_DEBUG "-g -O0")
+set(CMAKE_C_FLAGS_RELEASE "-O3")
+set(CMAKE_CXX_FLAGS_DEBUG ${CMAKE_C_FLAGS_DEBUG})
+set(CMAKE_CXX_FLAGS_RELEASE ${CMAKE_C_FLAGS_RELEASE})
