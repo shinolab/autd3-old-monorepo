012e1a1671629b8a71444062af66cb8dd90414d9
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-12 20:33:10 +0900

    update constructor

--- examples/remote_twincat.cpp
+++ examples/remote_twincat.cpp
@@ -15,15 +15,15 @@
 #include "runner.hpp"
 
 int main() try {
-  autd3::Geometry geometry;
-  geometry.add_device(autd3::core::Vector3::Zero(), autd3::core::Vector3::Zero());
+  autd3::Controller autd;
+
+  autd.geometry().add_device(autd3::core::Vector3::Zero(), autd3::core::Vector3::Zero());
 
   const std::string remote_ipv4_addr = "your server ip";
   const std::string remote_ams_net_id = "your server ams net id";
   const std::string local_ams_net_id = "your client ams net id";
   auto link = autd3::link::RemoteTwinCAT(2, remote_ipv4_addr, remote_ams_net_id).local_ams_net_id(local_ams_net_id).build();
-
-  autd3::Controller autd(std::move(link), std::move(geometry));
+  autd.open(std::move(link));
 
   return run(std::move(autd));
 } catch (std::exception& e) {
