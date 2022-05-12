0098ba62f11061c584df051f193e4820863a899a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-12 10:15:10 +0900

    add remote twincat

--- examples/remote_twincat.cpp
+++ examples/remote_twincat.cpp
@@ -1,4 +1,4 @@
-// File: twincat.cpp
+// File: remote_twincat.cpp
 // Project: examples
 // Created Date: 12/05/2022
 // Author: Shun Suzuki
@@ -9,7 +9,7 @@
 // Copyright (c) 2022 Hapis Lab. All rights reserved.
 //
 
-#include "autd3/link/twincat.hpp"
+#include "autd3/link/remote_twincat.hpp"
 
 #include "autd3.hpp"
 #include "runner.hpp"
@@ -18,7 +18,10 @@ int main() try {
   autd3::Geometry geometry;
   geometry.add_device(autd3::core::Vector3::Zero(), autd3::core::Vector3::Zero());
 
-  auto link = autd3::link::TwinCAT(2).build();
+  const std::string remote_ipv4_addr = "your server ip";
+  const std::string remote_ams_net_id = "your server ams net id";
+  const std::string local_ams_net_id = "your client ams net id";
+  auto link = autd3::link::RemoteTwinCAT(2, remote_ipv4_addr, remote_ams_net_id).local_ams_net_id(local_ams_net_id).build();
 
   autd3::Controller autd(std::move(link), std::move(geometry));
 
