2c430c8930ae062e5701578bb60efe7b05cf61fa
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-12 18:07:51 +0900

    add soem link

--- examples/tests/point_stm.hpp
+++ examples/tests/point_stm.hpp
@@ -3,7 +3,7 @@
 // Created Date: 11/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 11/05/2022
+// Last Modified: 12/05/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Hapis Lab. All rights reserved.
@@ -22,7 +22,7 @@ void point_stm(autd3::Controller<T>& autd) {
 
   autd3::PointSTM<T> stm;
 
-  const auto center = autd.geometry().center() + autd3::Vector3(0.0, 0.0, 150.0);
+  const autd3::Vector3 center = autd.geometry().center() + autd3::Vector3(0.0, 0.0, 150.0);
   constexpr auto point_num = 200;
   for (auto i = 0; i < point_num; i++) {
     constexpr auto radius = 30.0;
