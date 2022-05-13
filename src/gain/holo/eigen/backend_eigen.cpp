194becd1530c6896ff435623fc680f95bbc80e14
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-13 18:31:43 +0900

    declare

--- src/gain/holo/eigen/backend_eigen.cpp
+++ src/gain/holo/eigen/backend_eigen.cpp
@@ -12,17 +12,37 @@
 #include "autd3/gain/backend.hpp"
 
 namespace autd3::gain::holo {
+void EigenBackend::copy(const MatrixXc& src, MatrixXc& dst) {}
 
-void EigenBackend::make_complex(const VectorXd& r, const VectorXd& i, VectorXc& c) {
-  printf("Eigen backend\n");
-  c.real() = r;
-  c.imag() = i;
+void EigenBackend::scale(complex value, VectorXc& dst) {}
+
+complex EigenBackend::dot(const VectorXc& a, const VectorXc& b) { return a.dot(b); }
+
+void EigenBackend::mul(TRANSPOSE trans_a, TRANSPOSE trans_b, complex alpha, const MatrixXc& a, const MatrixXc& b, complex beta, MatrixXc& c) {}
+
+void EigenBackend::mul(TRANSPOSE trans_a, complex alpha, const MatrixXc& a, const VectorXc& b, complex beta, VectorXc& c) {}
+
+void EigenBackend::max_eigen_vector(const MatrixXc& src, VectorXc& dst) {}
+
+void EigenBackend::pseudo_inverse_svd(const MatrixXc& src, double alpha, const MatrixXc& u, const MatrixXc& s, const MatrixXc& vt,
+                                      const MatrixXc& buf, MatrixXc& dst) {}
+
+void EigenBackend::generate_transfer_matrix(const std::vector<core::Vector3>& foci, const std::vector<core::Vector3>& transducers, MatrixXc& dst) {}
+
+void EigenBackend::conj(const VectorXc& src, VectorXc& dst) {}
+
+void EigenBackend::create_diagonal(const VectorXc& src, MatrixXc& dst) {
+  dst.fill(ZERO);
+  dst.diagonal() = src;
 }
 
-void EigenBackend::make_complex(const MatrixXd& r, const MatrixXd& i, MatrixXc& c) {
-  printf("Eigen backend\n");
-  c.real() = r;
-  c.imag() = i;
-}
+void EigenBackend::set(size_t i, complex value, VectorXc& dst) {}
 
+void EigenBackend::set_row(VectorXc& src, size_t i, size_t begin, size_t end, MatrixXc& dst) {}
+
+void EigenBackend::set_col(VectorXc& src, size_t i, size_t begin, size_t end, MatrixXc& dst) {}
+
+void EigenBackend::get_col(const MatrixXc& src, size_t i, VectorXc& dst) {}
+
+complex EigenBackend::max_abs_element(const VectorXc& src) { return std::sqrt(src.cwiseAbs2().maxCoeff()); }
 }  // namespace autd3::gain::holo
