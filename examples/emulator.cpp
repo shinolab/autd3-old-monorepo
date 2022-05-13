bdded16c06b63b007e1594fdc7796fd628726307
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-13 14:34:37 +0900

    fix soem, add flag controll

--- examples/emulator.cpp
+++ examples/emulator.cpp
@@ -3,7 +3,7 @@
 // Created Date: 10/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 12/05/2022
+// Last Modified: 13/05/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Hapis Lab. All rights reserved.
