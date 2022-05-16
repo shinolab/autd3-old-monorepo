672a6aacbe5d976a4b49e099922f5adcf1657fce
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-16 17:11:23 +0900

    add base

---
+++ capi/base/c_api.cpp
@@ -0,0 +1,15 @@
+﻿// File: c_api.cpp
+// Project: base
+// Created Date: 16/05/2022
+// Author: Shun Suzuki
+// -----
+// Last Modified: 16/05/2022
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2022 Hapis Lab. All rights reserved.
+//
+
+#include "autd3.hpp"
+
+using T = autd3::NormalTransducer;
+#include "impl.hpp"
