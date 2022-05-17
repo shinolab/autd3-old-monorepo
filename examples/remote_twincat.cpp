6555f99fbbb72ef357f1f983e12ab361aadbfd4b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-17 12:47:09 +0900

    fix examples

--- examples/remote_twincat.cpp
+++ examples/remote_twincat.cpp
@@ -3,7 +3,7 @@
 // Created Date: 12/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 16/05/2022
+// Last Modified: 17/05/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Hapis Lab. All rights reserved.
@@ -17,7 +17,7 @@
 int main() try {
   autd3::Controller autd;
 
-  autd.geometry().add_device(autd3::core::Vector3::Zero(), autd3::core::Vector3::Zero());
+  autd.geometry().add_device(autd3::Vector3::Zero(), autd3::Vector3::Zero());
 
   const std::string remote_ipv4_addr = "your server ip";
   const std::string remote_ams_net_id = "your server ams net id";
