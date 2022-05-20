73f3bd1c1ea37e35a4565c9bba039f57148a77fe
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-20 17:29:20 +0900

    add empty test

---
+++ tests/core/core_test.cpp
@@ -0,0 +1,33 @@
+// File: core_test.cpp
+// Project: core
+// Created Date: 20/05/2022
+// Author: Shun Suzuki
+// -----
+// Last Modified: 20/05/2022
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2022 Hapis Lab. All rights reserved.
+// 
+
+
+#if _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 26439 26495 26812)
+#endif
+#include <gtest/gtest.h>
+#if _MSC_VER
+#pragma warning(pop)
+#endif
+
+#include "autd3/core/utils.hpp"
+#include "autd3/modulation/primitive.hpp"
+
+TEST(FocusTest, Test1) {
+  ASSERT_EQ(1, 1);
+  ASSERT_EQ(2, 2);
+}
+
+TEST(FocusTest, Test2) {
+  ASSERT_EQ(1, 1);
+  ASSERT_EQ(2, 2);
+}
---
+++ tests/gain/primitive/primitive_test.cpp
@@ -0,0 +1,32 @@
+// File: primitive_test.cpp
+// Project: primitive
+// Created Date: 20/05/2022
+// Author: Shun Suzuki
+// -----
+// Last Modified: 20/05/2022
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2022 Hapis Lab. All rights reserved.
+//
+
+#if _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 26439 26495 26812)
+#endif
+#include <gtest/gtest.h>
+#if _MSC_VER
+#pragma warning(pop)
+#endif
+
+#include "autd3/core/utils.hpp"
+#include "autd3/gain/primitive.hpp"
+
+TEST(FocusTest, Test1) {
+  ASSERT_EQ(1, 1);
+  ASSERT_EQ(2, 2);
+}
+
+TEST(FocusTest, Test2) {
+  ASSERT_EQ(1, 1);
+  ASSERT_EQ(2, 2);
+}
