623b25f2e5a11c1d8d9abbe334774f8fa90a3681
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-25 11:26:08 +0900

    add wrapper generator

---
+++ tools/wrapper_generator/src/traits.rs
@@ -0,0 +1,25 @@
+/*
+ * File: traits.rs
+ * Project: src
+ * Created Date: 25/05/2022
+ * Author: Shun Suzuki
+ * -----
+ * Last Modified: 25/05/2022
+ * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+ * -----
+ * Copyright (c) 2022 Hapis Lab. All rights reserved.
+ *
+ */
+
+use anyhow::Result;
+
+use std::io::Write;
+
+use capi_header_parser::parse::Function;
+
+pub trait Generator {
+    fn print_header<W: Write>(w: &mut W, bin_name: &str) -> Result<()>;
+    fn get_filename(name: &str) -> String;
+    fn register_func<W: Write>(w: &mut W, function: &Function) -> Result<()>;
+    fn print_footer<W: Write>(w: &mut W) -> Result<()>;
+}
