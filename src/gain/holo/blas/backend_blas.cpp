1bf6dd6868ef293e4ca5935951ea348a159216e7
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-05-30 13:31:21 +0900

    update header

--- src/gain/holo/blas/backend_blas.cpp
+++ src/gain/holo/blas/backend_blas.cpp
@@ -3,10 +3,10 @@
 // Created Date: 16/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 17/05/2022
+// Last Modified: 30/05/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
-// Copyright (c) 2022 Hapis Lab. All rights reserved.
+// Copyright (c) 2022 Shun Suzuki. All rights reserved.
 //
 
 #include "autd3/gain/backend_blas.hpp"
