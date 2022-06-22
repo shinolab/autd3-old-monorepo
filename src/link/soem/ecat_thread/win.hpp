8f4b44679db22c4f6caa8ae5b0852c81b734978a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-06-22 14:20:13 +0900

    rm rebundant

--- src/link/soem/ecat_thread/win.hpp
+++ src/link/soem/ecat_thread/win.hpp
@@ -84,6 +84,9 @@ void timed_wait_h(const timespec& abs_time) {
   nanosleep(sleep);
 }
 
+using wait_func = void(const timespec&);
+
+template <wait_func W>
 void ecat_run_(std::atomic<bool>* is_open, bool* is_running, int32_t expected_wkc, int64_t cycletime_ns, std::mutex& mtx,
                std::queue<driver::TxDatagram>& send_queue, IOMap& io_map, std::function<void(std::string)> on_lost) {
   const auto u_resolution = 1;
@@ -108,54 +111,7 @@ void ecat_run_(std::atomic<bool>* is_open, bool* is_running, int32_t expected_wk
   while (*is_running) {
     add_timespec(ts, cycletime_ns + toff);
 
-    timed_wait(ts);
-
-    if (ec_slave[0].state == EC_STATE_SAFE_OP) {
-      ec_slave[0].state = EC_STATE_OPERATIONAL;
-      ec_writestate(0);
-    }
-
-    if (!send_queue.empty()) {
-      std::lock_guard<std::mutex> lock(mtx);
-      io_map.copy_from(send_queue.front());
-      send_queue.pop();
-    }
-
-    ec_send_processdata();
-    if (ec_receive_processdata(EC_TIMEOUTRET) != expected_wkc && !error_handle(is_open, on_lost)) return;
-
-    ec_sync(ec_DCtime, cycletime_ns, &toff);
-  }
-
-  timeEndPeriod(1);
-  SetPriorityClass(h_process, priority);
-}
-
-void ecat_run_h(std::atomic<bool>* is_open, bool* is_running, int32_t expected_wkc, int64_t cycletime_ns, std::mutex& mtx,
-                std::queue<driver::TxDatagram>& send_queue, IOMap& io_map, std::function<void(std::string)> on_lost) {
-  const auto u_resolution = 1;
-  timeBeginPeriod(u_resolution);
-
-  auto* h_process = GetCurrentProcess();
-  const auto priority = GetPriorityClass(h_process);
-  SetPriorityClass(h_process, REALTIME_PRIORITY_CLASS);
-
-  auto ts = timespec{0, 0};
-
-  auto tp = timeval{0, 0};
-  gettimeofday_precise(&tp, nullptr);
-
-  const auto cyctime_us = cycletime_ns / 1000;
-
-  ts.tv_sec = tp.tv_sec;
-  const auto ht = ((tp.tv_usec / cyctime_us) + 1) * cyctime_us;
-  ts.tv_nsec = ht * 1000;
-
-  int64_t toff = 0;
-  while (*is_running) {
-    add_timespec(ts, cycletime_ns + toff);
-
-    timed_wait_h(ts);
+    W(ts);
 
     if (ec_slave[0].state == EC_STATE_SAFE_OP) {
       ec_slave[0].state = EC_STATE_OPERATIONAL;
@@ -182,9 +138,9 @@ void ecat_run_h(std::atomic<bool>* is_open, bool* is_running, int32_t expected_w
 void ecat_run(bool high_precision, std::atomic<bool>* is_open, bool* is_running, int32_t expected_wkc, int64_t cycletime_ns, std::mutex& mtx,
               std::queue<driver::TxDatagram>& send_queue, IOMap& io_map, std::function<void(std::string)> on_lost) {
   if (high_precision)
-    ecat_run_h(is_open, is_running, expected_wkc, cycletime_ns, mtx, send_queue, io_map, on_lost);
+    ecat_run_<timed_wait_h>(is_open, is_running, expected_wkc, cycletime_ns, mtx, send_queue, io_map, on_lost);
   else
-    ecat_run_(is_open, is_running, expected_wkc, cycletime_ns, mtx, send_queue, io_map, on_lost);
+    ecat_run_<timed_wait>(is_open, is_running, expected_wkc, cycletime_ns, mtx, send_queue, io_map, on_lost);
 }
 
 }  // namespace autd3::link
