47f15c546ab4c890aa5844716c9385de07a512fc
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-06-29 08:40:54 +0900

    lint

--- examples/tests/point_stm.hpp
+++ examples/tests/point_stm.hpp
@@ -3,7 +3,7 @@
 // Created Date: 11/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 28/06/2022
+// Last Modified: 29/06/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -15,7 +15,7 @@
 #include <autd3.hpp>
 #include <vector>
 
-void point_stm(autd3::Controller& autd) {
+inline void point_stm(autd3::Controller& autd) {
   auto config = autd3::SilencerConfig::none();
   autd.send(config);
 
--- include/autd3/core/amplitudes.hpp
+++ include/autd3/core/amplitudes.hpp
@@ -3,13 +3,15 @@
 // Created Date: 28/06/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 28/06/2022
+// Last Modified: 29/06/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
 //
 
 #pragma once
+
+#include <vector>
 
 namespace autd3::core {
 /**
