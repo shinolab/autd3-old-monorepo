0083b45b2ad35c42d1f78364b93b224ec03c2dbe
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-07-20 11:29:27 +0900

    add test program

--- examples/tests/focus.hpp
+++ examples/tests/focus.hpp
@@ -3,7 +3,7 @@
 // Created Date: 11/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 29/06/2022
+// Last Modified: 20/07/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -19,9 +19,22 @@ inline void focus_test(autd3::Controller& autd) {
 
   autd3::modulation::Sine m(150);  // 150Hz AM
 
+  // const autd3::Vector3 center = autd.geometry().center() + autd3::Vector3(0.0, 0.0, 150.0);
+
+  // autd3::gain::Focus g(center);
+
+  // autd.send(m, g);
+
   const autd3::Vector3 center = autd.geometry().center() + autd3::Vector3(0.0, 0.0, 150.0);
-
-  autd3::gain::Focus g(center);
-
-  autd.send(m, g);
+  constexpr size_t points_num = 2000;
+  constexpr auto radius = 30.0;
+  std::vector<size_t> points(points_num);
+  std::iota(points.begin(), points.end(), 0);
+  std::for_each(points.begin(), points.end(), [&](const size_t i) {
+    const auto theta = 2.0 * autd3::pi * static_cast<double>(i) / static_cast<double>(200);
+    autd3::gain::Focus g(center + autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0));
+    const auto r = autd.send(m, g);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    std::cout << std::boolalpha << r << std::endl;
+  });
 }
