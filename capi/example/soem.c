5cd600427170ab0b58c698022e36f5bfcac4f689
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-08-12 13:32:34 +0900

    fix soem capi

--- capi/example/soem.c
+++ capi/example/soem.c
@@ -4,7 +4,7 @@
  * Created Date: 16/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 04/08/2022
+ * Last Modified: 12/08/2022
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -31,22 +31,22 @@ int main() {
   AUTDAddDevice(cnt, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
   // AUTDAddDeviceQuaternion(cnt, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 
-  void* adapter_list = NULL;
-  int32_t i;
-  char name[128], desc[128];
-  const int32_t adapter_list_size = AUTDGetAdapterPointer(&adapter_list);
-  for (i = 0; i < adapter_list_size; i++) {
-    AUTDGetAdapter(adapter_list, i, desc, name);
-    printf("[%d]: %s, %s\n", i, desc, name);
-  }
-  printf("Choose number: ");
-  if (!scanf("%d", &i)) return -1;
-  (void)getchar();
-  AUTDGetAdapter(adapter_list, i, desc, name);
+  // void* adapter_list = NULL;
+  // int32_t i;
+  // char name[128], desc[128];
+  // const int32_t adapter_list_size = AUTDGetAdapterPointer(&adapter_list);
+  // for (i = 0; i < adapter_list_size; i++) {
+  //   AUTDGetAdapter(adapter_list, i, desc, name);
+  //   printf("[%d]: %s, %s\n", i, desc, name);
+  // }
+  // printf("Choose number: ");
+  // if (!scanf("%d", &i)) return -1;
+  // (void)getchar();
+  // AUTDGetAdapter(adapter_list, i, desc, name);
+  // AUTDFreeAdapterPointer(adapter_list);
   void* link = NULL;
   const int32_t device_num = AUTDNumDevices(cnt);
-  AUTDLinkSOEM(&link, name, device_num, 1, 1, false, (void*)callback, false);
-  AUTDFreeAdapterPointer(adapter_list);
+  AUTDLinkSOEM(&link, NULL, device_num, 1, 1, false, (void*)callback, false);
 
   if (!AUTDOpenController(cnt, link) || !AUTDIsOpen(cnt)) {
     const int32_t error_size = AUTDGetLastError(NULL);
