c10c840d36cdc82eaeaca1e216aa8f4c59dd9c9b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-08-14 10:17:11 +0900

    merge ecat thread

--- src/link/soem/ecat_thread/mac.hpp
+++ src/link/soem/ecat_thread/mac.hpp
@@ -3,20 +3,13 @@
 // Created Date: 12/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 30/05/2022
+// Last Modified: 14/08/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
 //
 
 #pragma once
-
-#include <queue>
-#include <string>
-
-#include "./ethercat.h"
-#include "error_handler.hpp"
-#include "utils.hpp"
 
 namespace autd3::link {
 
@@ -34,16 +27,7 @@ inline void add_timespec(timespec& ts, const int64_t addtime) {
   }
 }
 
-void timed_wait(const timespec& abs_time) {
-  auto tp = timeval{0, 0};
-  gettimeofday(&tp, nullptr);
-  const auto sleep = (abs_time.tv_sec - tp.tv_sec) * 1000000000 + (abs_time.tv_nsec - tp.tv_usec * 1000);
-  auto tc = timespec{0, sleep};
-  if (sleep > 0) nanosleep(&tc, nullptr);
-}
-
-void ecat_run_(std::atomic<bool>* is_open, bool* is_running, int32_t expected_wkc, int64_t cycletime_ns, std::mutex& mtx,
-               std::queue<driver::TxDatagram>& send_queue, IOMap& io_map, std::function<void(std::string)> on_lost) {
+timespec ecat_setup(const int64_t cycletime_ns) {
   auto ts = timespec{0, 0};
 
   auto tp = timeval{0, 0};
@@ -54,34 +38,22 @@ void ecat_run_(std::atomic<bool>* is_open, bool* is_running, int32_t expected_wk
   ts.tv_sec = tp.tv_sec;
   const auto ht = ((tp.tv_usec / cyctime_us) + 1) * cyctime_us;
   ts.tv_nsec = ht * 1000;
-
-  int64_t toff = 0;
-  while (*is_running) {
-    add_timespec(ts, cycletime_ns + toff);
-
-    timed_wait(ts);
-
-    if (ec_slave[0].state == EC_STATE_SAFE_OP) {
-      ec_slave[0].state = EC_STATE_OPERATIONAL;
-      ec_writestate(0);
-    }
-
-    if (!send_queue.empty()) {
-      std::lock_guard<std::mutex> lock(mtx);
-      io_map.copy_from(send_queue.front());
-      send_queue.pop();
-    }
-
-    ec_send_processdata();
-    if (ec_receive_processdata(EC_TIMEOUTRET) != expected_wkc && !error_handle(is_open, on_lost)) return;
-
-    ec_sync(ec_DCtime, cycletime_ns, &toff);
-  }
+  return ts;
 }
 
-void ecat_run(bool, std::atomic<bool>* is_open, bool* is_running, int32_t expected_wkc, int64_t cycletime_ns, std::mutex& mtx,
-              std::queue<driver::TxDatagram>& send_queue, IOMap& io_map, std::function<void(std::string)> on_lost) {
-  ecat_run_(is_open, is_running, expected_wkc, cycletime_ns, mtx, send_queue, io_map, on_lost);
+void timed_wait(const timespec& abs_time) {
+  auto tp = timeval{0, 0};
+  gettimeofday(&tp, nullptr);
+  const auto sleep = (abs_time.tv_sec - tp.tv_sec) * 1000000000 + (abs_time.tv_nsec - tp.tv_usec * 1000);
+  auto tc = timespec{0, sleep};
+  if (sleep > 0) nanosleep(&tc, nullptr);
 }
 
+void timed_wait_h(const timespec& abs_time) {
+  auto tp = timeval{0, 0};
+  gettimeofday(&tp, nullptr);
+  const auto sleep = (abs_time.tv_sec - tp.tv_sec) * 1000000000 + (abs_time.tv_nsec - tp.tv_usec * 1000);
+  auto tc = timespec{0, sleep};
+  if (sleep > 0) nanosleep(&tc, nullptr);
+}
 }  // namespace autd3::link
