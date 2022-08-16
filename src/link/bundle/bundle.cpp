69c92e1a25166a41af9235ea4156f7e3bab38ca2
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-08-16 09:32:20 +0900

    add bundle link

---
+++ src/link/bundle/bundle.cpp
@@ -0,0 +1,59 @@
+﻿// File: bundle.cpp
+// Project: bundle
+// Created Date: 16/08/2022
+// Author: Shun Suzuki
+// -----
+// Last Modified: 16/08/2022
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2022 Shun Suzuki. All rights reserved.
+//
+
+#include "autd3/link/bundle.hpp"
+
+#include "autd3/core/link.hpp"
+
+namespace autd3::link {
+
+class BundleImpl final : public core::Link {
+ public:
+  explicit BundleImpl(std::vector<core::LinkPtr> links) : Link(), _is_open(false), _links(std::move(links)) {}
+  ~BundleImpl() override = default;
+  BundleImpl(const BundleImpl& v) noexcept = delete;
+  BundleImpl& operator=(const BundleImpl& obj) = delete;
+  BundleImpl(BundleImpl&& obj) = delete;
+  BundleImpl& operator=(BundleImpl&& obj) = delete;
+
+  void open(const core::Geometry& geometry) override {
+    if (is_open()) return;
+    for (const auto& link : _links) link->open(geometry);
+    _is_open = true;
+  }
+
+  void close() override { _is_open = false; }
+
+  bool send(const driver::TxDatagram& tx) override {
+    bool result = true;
+    for (const auto& link : _links) result &= link->send(tx);
+    return result;
+  }
+  bool receive(driver::RxDatagram& rx) override {
+    bool result = true;
+    for (size_t i = 1; i < _links.size(); i++) result &= _links[i]->receive(rx);
+    result &= _links[0]->receive(rx);
+    return result;
+  }
+
+  bool is_open() override { return _is_open; }
+
+ private:
+  bool _is_open;
+  std::vector<core::LinkPtr> _links;
+};
+
+core::LinkPtr Bundle::build() {
+  core::LinkPtr link = std::make_unique<BundleImpl>(std::move(_links));
+  return link;
+}
+
+}  // namespace autd3::link
