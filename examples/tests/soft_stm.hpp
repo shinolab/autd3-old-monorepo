c5a4a16bd8eb26ca88038ff977eaee62a90af641
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-09-07 15:23:34 +0900

    fix arm build

--- examples/tests/soft_stm.hpp
+++ examples/tests/soft_stm.hpp
@@ -31,8 +31,7 @@ inline void soft_stm(autd3::Controller& autd) {
   std::iota(points.begin(), points.end(), 0);
   std::for_each(points.begin(), points.end(), [&](const size_t i) {
     const auto theta = 2.0 * autd3::pi * static_cast<double>(i) / static_cast<double>(points_num);
-    autd3::gain::Focus g(center + autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0.0));
-    stm.add(g);
+    stm.add(autd3::gain::Focus(center + autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0.0)));
   });
 
   const auto actual_freq = stm.set_frequency(1);
