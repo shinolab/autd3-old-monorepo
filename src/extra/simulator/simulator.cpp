3fee1a3fcf5abb4a3bd70032803caf6c8a1fff23
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-02 11:58:16 +0900

    add sound_sources

--- src/extra/simulator/simulator.cpp
+++ src/extra/simulator/simulator.cpp
@@ -3,16 +3,17 @@
 // Created Date: 30/09/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 30/09/2022
+// Last Modified: 02/10/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
-// 
+//
 
 #include "autd3/extra/simulator/simulator.hpp"
 
-namespace autd3::extra::simulator
-{
+#include "sound_sources.hpp"
+
+namespace autd3::extra::simulator {
 Simulator Simulator::start() { return std::move(*this); }
 void Simulator::exit() {}
-}
+}  // namespace autd3::extra::simulator
