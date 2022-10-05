32e5d5c6d0c6f421ae9a5de1ada3e33c991232a3
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-05 11:48:31 +0900

    view trans phase

--- examples/simulator.cpp
+++ examples/simulator.cpp
@@ -3,7 +3,7 @@
 // Created Date: 30/09/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 03/10/2022
+// Last Modified: 05/10/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -25,19 +25,10 @@ int main() try {
   // for (auto& dev : autd.geometry())
   //   for (auto& tr : dev) tr.set_frequency(70e3);
 
-  auto link = autd3::link::Simulator().port(50632).build();
+  auto link = autd3::link::Simulator().vsync(true).shader(AUTD3_SIMULATOR_SHADER_PATH).texture(AUTD3_SIMULATOR_TEXTURE_PATH).build();
   autd.open(std::move(link));
 
-  autd3::extra::simulator::Simulator simulator =
-      autd3::extra::simulator::Simulator().vsync(true).shader(AUTD3_SIMULATOR_SHADER_PATH).texture(AUTD3_SIMULATOR_TEXTURE_PATH);
-
-  bool sim_run = false;
-  auto th = std::thread([&sim_run, &simulator]() { simulator.start(&sim_run); });
-
   run(std::move(autd));
-
-  sim_run = false;
-  if (th.joinable()) th.join();
 
   return 0;
 } catch (std::exception& e) {
--- include/autd3/link/simulator.hpp
+++ include/autd3/link/simulator.hpp
@@ -3,7 +3,7 @@
 // Created Date: 16/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 30/09/2022
+// Last Modified: 05/10/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -12,34 +12,87 @@
 #pragma once
 
 #include <memory>
+#include <string>
+#include <utility>
 
 #include "autd3/core/link.hpp"
 
 namespace autd3::link {
 
 /**
- * \brief link for autd3::extra::simulator::Simulator
+ * \brief link for Simulator
  */
 class Simulator {
  public:
   /**
    * @brief Constructor
    */
-  Simulator() : _port(50632) {}
+  Simulator() noexcept = default;
   ~Simulator() = default;
   Simulator(const Simulator& v) noexcept = delete;
   Simulator& operator=(const Simulator& obj) = delete;
-  Simulator(Simulator&& obj) = delete;
-  Simulator& operator=(Simulator&& obj) = delete;
+  Simulator(Simulator&& obj) = default;
+  Simulator& operator=(Simulator&& obj) = default;
 
-  Simulator& port(const uint16_t port) {
-    _port = port;
+  /**
+   * @brief Set window size
+   */
+  Simulator& window_size(const int32_t width, const int32_t height) {
+    _width = width;
+    _height = height;
     return *this;
   }
-  core::LinkPtr build();
+
+  /**
+   * @brief Set vsync
+   */
+  Simulator& vsync(const bool vsync) {
+    _vsync = vsync;
+    return *this;
+  }
+
+  /**
+   * @brief Set shader path
+   */
+  Simulator& shader(std::string shader) {
+    _shader = std::move(shader);
+    return *this;
+  }
+
+  /**
+   * @brief Set texture path
+   */
+  Simulator& texture(std::string texture) {
+    _texture = std::move(texture);
+    return *this;
+  }
+
+  /**
+   * @brief Set font path
+   */
+  Simulator& font(std::string font) {
+    _font = std::move(font);
+    return *this;
+  }
+
+  /**
+   * @brief Set GPU index
+   */
+  Simulator& gpu_idx(const size_t idx) {
+    _gpu_idx = idx;
+    return *this;
+  }
+
+  [[nodiscard]] core::LinkPtr build() const;
 
  private:
-  uint16_t _port;
+  int32_t _width{800};
+  int32_t _height{600};
+  bool _vsync{true};
+  std::string _shader;
+  std::string _texture;
+  std::string _font;
+  size_t _gpu_idx{0};
 };
 
 }  // namespace autd3::link
