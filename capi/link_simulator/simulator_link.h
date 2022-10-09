3d5e36ebd504c61b6e0dcfd365c59c6ae41b823e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-09 13:21:16 +0900

    update doc

--- capi/link_simulator/simulator_link.h
+++ capi/link_simulator/simulator_link.h
@@ -3,7 +3,7 @@
 // Created Date: 16/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 05/10/2022
+// Last Modified: 09/10/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -16,7 +16,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-EXPORT_AUTD void AUTDLinkSimulator(OUT void** out);
+EXPORT_AUTD void AUTDLinkSimulator(OUT void** out, IN uint16_t port, IN const char* ip_addr);
 #ifdef __cplusplus
 }
 #endif
