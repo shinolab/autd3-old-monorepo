01461253649ce4ae0a7ac09bc66711cde383e8af
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-09 15:29:48 +0900

    fix

--- cs/src/NativeMethods/LinkSOEM.cs
+++ cs/src/NativeMethods/LinkSOEM.cs
@@ -1,7 +1,7 @@
 // This file was automatically generated from header file
 using System;
 using System.Runtime.InteropServices;
-
+            
 namespace AUTD3Sharp.NativeMethods
 {
     internal static class LinkSOEM
@@ -11,6 +11,6 @@ namespace AUTD3Sharp.NativeMethods
         [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)] public static extern int AUTDGetAdapterPointer(out IntPtr @out);
         [DllImport(DLL, CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true, CallingConvention = CallingConvention.Cdecl)] public static extern void AUTDGetAdapter(IntPtr pAdapter, int index, System.Text.StringBuilder? desc, System.Text.StringBuilder? name);
         [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)] public static extern void AUTDFreeAdapterPointer(IntPtr pAdapter);
-        [DllImport(DLL, CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true, CallingConvention = CallingConvention.Cdecl)] public static extern void AUTDLinkSOEM(out IntPtr @out, string? ifname, ushort sync0Cycle, ushort sendCycle, [MarshalAs(UnmanagedType.U1)] bool freerun, IntPtr onLost, [MarshalAs(UnmanagedType.U1)] bool highPrecision);
+        [DllImport(DLL, CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true, CallingConvention = CallingConvention.Cdecl)] public static extern void AUTDLinkSOEM(out IntPtr @out, string ifname, ushort sync0Cycle, ushort sendCycle, [MarshalAs(UnmanagedType.U1)] bool freerun, IntPtr onLost, [MarshalAs(UnmanagedType.U1)] bool highPrecision);
     }
 }
