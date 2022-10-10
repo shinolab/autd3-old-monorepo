9a16fefde717a34003d90faabbf51eb732a3a085
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-10 14:10:58 +0900

    add AUTDServer source

---
+++ dist/AUTDServer/src/AUTDServer/SyncMode.cs
@@ -0,0 +1,21 @@
+/*
+ * File: SyncMode.cs
+ * Project: AUTDServer
+ * Created Date: 05/08/2022
+ * Author: Shun Suzuki
+ * -----
+ * Last Modified: 05/08/2022
+ * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+ * -----
+ * Copyright (c) 2022 Shun Suzuki. All rights reserved.
+ * 
+ */
+
+namespace AUTDServer
+{
+    internal enum SyncMode
+    {
+        FreeRun,
+        DC
+    }
+}
---
+++ dist/AUTDServer/src/README.md
@@ -0,0 +1,7 @@
+# AUTDServer
+
+TwinCAT AUTD Server for [autd3](https://github.com/shinolab/autd3).
+
+# Author
+
+Shun Suzuki, 2022
