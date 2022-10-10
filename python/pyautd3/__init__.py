64661e78c77e80ab4c6bfe1156702188afb0b1ea
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-10 15:35:06 +0900

    fix version

--- python/pyautd3/__init__.py
+++ python/pyautd3/__init__.py
@@ -4,7 +4,7 @@ Project: pyautd3
 Created Date: 24/05/2021
 Author: Shun Suzuki
 -----
-Last Modified: 09/10/2022
+Last Modified: 10/10/2022
 Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 -----
 Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -83,7 +83,7 @@ __all__ = [
     'DEVICE_HEIGHT'
 ]
 
-__version__ = '2.4.2'
+__version__ = '2.4.1'
 
 _PLATFORM = platform.system()
 _TARGET_OS = ''
