bc34816a7f8f23390294357482d8dd833e22d411
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-12 10:41:32 +0900

    refactor

--- dist/GUI/AUTD3-GUI-Controller/Models/AngleUnitConverter.cs
+++ dist/GUI/AUTD3-GUI-Controller/Models/AngleUnitConverter.cs
@@ -4,36 +4,27 @@
  * Created Date: 11/10/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 11/10/2022
+ * Last Modified: 12/10/2022
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
  * 
  */
 
-namespace AUTD3_GUI_Controller.Models
+namespace AUTD3_GUI_Controller.Models;
+
+internal class AngleUnitConverter
 {
-    internal class AngleUnitConverter
+    public AngleUnit AngleUnit { get; set; } = AngleUnit.Degree;
+
+    public static AngleUnitConverter Instance { get; } = new();
+
+    private AngleUnitConverter()
     {
-        private static AngleUnitConverter instance = new AngleUnitConverter();
+    }
 
-        public AngleUnit AngleUnit { get; set; } = AngleUnit.Degree;
-
-        public static AngleUnitConverter Instance
-        {
-            get
-            {
-                return instance;
-            }
-        }
-
-        private AngleUnitConverter()
-        {
-        }
-
-        public double ToRadian(double v)
-        {
-            return AngleUnit == AngleUnit.Radian ? v : v / 180.0 * Math.PI;
-        }
+    public double ToRadian(double v)
+    {
+        return AngleUnit == AngleUnit.Radian ? v : v / 180.0 * Math.PI;
     }
 }
