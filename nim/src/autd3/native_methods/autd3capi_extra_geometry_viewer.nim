2c0c9e0f241ff2d34b5df49f4b7978d7dd0812c0
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-18 14:21:27 +0900

    fix capi

---
+++ nim/src/autd3/native_methods/autd3capi_extra_geometry_viewer.nim
@@ -0,0 +1,15 @@
+##  This file was automatically generated from header file
+
+{.deadCodeElim: on.}
+when defined(windows):
+  const
+    dll* = "bin/autd3capi-extra-geometry-viewer.dll"
+elif defined(macosx):
+  const
+    dll* = "bin/libautd3capi-extra-geometry-viewer.dylib"
+else:
+  const
+    dll* = "bin/libautd3capi-extra-geometry-viewer.so"
+proc AUTDExtraGeometryViewer*(cnt: pointer; width: int32; height: int32; vsync: bool;
+                             gpu_idx: int32) {.cdecl,
+    importc: "AUTDExtraGeometryViewer", dynlib: dll.}
