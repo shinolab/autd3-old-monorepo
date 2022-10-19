69d7da38f9b962660d80401aae1c6859075bc7c3
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-20 04:33:38 +0900

    add doc samples

---
+++ doc/book/samples/nim/sample.nim
@@ -0,0 +1,46 @@
+import strutils
+
+import autd3
+import autd3/soem
+
+proc onLost(msg: cstring) =
+    echo msg
+    quit(-1)
+
+when isMainModule:
+    try:
+        var cnt = initController()
+        cnt.addDevice([0.0, 0.0, 0.0], [0.0, 0.0, 0.0])
+
+        var soem = initSOEM()
+        let link = soem.highPrecision(true).onLost(onLost).build()
+        if not cnt.open(link):
+            echo Controller.lastError
+            quit(-1)
+
+        cnt.checkTrials = 50
+
+        cnt.clear()
+        cnt.synchronize()
+
+        let firmList = cnt.firmwareInfoList()
+        for firm in firmList:
+            echo firm
+
+        let config = initSilencerConfig()
+        cnt.send(config)
+
+        let f = initFocus([90.0, 80.0, 150.0])
+        let m = initSine(150)
+
+        cnt.send(m, f)
+
+        discard stdin.readLine
+
+        cnt.close()
+
+    except:
+        let
+            e = getCurrentException()
+            msg = getCurrentExceptionMsg()
+        echo "Got exception ", repr(e), " with message ", msg
