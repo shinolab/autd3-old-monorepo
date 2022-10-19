f4a31df03c44c9cfe36fae111dc5927998460e45
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-10-20 04:33:38 +0900

    add doc samples

---
+++ doc/book/samples/julia/sample.jl
@@ -0,0 +1,37 @@
+using Printf
+
+using StaticArrays
+
+using AUTD3
+
+function on_lost(msg::Cstring)
+    println(msg)
+    exit(-1)
+end
+
+const cnt = Controller()
+cnt.add_device(SVector(0.0, 0.0, 0.0), SVector(0.0, 0.0, 0.0))
+
+const link = SOEM(on_lost=on_lost, high_precision=true)
+
+if !cnt.open(link)
+    println(get_last_error())
+    exit(-1)
+end
+
+cnt.clear()
+cnt.synchronize()
+
+firm_info_list = cnt.firmware_info_list()
+for firm_info in firm_info_list
+    @printf("%s\n", firm_info)
+end
+
+const g = Focus(SVector(90.0, 80.0, 150.0))
+const m = Sine(150)
+
+cnt.send(m, g)
+
+readline()
+
+cnt.close()
