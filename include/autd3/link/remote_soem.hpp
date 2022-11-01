5a44562c5a746ccfcd7df6285f58ad8a6d556488
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-01 12:06:55 +0900

    start

---
+++ include/autd3/link/remote_soem.hpp
@@ -0,0 +1,12 @@
+// File: remote_soem.hpp
+// Project: link
+// Created Date: 26/10/2022
+// Author: Shun Suzuki
+// -----
+// Last Modified: 26/10/2022
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2022 Shun Suzuki. All rights reserved.
+//
+
+#pragma once
