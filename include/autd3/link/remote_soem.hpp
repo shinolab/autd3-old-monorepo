0e24762d9af99c5042260d99ec3002c5de8c51e1
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-02 10:55:00 +0900

    add remote soem link

--- include/autd3/link/remote_soem.hpp
+++ include/autd3/link/remote_soem.hpp
@@ -3,10 +3,53 @@
 // Created Date: 26/10/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 26/10/2022
+// Last Modified: 02/11/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
 //
 
 #pragma once
+
+#include <memory>
+#include <string>
+
+#include "autd3/core/link.hpp"
+
+namespace autd3::link {
+
+/**
+ * @brief Link using SOEMAUTDServer
+ */
+class RemoteSOEM {
+ public:
+  /**
+   * @brief Create RemoteSOEM
+   */
+  core::LinkPtr build();
+
+  RemoteSOEM& ip(const std::string& ip) {
+    _ip = ip;
+    return *this;
+  }
+
+  RemoteSOEM& port(const uint16_t& port) {
+    _port = port;
+    return *this;
+  }
+
+  /**
+   * @brief Constructor
+   */
+  RemoteSOEM() : _port(50632) {}
+  ~RemoteSOEM() = default;
+  RemoteSOEM(const RemoteSOEM& v) noexcept = delete;
+  RemoteSOEM& operator=(const RemoteSOEM& obj) = delete;
+  RemoteSOEM(RemoteSOEM&& obj) = delete;
+  RemoteSOEM& operator=(RemoteSOEM&& obj) = delete;
+
+ private:
+  std::string _ip;
+  uint16_t _port;
+};
+}  // namespace autd3::link
--- src/link/soem/soem_handler.hpp
+++ src/link/soem/soem_handler.hpp
@@ -3,7 +3,7 @@
 // Created Date: 16/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 01/11/2022
+// Last Modified: 02/11/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -44,13 +44,11 @@
 #include <utility>
 #include <vector>
 
-#include "./ethercat.h"
 #include "autd3/driver/cpu/datagram.hpp"
 #include "autd3/driver/cpu/ec_config.hpp"
 #include "autd3/link/soem.hpp"
 #include "ecat_thread.hpp"
 #include "error_handler.hpp"
-#include "iomap.hpp"
 
 namespace autd3::link {
 
