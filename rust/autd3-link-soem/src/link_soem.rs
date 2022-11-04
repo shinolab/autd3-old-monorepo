6bfb8ee0911436e50bc746fc8dcff1ce93c10261
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-04 14:12:31 +0900

    fix link soem

--- rust/autd3-link-soem/src/link_soem.rs
+++ rust/autd3-link-soem/src/link_soem.rs
@@ -13,11 +13,10 @@
 
 use std::{
     sync::{
-        atomic::{AtomicBool, Ordering},
+        atomic::{AtomicBool, AtomicI32, Ordering},
         Arc, Mutex,
     },
-    thread::{self, JoinHandle},
-    time::Duration,
+    thread::JoinHandle,
     usize,
 };
 
@@ -44,13 +43,14 @@ const SEND_BUF_SIZE: usize = 32;
 
 pub struct SOEM<F: Fn(&str) + Send> {
     ecatth_handle: Option<JoinHandle<()>>,
+    ecat_check_th: Option<JoinHandle<()>>,
     error_handle: Option<F>,
-    is_open: bool,
     config: Config,
     sender: Option<Sender<TxDatagram>>,
-    thread_running: Arc<AtomicBool>,
+    is_open: Arc<AtomicBool>,
     ec_sync0_cycle_time_ns: u32,
     ec_send_cycle_time_ns: u32,
+    io_map: Arc<Mutex<IOMap>>,
 }
 
 impl<F: Fn(&str) + Send> SOEM<F> {
@@ -59,13 +59,14 @@ impl<F: Fn(&str) + Send> SOEM<F> {
         let ec_sync0_cycle_time_ns = EC_CYCLE_TIME_BASE_NANO_SEC * config.sync0_cycle as u32;
         Self {
             ecatth_handle: None,
+            ecat_check_th: None,
             error_handle: Some(error_handle),
-            is_open: false,
             sender: None,
-            thread_running: Arc::new(AtomicBool::new(false)),
+            is_open: Arc::new(AtomicBool::new(false)),
             config,
             ec_sync0_cycle_time_ns,
             ec_send_cycle_time_ns,
+            io_map: Arc::new(Mutex::new(IOMap::new(0))),
         }
     }
 }
@@ -112,7 +113,7 @@ impl<F: 'static + Fn(&str) + Send> Link for SOEM<F> {
     fn open<T: Transducer>(&mut self, geometry: &Geometry<T>) -> anyhow::Result<()> {
         let dev_num = geometry.num_devices() as u16;
 
-        let mut io_map = Box::new(IOMap::new(dev_num as _));
+        self.io_map = Arc::new(Mutex::new(IOMap::new(dev_num as _)));
 
         let (tx_sender, tx_receiver) = bounded(SEND_BUF_SIZE);
 
@@ -148,7 +149,7 @@ impl<F: 'static + Fn(&str) + Send> Link for SOEM<F> {
 
             ec_configdc();
 
-            ec_config_map(io_map.data() as *mut c_void);
+            ec_config_map(self.io_map.lock().unwrap().data() as *mut c_void);
 
             ec_statecheck(
                 0,
@@ -165,28 +166,30 @@ impl<F: 'static + Fn(&str) + Send> Link for SOEM<F> {
 
             ec_writestate(0);
 
+            self.is_open.store(true, Ordering::Release);
             let expected_wkc = (ec_group[0].outputsWKC * 2 + ec_group[0].inputsWKC) as i32;
             let cycletime = self.ec_send_cycle_time_ns as i64;
-            let error_handle = self.error_handle.take();
-            let thread_running = self.thread_running.clone();
+            let is_open = self.is_open.clone();
             let is_high_precision = self.config.high_precision_timer;
+            let wkc = Arc::new(AtomicI32::new(0));
+            let wkc_clone = wkc.clone();
+            let io_map = self.io_map.clone();
             self.ecatth_handle = Some(std::thread::spawn(move || {
-                let error_handler = EcatErrorHandler { error_handle };
                 if is_high_precision {
                     let mut callback = EcatThreadHandler::<HighPrecisionWaiter>::new(
                         io_map,
-                        thread_running,
+                        is_open,
+                        wkc_clone,
                         tx_receiver,
-                        expected_wkc,
                         cycletime,
                     );
                     callback.run();
                 } else {
                     let mut callback = EcatThreadHandler::<NormalWaiter>::new(
                         io_map,
-                        thread_running,
+                        is_open,
+                        wkc_clone,
                         tx_receiver,
-                        expected_wkc,
                         cycletime,
                     );
                     callback.run();
@@ -202,6 +205,10 @@ impl<F: 'static + Fn(&str) + Send> Link for SOEM<F> {
             );
 
             if ec_slave[0].state != ec_state_EC_STATE_OPERATIONAL as u16 {
+                self.is_open.store(false, Ordering::Release);
+                if let Some(timer) = self.ecatth_handle.take() {
+                    let _ = timer.join();
+                }
                 return Err(SOEMError::NotResponding.into());
             }
 
@@ -210,16 +217,31 @@ impl<F: 'static + Fn(&str) + Send> Link for SOEM<F> {
                     dc_config(&mut ecx_context as *mut _, i);
                 });
             }
+
+            let is_open = self.is_open.clone();
+            let wkc_clone = wkc.clone();
+            let error_handle = self.error_handle.take();
+            let state_check_interval = self.config.check_interval;
+            self.ecat_check_th = Some(std::thread::spawn(move || {
+                let error_handler = EcatErrorHandler { error_handle };
+                while is_open.load(Ordering::Acquire) {
+                    if wkc_clone.load(Ordering::Acquire) < expected_wkc
+                        || ec_group[0].docheckstate != 0
+                    {
+                        error_handler.handle();
+                    }
+                    std::thread::sleep(state_check_interval);
+                }
+            }));
         }
 
-        self.is_open = true;
         self.sender = Some(tx_sender);
 
         Ok(())
     }
 
     fn close(&mut self) -> Result<()> {
-        if !self.is_open {
+        if self.is_open() {
             return Ok(());
         }
 
@@ -229,16 +251,11 @@ impl<F: 'static + Fn(&str) + Send> Link for SOEM<F> {
             ));
         }
 
-        self.is_open = false;
-
-        std::thread::sleep(std::time::Duration::from_millis(200));
-
-        self.thread_running.store(false, Ordering::Release);
+        self.is_open.store(false, Ordering::Release);
         if let Some(timer) = self.ecatth_handle.take() {
             let _ = timer.join();
         }
-
-        if let Some(th) = self.recv_thread.take() {
+        if let Some(th) = self.ecat_check_th.take() {
             let _ = th.join();
         }
 
@@ -263,24 +280,27 @@ impl<F: 'static + Fn(&str) + Send> Link for SOEM<F> {
     }
 
     fn send(&mut self, tx: &TxDatagram) -> Result<bool> {
-        let buf = tx.clone();
+        if !self.is_open() {
+            return Err(AUTDInternalError::LinkClosed.into());
+        }
 
+        let buf = tx.clone();
         self.sender.as_mut().unwrap().send(buf)?;
 
         Ok(true)
     }
 
     fn receive(&mut self, rx: &mut RxDatagram) -> Result<bool> {
-        if !self.is_open {
+        if !self.is_open() {
             return Err(AUTDInternalError::LinkClosed.into());
         }
 
-        rx.copy_from(&self.rx.lock().unwrap());
+        rx.copy_from(&self.io_map.lock().unwrap().input());
 
         Ok(true)
     }
 
     fn is_open(&self) -> bool {
-        self.is_open
+        self.is_open.load(Ordering::Acquire)
     }
 }
