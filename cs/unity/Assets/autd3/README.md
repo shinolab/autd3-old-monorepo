10232fae3478b0ae34efb923aebf530cd136431b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-10 09:12:12 +0900

    Unity Package Manager

---
+++ cs/unity/Assets/autd3/README.md
@@ -0,0 +1,9 @@
+# autd3-unity
+
+This package is [autd3](https://github.com/shinolab/autd3) wrapper library for Unity.
+
+See [autd3](https://github.com/shinolab/autd3) repository for more details.
+
+# Author
+
+Shun Suzuki, 2022
--- cs/unity/Assets/autd3/Samples/Simple/SimpleAUTDController.cs
+++ cs/unity/Assets/autd3/Samples/Simple/SimpleAUTDController.cs
@@ -4,7 +4,7 @@
  * Created Date: 10/10/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 08/11/2022
+ * Last Modified: 10/11/2022
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -20,18 +20,18 @@ using UnityEngine;
 
 public class SimpleAUTDController : MonoBehaviour
 {
-    readonly Controller _autd = new();
+    private readonly Controller _autd = new();
     public GameObject? Target = null;
 
     private Vector3 _oldPosition;
 
-    private static bool isPlaying = true;
+    private static bool _isPlaying = true;
 
     private static void OnLost(string msg)
     {
         Debug.LogError(msg);
 #if UNITY_EDITOR
-        isPlaying = false;  // UnityEditor.EditorApplication.isPlaying can be set only from the main thread
+        _isPlaying = false;  // UnityEditor.EditorApplication.isPlaying can be set only from the main thread
 #elif UNITY_STANDALONE
         UnityEngine.Application.Quit();
 #endif
@@ -46,21 +46,21 @@ public class SimpleAUTDController : MonoBehaviour
     {
     }
 
-    readonly AUTD3Sharp.Link.SOEM.OnLostCallbackDelegate _onLost = new(OnLost);
-    readonly AUTD3Sharp.Link.SOEM.OnLogOutputCallback _output = new(LogOutput);
-    readonly AUTD3Sharp.Link.SOEM.OnLogFlushCallback _flush = new(LogFlush);
+    private readonly AUTD3Sharp.Link.SOEM.OnLostCallbackDelegate _onLost = new(OnLost);
+    private readonly AUTD3Sharp.Link.SOEM.OnLogOutputCallback _output = new(LogOutput);
+    private readonly AUTD3Sharp.Link.SOEM.OnLogFlushCallback _flush = new(LogFlush);
 
-    void Awake()
+    private void Awake()
     {
         AUTD3Sharp.Link.SOEM.SetLogFunc(_output, _flush);
 
         _autd.Geometry.AddDevice(gameObject.transform.position, gameObject.transform.rotation);
 
         var link = new AUTD3Sharp.Link.SOEM()
-                            .HighPrecision(true)
-                            // .FreeRun(true)
-                            .OnLost(_onLost)
-                            .Build();
+           .HighPrecision(true)
+           // .FreeRun(true)
+           .OnLost(_onLost)
+           .Build();
 
         if (!_autd.Open(link))
         {
@@ -80,28 +80,24 @@ public class SimpleAUTDController : MonoBehaviour
 
         _autd.Send(new AUTD3Sharp.Modulation.Sine(150)); // 150 Hz
 
-        if (Target != null)
-        {
-            _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position, 1.0));
-            _oldPosition = Target.transform.position;
-        }
+        if (Target == null) return;
+        _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position, 1.0));
+        _oldPosition = Target.transform.position;
     }
 
-    void Update()
+    private void Update()
     {
 #if UNITY_EDITOR
-        if (!isPlaying)
+        if (!_isPlaying)
         {
             UnityEditor.EditorApplication.isPlaying = false;
             return;
         }
 #endif
 
-        if (Target != null && Target.transform.position != _oldPosition)
-        {
-            _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position, 1.0));
-            _oldPosition = Target.transform.position;
-        }
+        if (Target == null || Target.transform.position == _oldPosition) return;
+        _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
+        _oldPosition = Target.transform.position;
     }
 
     private void OnApplicationQuit()
