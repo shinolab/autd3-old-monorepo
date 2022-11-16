5d85407672ec7a70ecd5f018a6f8a9be03a916c1
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-16 10:15:40 +0900

    add autd3 lib

--- include/autd3/core/amplitudes.hpp
+++ include/autd3/core/amplitudes.hpp
@@ -3,7 +3,7 @@
 // Created Date: 28/06/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 15/11/2022
+// Last Modified: 16/11/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -13,6 +13,10 @@
 
 #include <algorithm>
 #include <vector>
+
+#include "autd3/core/geometry.hpp"
+#include "autd3/core/interface.hpp"
+#include "autd3/driver/common/cpu/datagram.hpp"
 
 namespace autd3::core {
 /**
