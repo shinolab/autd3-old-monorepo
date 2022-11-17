d0f385c0905359774a65a86b04a7236a88477a54
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-17 15:25:18 +0900

    fix examples

--- python/example/simulator_server.py
+++ python/example/simulator_server.py
@@ -4,7 +4,7 @@ Project: example
 Created Date: 10/10/2022
 Author: Shun Suzuki
 -----
-Last Modified: 21/10/2022
+Last Modified: 17/11/2022
 Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 -----
 Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -15,4 +15,4 @@ from pyautd3.extra import Simulator
 
 
 if __name__ == '__main__':
-    Simulator().settings_path("settings.json").ip("127.0.0.1").port(50632).vsync(True).gpu_idx(0).run()
+    Simulator().settings_path("settings.json").vsync(True).gpu_idx(0).run()
