c913a6f837234f2f7bc7c868ff0679374ae040fb
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-17 10:48:51 +0900

    fix debug link

--- src/link/debug/debug.cpp
+++ src/link/debug/debug.cpp
@@ -3,7 +3,7 @@
 // Created Date: 26/08/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 18/10/2022
+// Last Modified: 17/11/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -13,6 +13,27 @@
 
 #include "autd3/core/link.hpp"
 #include "autd3/extra/cpu_emulator.hpp"
+
+#if _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 6285 6385 26437 26800 26498 26451 26495 26450)
+#endif
+#if defined(__GNUC__) && !defined(__llvm__)
+#pragma GCC diagnostic push
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#endif
+#include "spdlog/spdlog.h"
+#if _MSC_VER
+#pragma warning(pop)
+#endif
+#if defined(__GNUC__) && !defined(__llvm__)
+#pragma GCC diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 namespace autd3::link {
 
