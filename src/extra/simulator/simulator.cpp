5b2fbfaacd899cc7b96e25aab3501c268a7cc3d4
Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-21 12:44:07 +0900

    fix #45

--- src/extra/simulator/simulator.cpp
+++ src/extra/simulator/simulator.cpp
@@ -3,7 +3,7 @@
 // Created Date: 30/09/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 19/11/2022
+// Last Modified: 21/11/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
