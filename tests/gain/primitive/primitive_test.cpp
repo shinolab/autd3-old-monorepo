acacc4216d6bf43d81423882f8c2ba464b05d437
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-25 16:26:55 +0900

    update tests

--- tests/gain/primitive/primitive_test.cpp
+++ tests/gain/primitive/primitive_test.cpp
@@ -3,7 +3,7 @@
 // Created Date: 24/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 17/11/2022
+// Last Modified: 25/11/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -20,6 +20,7 @@
 
 #include <autd3/core/acoustics.hpp>
 
+#include "autd3/autd3_device.hpp"
 #include "autd3/core/geometry.hpp"
 #include "autd3/gain/primitive.hpp"
 
@@ -29,7 +30,7 @@ using autd3::driver::pi;
 
 TEST(NullTest, Test) {
   auto geometry = autd3::core::Geometry();
-  geometry.add_device(Vector3::Zero(), Vector3::Zero());
+  geometry.add_device(autd3::AUTD3(Vector3::Zero(), Vector3::Zero()));
 
   auto g = autd3::gain::Null();
   g.build(geometry);
@@ -41,17 +42,17 @@ TEST(NullTest, Test) {
 
 TEST(FocusTest, Test) {
   auto geometry = autd3::core::Geometry();
-  geometry.add_device(Vector3::Zero(), Vector3::Zero());
+  geometry.add_device(autd3::AUTD3(Vector3::Zero(), Vector3::Zero()));
 
   const Vector3 f(10, 20, 30);
 
   auto g = autd3::gain::Focus(f);
   g.build(geometry);
 
-  const auto expect = std::arg(propagate(geometry[0][0].position(), geometry[0][0].z_direction(), 0.0, geometry[0][0].wavenumber(), f) *
+  const auto expect = std::arg(propagate(geometry[0].position(), geometry[0].z_direction(), 0.0, geometry[0].wavenumber(), f) *
                                std::exp(std::complex(0.0, g.drives()[0].phase)));
   for (size_t i = 0; i < g.drives().size(); i++) {
-    const auto p = std::arg(propagate(geometry[0][i].position(), geometry[0][i].z_direction(), 0.0, geometry[0][i].wavenumber(), f) *
+    const auto p = std::arg(propagate(geometry[i].position(), geometry[i].z_direction(), 0.0, geometry[i].wavenumber(), f) *
                             std::exp(std::complex(0.0, g.drives()[i].phase)));
     ASSERT_EQ(g.drives()[i].amp, 1.0);
     ASSERT_NEAR(p, expect, 2.0 * pi / 256.0);
