426fa0b866c8204e4e4c96416a90a59b222837b7
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-11-26 11:43:11 +0900

    update example

--- examples/remote_twincat.cpp
+++ examples/remote_twincat.cpp
@@ -3,7 +3,7 @@
 // Created Date: 12/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 19/11/2022
+// Last Modified: 26/11/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -17,7 +17,7 @@
 int main() try {
   autd3::Controller autd;
 
-  autd.geometry().add_device(autd3::Vector3::Zero(), autd3::Vector3::Zero());
+  autd.geometry().add_device(autd3::AUTD3(autd3::Vector3::Zero(), autd3::Vector3::Zero()));
 
   const std::string server_ams_net_id = "your TwinCATAUTDServer AMS net id (e.g. 172.16.99.2.1.1)";
   if (auto link = autd3::link::RemoteTwinCAT(server_ams_net_id).build(); !autd.open(std::move(link))) {
