f265804d3010cb6758f89d486beb5a2c1a37c42f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-12-07 08:09:43 +0900

    refactor

--- src/link/remote_twincat/remote_twincat.cpp
+++ src/link/remote_twincat/remote_twincat.cpp
@@ -3,13 +3,32 @@
 // Created Date: 16/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 29/11/2022
+// Last Modified: 07/12/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
 //
 
+#if _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 26495)
+#endif
+#if defined(__GNUC__) && !defined(__llvm__)
+#pragma GCC diagnostic push
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#endif
 #include <AdsLib.h>
+#if _MSC_VER
+#pragma warning(pop)
+#endif
+#if defined(__GNUC__) && !defined(__llvm__)
+#pragma GCC diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 #if WIN32
 #include <Windows.h>
