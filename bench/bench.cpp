46d717dc938ceef37e1f934f5b40d4aea2a66fc7
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-12-09 10:28:30 +0900

    add bench

---
+++ bench/bench.cpp
@@ -0,0 +1,14 @@
+// File: bench.cpp
+// Project: bench
+// Created Date: 07/12/2022
+// Author: Shun Suzuki
+// -----
+// Last Modified: 09/12/2022
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2022 Shun Suzuki. All rights reserved.
+//
+
+#include <benchmark/benchmark.h>
+
+BENCHMARK_MAIN();
