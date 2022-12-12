62d5a9e27e66502ad4fa272e662e6fa47b97b813
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-12-12 14:45:56 +0900

    add holo bench

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -3,7 +3,7 @@
 // Created Date: 07/12/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 09/12/2022
+// Last Modified: 12/12/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -11,4 +11,15 @@
 
 #include <benchmark/benchmark.h>
 
-BENCHMARK_MAIN();
+int main() {
+  char arg0_default[] = "benchmark";
+  char arg1_default[] = "--benchmark_time_unit=ms";
+  std::vector args_default = {arg0_default, arg1_default};
+  int argc = 2;
+  char** argv = args_default.data();
+  benchmark::Initialize(&argc, argv);
+  if (benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  benchmark::RunSpecifiedBenchmarks();
+  benchmark::Shutdown();
+  return 0;
+}
