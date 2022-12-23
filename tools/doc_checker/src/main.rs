ae8b715216d98b2b2672253c56ae2d6a5a0a7581
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-12-23 13:16:46 +0900

    add single option to tools

--- tools/doc_checker/src/main.rs
+++ tools/doc_checker/src/main.rs
@@ -4,7 +4,7 @@
  * Created Date: 24/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 09/10/2022
+ * Last Modified: 23/12/2022
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
