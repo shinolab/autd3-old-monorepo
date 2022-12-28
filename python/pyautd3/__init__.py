55424057f89f3e0844e9c2ead9e44acbe171957a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-12-28 10:24:19 +0900

    add DRIVER_V2_7 const to pyautd3

--- python/pyautd3/__init__.py
+++ python/pyautd3/__init__.py
@@ -4,7 +4,7 @@ Project: pyautd3
 Created Date: 24/05/2021
 Author: Shun Suzuki
 -----
-Last Modified: 27/12/2022
+Last Modified: 28/12/2022
 Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 -----
 Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -16,7 +16,7 @@ from pyautd3.autd import SilencerConfig
 from pyautd3.autd import Controller
 from pyautd3.autd import Amplitudes
 from pyautd3.autd import NUM_TRANS_IN_UNIT, NUM_TRANS_X, NUM_TRANS_Y, TRANS_SPACING, DEVICE_HEIGHT, DEVICE_WIDTH
-from pyautd3.autd import DRIVER_LATEST, DRIVER_V2_2, DRIVER_V2_3, DRIVER_V2_4, DRIVER_V2_5, DRIVER_V2_6
+from pyautd3.autd import DRIVER_LATEST, DRIVER_V2_2, DRIVER_V2_3, DRIVER_V2_4, DRIVER_V2_5, DRIVER_V2_6, DRIVER_V2_7
 from pyautd3.autd import Clear, UpdateFlag, Synchronize, ModDelayConfig, Stop
 from pyautd3.autd import LogOutputFunc, LogFlushFunc, set_log_level, set_log_func
 
@@ -41,6 +41,7 @@ __all__ = [
     'DRIVER_V2_4',
     'DRIVER_V2_5',
     'DRIVER_V2_6',
+    'DRIVER_V2_7',
     'LogOutputFunc',
     'LogFlushFunc',
     'set_log_level',
