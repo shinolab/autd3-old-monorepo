5348ba1bc9302729bb6a5f9ff735ad50629e5541
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-12-29 08:53:49 +0900

    refactor examples

--- examples/tests/soft_stm.hpp
+++ examples/tests/soft_stm.hpp
@@ -3,7 +3,7 @@
 // Created Date: 06/09/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 08/11/2022
+// Last Modified: 29/12/2022
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -25,12 +25,10 @@ inline void soft_stm(autd3::Controller& autd) {
   const autd3::Vector3 center = autd.geometry().center() + autd3::Vector3(0.0, 0.0, 150.0);
   constexpr size_t points_num = 200;
   constexpr auto radius = 30.0;
-  std::vector<size_t> points(points_num);
-  std::iota(points.begin(), points.end(), 0);
-  std::for_each(points.begin(), points.end(), [&](const size_t i) {
+  for (size_t i = 0; i < points_num; i++) {
     const auto theta = 2.0 * autd3::pi * static_cast<double>(i) / static_cast<double>(points_num);
     stm.add(autd3::gain::Focus(center + autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0.0)));
-  });
+  }
 
   const auto actual_freq = stm.set_frequency(1);
   std::cout << "Actual frequency is " << actual_freq << " Hz\n";
