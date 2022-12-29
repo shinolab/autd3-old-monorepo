0ae6c04b95cd48270204534052c58a8f36e7d913
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2022-12-30 08:57:57 +0900

    2.7.3

--- python/pyautd3/__init__.py
+++ python/pyautd3/__init__.py
@@ -4,7 +4,7 @@ Project: pyautd3
 Created Date: 24/05/2021
 Author: Shun Suzuki
 -----
-Last Modified: 28/12/2022
+Last Modified: 30/12/2022
 Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 -----
 Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -48,4 +48,4 @@ __all__ = [
     'set_log_func',
 ]
 
-__version__ = '2.7.2'
+__version__ = '2.7.3'
