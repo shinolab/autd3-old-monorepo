1530876922609720bb6f8955b5a46208b59a1bd8
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-01-15 13:18:13 +0900

    refactor core

--- rust/autd3-core/src/gain.rs
+++ rust/autd3-core/src/gain.rs
@@ -4,19 +4,24 @@
  * Created Date: 27/04/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 09/01/2023
+ * Last Modified: 15/01/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
  *
  */
 
+use crate::geometry::{Geometry, Transducer};
+
+use anyhow::Result;
 use autd3_driver::Drive;
 
-use crate::{datagram::DatagramBody, geometry::Transducer};
+pub trait GainData {
+    fn take_drives(&mut self) -> Vec<Drive>;
+}
 
 /// Gain contains amplitude and phase of each transducer in the AUTD.
 /// Note that the amplitude means duty ratio of Pulse Width Modulation, respectively.
-pub trait Gain<T: Transducer>: DatagramBody<T> {
-    fn drives(&self) -> &[Drive];
+pub trait Gain<T: Transducer>: GainData {
+    fn calc(&mut self, geometry: &Geometry<T>) -> Result<()>;
 }
