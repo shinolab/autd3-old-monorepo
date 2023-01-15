26b24e94ffefe60843fd50ea77e5e0f0f5f5596f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-01-15 13:18:21 +0900

    impl focus

--- rust/autd3/src/gain/focus.rs
+++ rust/autd3/src/gain/focus.rs
@@ -4,26 +4,30 @@
  * Created Date: 28/04/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 09/01/2023
+ * Last Modified: 15/01/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
  *
  */
 
-use autd3_core::geometry::{Geometry, Transducer, Vector3};
+use autd3_core::{
+    gain::Gain,
+    geometry::{Geometry, Transducer, Vector3},
+    Drive,
+};
 
 use autd3_traits::Gain;
 
 /// Gain to produce single focal point
 #[derive(Gain)]
-pub struct Focus<T: Transducer> {
-    op: T::Gain,
+pub struct Focus {
+    drives: Vec<Drive>,
     amp: f64,
     pos: Vector3,
 }
 
-impl<T: Transducer> Focus<T> {
+impl Focus {
     /// constructor
     ///
     /// # Arguments
@@ -43,18 +47,21 @@ impl<T: Transducer> Focus<T> {
     ///
     pub fn with_amp(pos: Vector3, amp: f64) -> Self {
         Self {
-            op: Default::default(),
+            drives: vec![],
             amp,
             pos,
         }
     }
+}
 
+impl<T: Transducer> Gain<T> for Focus {
     fn calc(&mut self, geometry: &Geometry<T>) -> anyhow::Result<()> {
         let sound_speed = geometry.sound_speed;
         geometry.transducers().for_each(|tr| {
             let dist = (self.pos - tr.position()).norm();
             let phase = tr.align_phase_at(dist, sound_speed);
-            self.op.set_drive(tr.id(), self.amp, phase);
+            self.drives[tr.id()].amp = self.amp;
+            self.drives[tr.id()].phase = phase;
         });
         Ok(())
     }
