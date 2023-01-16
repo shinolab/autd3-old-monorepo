4b241e3e1858ddfba5379d96e395930b3e139695
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-01-16 12:54:33 +0900

    update example

--- examples/tests/advanced.hpp
+++ examples/tests/advanced.hpp
@@ -3,7 +3,7 @@
 // Created Date: 16/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 07/01/2023
+// Last Modified: 16/01/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -17,12 +17,13 @@
 
 class BurstModulation final : public autd3::Modulation {
  public:
-  void calc() override {
-    buffer().resize(_buf_size, 0);
-    buffer().at(_buf_size - 1) = 0xFF;
+  std::vector<uint8_t> calc() override {
+    std::vector<uint8_t> buffer(_buf_size, 0);
+    buffer[_buf_size - 1] = 0xFF;
+    return buffer;
   }
 
-  explicit BurstModulation(const size_t buf_size = 4000, const uint16_t freq_div = 40960) noexcept : _buf_size(buf_size) { _op.freq_div = freq_div; }
+  explicit BurstModulation(const size_t buf_size = 4000, const uint16_t freq_div = 40960) noexcept : _buf_size(buf_size) { _freq_div = freq_div; }
 
  private:
   size_t _buf_size;
@@ -32,8 +33,11 @@ class UniformGain final : public autd3::Gain {
  public:
   UniformGain() = default;
 
-  void calc(const autd3::Geometry& geometry) override {
-    std::transform(geometry.begin(), geometry.end(), this->begin(), [this](const auto&) { return autd3::driver::Drive{0.0, 1.0}; });
+  std::vector<autd3::driver::Drive> calc(const autd3::Geometry& geometry) override {
+    std::vector<autd3::driver::Drive> drives;
+    drives.reserve(geometry.num_transducers());
+    std::transform(geometry.begin(), geometry.end(), std::back_inserter(drives), [this](const auto&) { return autd3::driver::Drive{0.0, 1.0}; });
+    return drives;
   }
 };
 
