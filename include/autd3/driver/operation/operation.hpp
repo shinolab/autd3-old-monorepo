405cd18c1f0223b4ad11c554594f9e919accdffe
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-01-16 12:51:47 +0900

    refactor driver

--- include/autd3/driver/operation/operation.hpp
+++ include/autd3/driver/operation/operation.hpp
@@ -3,7 +3,7 @@
 // Created Date: 06/01/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 11/01/2023
+// Last Modified: 16/01/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -22,5 +22,15 @@ struct NormalPhase : Mode {};
 
 template <typename T>
 inline constexpr bool is_mode_v = std::is_base_of_v<Mode, T>;
+
+struct Operation {
+  virtual ~Operation() = default;
+  virtual void init() = 0;
+  virtual void pack(TxDatagram& tx) = 0;
+  virtual bool is_finished() const = 0;
+};
+
+template <typename T>
+inline constexpr bool is_operation_v = std::is_base_of_v<Operation, T>;
 
 }  // namespace autd3::driver
