1afd9946fc601faf550c6f87c76e086bb9e860a1
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-01-22 06:09:22 +0900

    refactor examples

--- examples/tests/focus_stm.hpp
+++ examples/tests/focus_stm.hpp
@@ -3,7 +3,7 @@
 // Created Date: 11/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 31/12/2022
+// Last Modified: 22/01/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -24,13 +24,11 @@ inline void focus_stm(autd3::Controller& autd) {
 
   const autd3::Vector3 center = autd.geometry().center() + autd3::Vector3(0.0, 0.0, 150.0);
   constexpr size_t points_num = 200;
-  constexpr auto radius = 30.0;
-  std::vector<size_t> points(points_num);
-  std::iota(points.begin(), points.end(), 0);
-  std::transform(points.begin(), points.end(), std::back_inserter(stm), [&](const size_t i) {
+  for (size_t i = 0; i < points_num; i++) {
+    constexpr auto radius = 30.0;
     const auto theta = 2.0 * autd3::pi * static_cast<double>(i) / static_cast<double>(points_num);
-    return autd3::FocusSTM::Focus(center + autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0));
-  });
+    stm.add(autd3::FocusSTM::Focus(center + autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0)));
+  }
 
   const auto actual_freq = stm.set_frequency(1);
   std::cout << "Actual frequency is " << actual_freq << " Hz\n";
