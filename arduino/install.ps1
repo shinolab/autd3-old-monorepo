0b5f711fdae0a9f0630e2d6401779b639c05f868
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-01-22 07:19:31 +0900

    rm submodule SOEM4Arduino

--- arduino/install.ps1
+++ arduino/install.ps1
@@ -3,7 +3,7 @@
 # Created Date: 12/10/2022
 # Author: Shun Suzuki
 # -----
-# Last Modified: 13/10/2022
+# Last Modified: 22/01/2023
 # Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 # -----
 # Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -13,9 +13,6 @@ pushd $PSScriptRoot
 
 cd ..
 
-git submodule update --init -- 3rdparty/SOEM4Arduino
-
-Compress-Archive -Path 3rdparty/SOEM4Arduino -DestinationPath $PSScriptRoot/SOEM4Arduino.zip -Force
 Compress-Archive -Path arduino/AUTD3 -DestinationPath $PSScriptRoot/AUTD3.zip -Force
 
 popd
