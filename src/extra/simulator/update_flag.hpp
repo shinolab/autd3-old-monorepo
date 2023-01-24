2410f5ee90cec1eec7f0c05c4da219460fa9e459
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-01-25 05:38:09 +0900

    add thermal sensor option

--- src/extra/simulator/update_flag.hpp
+++ src/extra/simulator/update_flag.hpp
@@ -3,7 +3,7 @@
 // Created Date: 05/10/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 23/12/2022
+// Last Modified: 25/01/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -32,6 +32,7 @@ class UpdateFlags final {
     UpdateSourceAlpha = 1 << 5,
     UpdateSourceFlag = 1 << 6,
     SaveImage = 1 << 7,
+    UpdateDeviceInfo = 1 << 8,
   };
 
   UpdateFlags() = default;
