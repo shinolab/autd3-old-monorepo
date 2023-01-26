f25bb90b835c1abf4243edf6fcd539769bec840f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-01-27 07:50:44 +0900

    add send_receive to link

--- include/autd3/core/link.hpp
+++ include/autd3/core/link.hpp
@@ -3,7 +3,7 @@
 // Created Date: 11/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 04/01/2023
+// Last Modified: 27/01/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -11,7 +11,9 @@
 
 #pragma once
 
+#include <chrono>
 #include <memory>
+#include <thread>
 
 #include "autd3/core/geometry.hpp"
 #include "autd3/driver/cpu/datagram.hpp"
@@ -49,15 +51,38 @@ class Link {
   [[nodiscard]] virtual bool send(const driver::TxDatagram& tx) = 0;
 
   /**
-   * @brief  Read data from devices
+   * @brief  Receive data from devices
    * @return true if succeed
    */
   [[nodiscard]] virtual bool receive(driver::RxDatagram& rx) = 0;
 
   /**
+   * @brief  Send and Read data from devices
+   * @return true if succeed
+   */
+  [[nodiscard]] virtual bool send_receive(const driver::TxDatagram& tx, driver::RxDatagram& rx,
+                                          const std::chrono::high_resolution_clock::duration interval,
+                                          const std::chrono::high_resolution_clock::duration timeout) {
+    if (!send(tx)) return false;
+    if (timeout == std::chrono::high_resolution_clock::duration::zero()) return receive(rx);
+    return wait_msg_processed(tx.header().msg_id, rx, interval, timeout);
+  }
+
+  /**
    * @return true if opened
    */
   [[nodiscard]] virtual bool is_open() = 0;
+
+ protected:
+  bool wait_msg_processed(const uint8_t msg_id, driver::RxDatagram& rx, const std::chrono::high_resolution_clock::duration interval,
+                          const std::chrono::high_resolution_clock::duration timeout) {
+    const auto start = std::chrono::high_resolution_clock::now();
+    do {
+      if (receive(rx) && rx.is_msg_processed(msg_id)) return true;
+      std::this_thread::sleep_for(interval);
+    } while (std::chrono::high_resolution_clock::now() - start < timeout);
+    return false;
+  }
 };
 
 using LinkPtr = std::unique_ptr<Link>;
