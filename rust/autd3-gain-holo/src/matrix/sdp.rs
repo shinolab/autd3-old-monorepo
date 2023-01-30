6aa6ef888a1c0965cf9c465605a599afd8ba1226
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-01-30 11:09:57 +0900

    add Amp, Phase

--- rust/autd3-gain-holo/src/matrix/sdp.rs
+++ rust/autd3-gain-holo/src/matrix/sdp.rs
@@ -4,7 +4,7 @@
  * Created Date: 28/05/2021
  * Author: Shun Suzuki
  * -----
- * Last Modified: 15/01/2023
+ * Last Modified: 30/01/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2021 Shun Suzuki. All rights reserved.
@@ -19,7 +19,7 @@ use anyhow::Result;
 use autd3_core::{
     gain::Gain,
     geometry::{Geometry, Transducer, Vector3},
-    Drive,
+    Amp, Drive, Phase,
 };
 use autd3_traits::Gain;
 use nalgebra::ComplexField;
@@ -176,7 +176,10 @@ impl<B: Backend, C: Constraint, T: Transducer> Gain<T> for SDP<B, C> {
             .map(|tr| {
                 let phase = q[tr.id()].argument() + PI;
                 let amp = self.constraint.convert(q[tr.id()].abs(), max_coefficient);
-                Drive { amp, phase }
+                Drive {
+                    amp: Amp::new(amp),
+                    phase: Phase::new(phase),
+                }
             })
             .collect())
     }
