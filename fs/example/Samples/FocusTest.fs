d89ce9a41950e263d94ce7073c0327dbf2b922b9
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-02-03 10:03:25 +0900

    add F# example

---
+++ fs/example/Samples/FocusTest.fs
@@ -0,0 +1,14 @@
+﻿namespace Samples
+
+open AUTD3Sharp
+open AUTD3Sharp.Gain
+open AUTD3Sharp.Modulation
+open AUTD3Sharp.Utils
+
+module FocusTest =
+    let Test (autd : Controller) = 
+        new SilencerConfig() |> autd.Send |> ignore
+
+        let m = new Sine 150;
+        let g = new Focus(autd.Geometry.Center + Vector3d(0, 0, 150));
+        autd.Send(m, g) |> ignore
