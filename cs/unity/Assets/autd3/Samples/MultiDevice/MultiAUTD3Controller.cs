d7208be0f77f9726729c0285aefbd80bce992648
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-02-03 10:31:24 +0900

    fix #58

--- cs/unity/Assets/autd3/Samples/MultiDevice/MultiAUTD3Controller.cs
+++ cs/unity/Assets/autd3/Samples/MultiDevice/MultiAUTD3Controller.cs
@@ -4,13 +4,14 @@
  * Created Date: 27/12/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 27/12/2022
+ * Last Modified: 03/02/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
  * 
  */
 
+using System;
 using System.Linq;
 using AUTD3Sharp;
 using UnityEngine;
@@ -21,18 +22,24 @@ using UnityEngine;
 
 public class MultiAUTD3Controller : MonoBehaviour
 {
-    private readonly Controller _autd = new();
+    private Controller? _autd = null;
     public GameObject? Target = null;
     private Vector3 _oldPosition;
 
     void Awake()
     {
+        var builder = new GeometryBuilder();
         foreach (var obj in FindObjectsOfType<AUTD3Device>(false).OrderBy(obj => obj.ID))
-            _autd.Geometry.AddDevice(obj.transform.position, obj.transform.rotation);
+            builder.AddDevice(obj.transform.position, obj.transform.rotation);
+        var geometry = builder.Build();
 
         var link = new AUTD3Sharp.Link.Simulator().Build();
 
-        if (!_autd.Open(link))
+        try
+        {
+            _autd = Controller.Open(geometry, link);
+        }
+        catch (Exception)
         {
             Debug.LogError("Before running this sample, open simulator from \"AUTD -> Simulator -> Run\" in menubar.");
 #if UNITY_EDITOR
@@ -42,29 +49,29 @@ public class MultiAUTD3Controller : MonoBehaviour
 #endif
         }
 
-        _autd.AckCheckTimeoutMs = 20;
+        _autd!.AckCheckTimeoutMs = 20;
 
-        _autd.Send(new Clear());
+        _autd!.Send(new Clear());
 
-        _autd.Send(new Synchronize());
+        _autd!.Send(new Synchronize());
 
-        _autd.Send(new AUTD3Sharp.Modulation.Sine(150)); // 150 Hz
+        _autd!.Send(new AUTD3Sharp.Modulation.Sine(150)); // 150 Hz
 
         if (Target == null) return;
-        _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position, 1.0f));
+        _autd!.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position, 1.0f));
         _oldPosition = Target.transform.position;
     }
 
     private void Update()
     {
         if (Target == null || Target.transform.position == _oldPosition) return;
-        _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
+        _autd?.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
         _oldPosition = Target.transform.position;
     }
 
     private void OnApplicationQuit()
     {
-        _autd.Dispose();
+        _autd?.Dispose();
     }
 }
 
