bb20303e032214ada41badbb59ba91f365667aae
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-02-03 10:31:24 +0900

    fix #58

--- cs/unity/Assets/autd3/Samples/Simple/SimpleAUTDController.cs
+++ cs/unity/Assets/autd3/Samples/Simple/SimpleAUTDController.cs
@@ -4,13 +4,14 @@
  * Created Date: 10/10/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 25/12/2022
+ * Last Modified: 03/02/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
  * 
  */
 
+using System;
 using AUTD3Sharp;
 using UnityEngine;
 
@@ -20,7 +21,7 @@ using UnityEngine;
 
 public class SimpleAUTDController : MonoBehaviour
 {
-    private readonly Controller _autd = new();
+    private Controller? _autd = null;
     public GameObject? Target = null;
 
     private Vector3 _oldPosition;
@@ -52,17 +53,22 @@ public class SimpleAUTDController : MonoBehaviour
 
     private void Awake()
     {
-        AUTD3Sharp.Link.SOEM.SetLogFunc(_output, _flush);
-
-        _autd.Geometry.AddDevice(gameObject.transform.position, gameObject.transform.rotation);
+        var geometry = new GeometryBuilder()
+            .AddDevice(gameObject.transform.position, gameObject.transform.rotation)
+            .Build();
 
         var link = new AUTD3Sharp.Link.SOEM()
            .HighPrecision(true)
            // .FreeRun(true)
            .OnLost(_onLost)
+           .DebugLogFunc(_output, _flush)
            .Build();
 
-        if (!_autd.Open(link))
+        try
+        {
+            _autd = Controller.Open(geometry, link);
+        }
+        catch (Exception)
         {
             Debug.LogError("Failed to open AUTD3 controller!");
 #if UNITY_EDITOR
@@ -72,16 +78,16 @@ public class SimpleAUTDController : MonoBehaviour
 #endif
         }
 
-        _autd.AckCheckTimeoutMs = 20;
+        _autd!.AckCheckTimeoutMs = 20;
 
-        _autd.Send(new Clear());
+        _autd!.Send(new Clear());
 
-        _autd.Send(new Synchronize());
+        _autd!.Send(new Synchronize());
 
-        _autd.Send(new AUTD3Sharp.Modulation.Sine(150)); // 150 Hz
+        _autd!.Send(new AUTD3Sharp.Modulation.Sine(150)); // 150 Hz
 
         if (Target == null) return;
-        _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position, 1.0f));
+        _autd?.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
         _oldPosition = Target.transform.position;
     }
 
@@ -96,13 +102,13 @@ public class SimpleAUTDController : MonoBehaviour
 #endif
 
         if (Target == null || Target.transform.position == _oldPosition) return;
-        _autd.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
+        _autd?.Send(new AUTD3Sharp.Gain.Focus(Target.transform.position));
         _oldPosition = Target.transform.position;
     }
 
     private void OnApplicationQuit()
     {
-        _autd.Dispose();
+        _autd?.Dispose();
     }
 }
 
