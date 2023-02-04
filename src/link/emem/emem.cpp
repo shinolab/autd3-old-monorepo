9e0588f7c3dd1d97020cefa4f1ad6d9a2ed081d5
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-02-04 11:02:34 +0900

    begin emem

---
+++ src/link/emem/emem.cpp
@@ -0,0 +1,11 @@
+// File: emem.cpp
+// Project: emem
+// Created Date: 04/02/2023
+// Author: Shun Suzuki
+// -----
+// Last Modified: 04/02/2023
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2023 Shun Suzuki. All rights reserved.
+// 
+
