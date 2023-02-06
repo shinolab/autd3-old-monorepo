fc72902b58e37efdd7fc388fc5bc50d4a42b2c74
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-02-06 09:44:03 +0900

    fix header

--- src/link/emem/ethercat/header.hpp
+++ src/link/emem/ethercat/header.hpp
@@ -3,7 +3,7 @@
 // Created Date: 05/02/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 05/02/2023
+// Last Modified: 06/02/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -22,21 +22,22 @@ namespace autd3::link::ethercat {
 const uint16_t ETH_TYPE_ECAT = htons(0x88A4);
 
 struct EthernetHeader {
-  uint16_t _da0;
-  uint16_t _da1;
-  uint16_t _da2;
-  uint16_t _sa0;
-  uint16_t _sa1;
-  uint16_t _sa2;
-  uint16_t etype;
+  static EthernetHeader ecat_header() noexcept { return {0xFFFF, 0xFFFF, 0xFFFF, 0x0101, 0x0101, 0x0101, ETH_TYPE_ECAT}; }
 
-  [[nodiscard]] bool is_ecat_frame() const noexcept { return etype == ETH_TYPE_ECAT; }
+  [[nodiscard]] bool is_ecat_frame() const noexcept { return _etype == ETH_TYPE_ECAT; }
+
+ private:
+  EthernetHeader(const uint16_t da0, const uint16_t da1, const uint16_t da2, const uint16_t sa0, const uint16_t sa1, const uint16_t sa2,
+                 const uint16_t etype)
+      : _da0(da0), _da1(da1), _da2(da2), _sa0(sa0), _sa1(sa1), _sa2(sa2), _etype(etype) {}
+
+  uint16_t _da0;  // NOLINT
+  uint16_t _da1;  // NOLINT
+  uint16_t _da2;  // NOLINT
+  uint16_t _sa0;  // NOLINT
+  uint16_t _sa1;  // NOLINT
+  uint16_t _sa2;  // NOLINT
+  uint16_t _etype;
 };
-
-const EthernetHeader ETH_ECAT_HEADER = EthernetHeader{
-    0xFFFF, 0xFFFF, 0xFFFF, 0x0101, 0x0101, 0x0101, ETH_TYPE_ECAT,
-};
-
-constexpr size_t ETH_HEADER_SIZE = sizeof(EthernetHeader);
 
 }  // namespace autd3::link::ethercat
