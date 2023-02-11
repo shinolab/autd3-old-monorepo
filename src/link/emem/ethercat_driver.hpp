942254fdbe68e55c5c4dcf8e20586c27eb3b6f56
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-02-12 07:05:21 +0900

    tentative error handler

--- src/link/emem/ethercat_driver.hpp
+++ src/link/emem/ethercat_driver.hpp
@@ -3,7 +3,7 @@
 // Created Date: 06/02/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 10/02/2023
+// Last Modified: 12/02/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -184,11 +184,11 @@ class EtherCATDriver {
           if (_idx_stack.dc_offset(pos) > 0) {
             std::memcpy(_idx_stack.data(pos), _net_driver.buffer(idx).rx_data() + 2 + sizeof(ethercat::DatagramHeader), _idx_stack.length(pos));
             uint8_t le_wkc[sizeof(uint16_t)];
-            std::memcpy(&le_wkc, _net_driver.buffer(idx).rx_data() + 2 + sizeof(ethercat::DatagramHeader) + _idx_stack.length(pos), sizeof(uint16_t));
+            std::memcpy(le_wkc, _net_driver.buffer(idx).rx_data() + 2 + sizeof(ethercat::DatagramHeader) + _idx_stack.length(pos), sizeof(uint16_t));
             wkc = u16_from_le_bytes(le_wkc[0], le_wkc[1]);
 
             uint8_t le_dc_time[sizeof(int64_t)];
-            std::memcpy(&le_dc_time, _net_driver.buffer(idx).rx_data() + _idx_stack.dc_offset(pos), sizeof(int64_t));
+            std::memcpy(le_dc_time, _net_driver.buffer(idx).rx_data() + _idx_stack.dc_offset(pos), sizeof(int64_t));
             dc_time = i64_from_le_bytes(le_dc_time);
           } else {
             std::memcpy(_idx_stack.data(pos), _net_driver.buffer(idx).rx_data() + 2 + sizeof(ethercat::DatagramHeader), _idx_stack.length(pos));
@@ -198,11 +198,11 @@ class EtherCATDriver {
         } else if (cmd == ethercat::Command::Lwr) {
           if (_idx_stack.dc_offset(pos) > 0) {
             uint8_t le_wkc[sizeof(uint16_t)];
-            std::memcpy(&le_wkc, _net_driver.buffer(idx).rx_data() + 2 + sizeof(ethercat::DatagramHeader) + _idx_stack.length(pos), sizeof(uint16_t));
+            std::memcpy(le_wkc, _net_driver.buffer(idx).rx_data() + 2 + sizeof(ethercat::DatagramHeader) + _idx_stack.length(pos), sizeof(uint16_t));
             wkc = u16_from_le_bytes(le_wkc[0], le_wkc[1]) * 2;
 
             uint8_t le_dc_time[sizeof(int64_t)];
-            std::memcpy(&le_dc_time, _net_driver.buffer(idx).rx_data() + _idx_stack.dc_offset(pos), sizeof(int64_t));
+            std::memcpy(le_dc_time, _net_driver.buffer(idx).rx_data() + _idx_stack.dc_offset(pos), sizeof(int64_t));
             dc_time = i64_from_le_bytes(le_dc_time);
           } else {
             wkc += wkc2 * 2;
