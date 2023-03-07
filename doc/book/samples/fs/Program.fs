b003395ba23c995076cb121605d1d2096a65dc39
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-03-08 08:43:07 +0900

    fix doc samples

--- doc/book/samples/fs/Program.fs
+++ doc/book/samples/fs/Program.fs
@@ -1,4 +1,5 @@
-﻿open AUTD3Sharp
+﻿open System
+open AUTD3Sharp
 open AUTD3Sharp.Utils
 open AUTD3Sharp.Gain
 open AUTD3Sharp.Modulation
@@ -13,21 +14,19 @@ let link = (new SOEM()).HighPrecision(true).Build()
 
 let autd = Controller.Open (geometry, link)
 
-autd.AckCheckTimeoutMs <- 20uL;
-
-new Clear() |> autd.Send |> ignore;
-new Synchronize() |> autd.Send |> ignore;
+(new Clear(), TimeSpan.FromMilliseconds(20)) |> autd.Send |> ignore;
+(new Synchronize(), TimeSpan.FromMilliseconds(20)) |> autd.Send |> ignore;
 
 let print_firm firm = printfn $"{firm}" 
 printfn "==================================== Firmware information ======================================"
 autd.FirmwareInfoList() |> Seq.iter print_firm
 printfn "================================================================================================"
 
-new SilencerConfig() |> autd.Send |> ignore;
+(new SilencerConfig(), TimeSpan.FromMilliseconds(20)) |> autd.Send |> ignore;
 
 let m = new Sine 150;
 let g = new Focus(autd.Geometry.Center + Vector3d(0, 0, 150));
-autd.Send(m, g) |> ignore
+autd.Send(m, g, TimeSpan.FromMilliseconds(20)) |> ignore
 
 System.Console.ReadKey true |> ignore;
 
