d0901b76f3c1300cad27b619cf9dd6cfda8f0f0d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-03-08 08:43:07 +0900

    fix doc samples

--- doc/book/samples/rust/sample/src/main.rs
+++ doc/book/samples/rust/sample/src/main.rs
@@ -4,7 +4,7 @@ use autd3_link_soem::{Config, SOEM};
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let mut geometry = GeometryBuilder::new().legacy_mode().build();
+    let mut geometry = GeometryBuilder::new().build();
     geometry.add_device(AUTD3::new(Vector3::zeros(), Vector3::zeros()))?;
 
     let config = Config {
@@ -18,27 +18,31 @@ fn main() -> Result<()> {
 
     let mut autd = Controller::open(geometry, link).expect("Failed to open");
 
-    autd.ack_check_timeout = std::time::Duration::from_millis(20);
-
     let mut clear = Clear::new();
-    autd.send(&mut clear).flush()?;
-
+    autd.timeout(std::time::Duration::from_millis(20))
+        .send(&mut clear)
+        .flush()?;
     let mut sync = Synchronize::new();
-    autd.send(&mut sync).flush()?;
+    autd.timeout(std::time::Duration::from_millis(20))
+        .send(&mut sync)
+        .flush()?;
 
     autd.firmware_infos().unwrap().iter().for_each(|firm_info| {
         println!("{}", firm_info);
     });
 
     let mut silencer = SilencerConfig::default();
-    autd.send(&mut silencer).flush()?;
+    autd.timeout(std::time::Duration::from_millis(20))
+        .send(&mut silencer)
+        .flush()?;
 
     let center = autd.geometry().center() + Vector3::new(0., 0., 150.0);
-
     let mut g = Focus::new(center);
     let mut m = Sine::new(150);
 
-    autd.send(&mut m).send(&mut g)?;
+    autd.timeout(std::time::Duration::from_millis(20))
+        .send(&mut m)
+        .send(&mut g)?;
 
     let mut _s = String::new();
     std::io::stdin().read_line(&mut _s)?;
