daa27541977c041e25085b7acf7ee283da422956
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-03-20 05:05:18 +0900

    fix

--- include/autd3/core/utils/osal_timer/callback.hpp
+++ include/autd3/core/utils/osal_timer/callback.hpp
@@ -1,9 +1,9 @@
-// File: timer_strategy.hpp
-// Project: core
-// Created Date: 19/03/2023
+// File: callback.hpp
+// Project: osal_timer
+// Created Date: 18/03/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 19/03/2023
+// Last Modified: 20/03/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -13,10 +13,10 @@
 
 namespace autd3::core {
 
-enum class TimerStrategy {
-  Sleep,
-  BusyWait,
-  NativeTimer,
+class CallbackHandler {
+ public:
+  virtual void callback() = 0;
+  virtual ~CallbackHandler() = default;
 };
 
 }  // namespace autd3::core
