d5e7e747daae4695ce798f1d997356719d7240ed
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-04-11 13:38:03 +0900

    refactor macro

---
+++ capi/base/lpf_wrapper.hpp
@@ -0,0 +1,24 @@
+// File: lpf_wrapper.hpp
+// Project: base
+// Created Date: 11/04/2023
+// Author: Shun Suzuki
+// -----
+// Last Modified: 11/04/2023
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2023 Shun Suzuki. All rights reserved.
+//
+
+#pragma once
+
+#include "autd3.hpp"
+#include "autd3/modulation/lpf.hpp"
+
+class LPF4CAPI : autd3::Modulation {
+ public:
+  explicit LPF4CAPI(autd3::core::Modulation* modulation) : Modulation(8192), _lpf(modulation) {}
+  std::vector<autd3::driver::autd3_float_t> calc() override { return _lpf.calc(); }
+
+ private:
+  autd3::modulation::LPF<Modulation*> _lpf;
+};
