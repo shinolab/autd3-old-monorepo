6c809e18ec81a806fd25aac3253086917a2c8d01
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-04-17 11:13:16 +0900

    update examples

--- examples/tests/plane.hpp
+++ examples/tests/plane.hpp
@@ -3,7 +3,7 @@
 // Created Date: 16/05/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 07/03/2023
+// Last Modified: 17/04/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -15,12 +15,12 @@
 
 inline void plane_test(autd3::Controller& autd) {
   autd3::SilencerConfig silencer;
-  autd.send(silencer, std::chrono::milliseconds(20));
+  autd.send(silencer);
 
   autd3::modulation::Sine m(150);  // 150Hz AM
 
   const autd3::Vector3 direction = autd3::Vector3::UnitZ();
   autd3::gain::PlaneWave g(direction);
 
-  autd.send(m, g, std::chrono::milliseconds(20));
+  autd.send(m, g);
 }
