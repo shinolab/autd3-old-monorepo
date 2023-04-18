50f5f20f9abf478de4f8e5ecc5e2d3b8da26050b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-04-18 13:08:15 +0900

    update rust

--- doc/book/samples/rust/sample/src/main.rs
+++ doc/book/samples/rust/sample/src/main.rs
@@ -16,30 +16,22 @@ fn main() -> Result<()> {
     let mut autd = Controller::open(geometry, link).expect("Failed to open");
 
     let mut clear = Clear::new();
-    autd.timeout(std::time::Duration::from_millis(20))
-        .send(&mut clear)
-        .flush()?;
+    autd.send(&mut clear).flush()?;
     let mut sync = Synchronize::new();
-    autd.timeout(std::time::Duration::from_millis(20))
-        .send(&mut sync)
-        .flush()?;
+    autd.send(&mut sync).flush()?;
 
     autd.firmware_infos().unwrap().iter().for_each(|firm_info| {
         println!("{}", firm_info);
     });
 
     let mut silencer = SilencerConfig::default();
-    autd.timeout(std::time::Duration::from_millis(20))
-        .send(&mut silencer)
-        .flush()?;
+    autd.send(&mut silencer).flush()?;
 
     let center = autd.geometry().center() + Vector3::new(0., 0., 150.0);
     let mut g = Focus::new(center);
     let mut m = Sine::new(150);
 
-    autd.timeout(std::time::Duration::from_millis(20))
-        .send(&mut m)
-        .send(&mut g)?;
+    autd.send(&mut m).send(&mut g)?;
 
     let mut _s = String::new();
     std::io::stdin().read_line(&mut _s)?;
--- rust/autd3-examples/src/tests/focus.rs
+++ rust/autd3-examples/src/tests/focus.rs
@@ -4,7 +4,7 @@
  * Created Date: 28/05/2021
  * Author: Shun Suzuki
  * -----
- * Last Modified: 07/03/2023
+ * Last Modified: 18/04/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2021 Shun Suzuki. All rights reserved.
@@ -15,19 +15,13 @@
 macro_rules! focus {
     ($autd:ident) => {{
         let mut silencer_config = SilencerConfig::default();
-        $autd
-            .timeout(std::time::Duration::from_millis(20))
-            .send(&mut silencer_config)
-            .flush()?;
+        $autd.send(&mut silencer_config).flush()?;
 
         let center = $autd.geometry().center() + Vector3::new(0., 0., 150.0);
 
         let mut g = Focus::new(center);
         let mut m = Sine::new(150);
 
-        $autd
-            .timeout(std::time::Duration::from_millis(20))
-            .send(&mut m)
-            .send(&mut g)?;
+        $autd.send(&mut m).send(&mut g)?;
     }};
 }
