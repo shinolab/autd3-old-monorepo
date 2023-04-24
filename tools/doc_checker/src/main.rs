66615bdb62e6e46f618ecfa0623d2ccea603c221
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-04-25 08:35:04 +0900

    update tools

--- tools/doc_checker/src/main.rs
+++ tools/doc_checker/src/main.rs
@@ -4,7 +4,7 @@
  * Created Date: 24/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 16/03/2023
+ * Last Modified: 25/04/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
