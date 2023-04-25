ee641f137e4471099dc39c9cc0ffe86dad420899
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-04-25 15:06:08 +0900

    use boost for tcp

--- dist/SOEMAUTDServer/tcp_interface.hpp
+++ dist/SOEMAUTDServer/tcp_interface.hpp
@@ -3,7 +3,7 @@
 // Created Date: 01/11/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 20/01/2023
+// Last Modified: 25/04/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -11,36 +11,49 @@
 
 #pragma once
 
+#ifdef WIN32
+#include <SDKDDKVer.h>
+#endif
+
+#if _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4312)
+#endif
+#if defined(__GNUC__) && !defined(__llvm__)
+#pragma GCC diagnostic push
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#endif
+#include <boost/asio.hpp>
+#include <boost/format.hpp>
+#if _MSC_VER
+#pragma warning(pop)
+#endif
+#if defined(__GNUC__) && !defined(__llvm__)
+#pragma GCC diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 
+#include "../../spdlog.hpp"
 #include "interface.hpp"
-
-#if WIN32
-#include <WS2tcpip.h>
-#else
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-#include <string>
-#endif
-
-#if WIN32
-#elif __APPLE__
-#else
-#include <signal.h>
-#endif
 
 namespace autd3::publish {
 
 class TcpInterface final : public Interface {
  public:
-  TcpInterface(std::string ip, const uint16_t port, const size_t dev) noexcept : _is_open(false), _dev(dev), _ip(std::move(ip)), _port(port) {}
+  TcpInterface(std::string ip, const uint16_t port, const size_t dev) noexcept
+      : _is_open(false),
+        _dev(dev),
+        _acc(_io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(ip), port)),
+        _socket(_io_service) {}
   ~TcpInterface() override = default;
   TcpInterface(const TcpInterface& v) noexcept = delete;
   TcpInterface& operator=(const TcpInterface& obj) = delete;
@@ -48,81 +61,36 @@ class TcpInterface final : public Interface {
   TcpInterface& operator=(TcpInterface&& obj) = delete;
 
   void connect() override {
-#if WIN32
-#pragma warning(push)
-#pragma warning(disable : 6031)
-    WSAData wsa_data{};
-    WSAStartup(MAKEWORD(2, 0), &wsa_data);
-#pragma warning(pop)
-#else
-    signal(SIGPIPE, SIG_IGN);
-#endif
-
-    _socket = socket(AF_INET, SOCK_STREAM, 0);
-#if WIN32
-    if (_socket == INVALID_SOCKET) {
-      WSACleanup();
-#else
-    if (_socket < 0) {
-#endif
-      throw std::runtime_error("Cannot connect to client");
-    }
-
-    constexpr int y = 1;
-    setsockopt(_socket, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char*>(&y), sizeof y);
-
-    _addr.sin_family = AF_INET;
-    _addr.sin_port = htons(_port);
-#if WIN32
-    _addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-#else
-    _addr.sin_addr.s_addr = htonl(INADDR_ANY);
-#endif
-
-    if (const auto e = bind(_socket, reinterpret_cast<sockaddr*>(&_addr), sizeof _addr); e != 0)
-      throw std::runtime_error("Failed to bind socket: " + std::to_string(e));
-    if (const auto e = listen(_socket, 1); e != 0) throw std::runtime_error("Failed to listen: " + std::to_string(e));
+    spdlog::info("Waiting for client connection...");
+    boost::system::error_code error;
+    _acc.accept(_socket, error);
+    if (error) throw std::runtime_error((boost::format("Cannot connect to client: %1%") % error.message()).str());
+    spdlog::info("Connected to client");
 
     const auto size = driver::HEADER_SIZE + _dev * AUTD3::NUM_TRANS_IN_UNIT * sizeof(uint16_t);
     _ptr = std::make_unique<uint8_t[]>(size);
     std::memset(_ptr.get(), 0, size);
 
-    socklen_t dst_addr_size = sizeof _dst_addr;
-    spdlog::info("Waiting for client connection...");
-    _dst_socket = accept(_socket, reinterpret_cast<sockaddr*>(&_dst_addr), &dst_addr_size);
-#if WIN32
-    if (_dst_socket == INVALID_SOCKET) return;
-    if (GetLastError() == WSAEINTR) return;
-#else
-    if (_dst_socket < 0) return;
-    if (errno == 53) return;
-    if (errno != 0) throw std::runtime_error("Failed to connect client: " + std::string(strerror(errno)));
-#endif
-    spdlog::info("Connected to client");
-
-    u_long val = 1;
-#if WIN32
-    ioctlsocket(_dst_socket, FIONBIO, &val);
-#else
-    ioctl(_dst_socket, FIONBIO, &val);
-#endif
-
     _run = true;
     _th = std::thread([this, size] {
-      std::vector<char> buffer(size);
+      boost::asio::streambuf receive_buffer;
       while (_run) {
-        const auto len = recv(_dst_socket, buffer.data(), static_cast<int>(buffer.size()), 0);
-        if (len <= 0) continue;
-        const auto ulen = static_cast<size_t>(len);
-        if (ulen < driver::HEADER_SIZE) {
-          spdlog::warn("Unknown data size: {}", ulen);
+        boost::system::error_code e;
+        const auto len = read(_socket, receive_buffer, boost::asio::transfer_all(), e);
+        if (e) {
+          spdlog::warn("Receive failed: {}", e.message());
           continue;
         }
-        if (const auto body_len = ulen - driver::HEADER_SIZE; body_len % (AUTD3::NUM_TRANS_IN_UNIT * sizeof(uint16_t)) != 0) {
-          spdlog::warn("Unknown data size: {}", ulen);
+        if (len < driver::HEADER_SIZE) {
+          spdlog::warn("Received data size unknown: {}", len);
           continue;
         }
-        std::memcpy(_ptr.get(), buffer.data(), ulen);
+        if (const auto body_len = len - driver::HEADER_SIZE; body_len % (AUTD3::NUM_TRANS_IN_UNIT * sizeof(uint16_t)) != 0) {
+          spdlog::warn("Received data size unknown: {}", len);
+          continue;
+        }
+        const auto* buffer = boost::asio::buffer_cast<const uint8_t*>(receive_buffer.data());
+        std::memcpy(_ptr.get(), buffer, len);
       }
     });
 
@@ -138,26 +106,7 @@ class TcpInterface final : public Interface {
       if (_th.joinable()) _th.join();
     }
 
-#if WIN32
-    if (_dst_socket != INVALID_SOCKET) {
-      closesocket(_dst_socket);
-      _dst_socket = INVALID_SOCKET;
-    }
-    if (_socket != INVALID_SOCKET) {
-      closesocket(_socket);
-      _socket = INVALID_SOCKET;
-      WSACleanup();
-    }
-#else
-    if (_dst_socket != -1) {
-      ::close(_dst_socket);
-      _dst_socket = -1;
-    }
-    if (_socket != -1) {
-      ::close(_socket);
-      _socket = -1;
-    }
-#endif
+    _socket.close();
   }
 
   bool tx(driver::TxDatagram& tx) override {
@@ -169,13 +118,11 @@ class TcpInterface final : public Interface {
   }
 
   bool rx(driver::RxDatagram& rx) override {
-#if WIN32
-    if (_dst_socket == INVALID_SOCKET) return true;
-#else
-    if (_dst_socket < 0) return true;
-#endif
-    return send(_dst_socket, reinterpret_cast<const char*>(rx.messages().data()),
-                static_cast<int>(rx.messages().size() * driver::EC_INPUT_FRAME_SIZE), 0) >= 0;
+    boost::system::error_code error;
+    write(_socket, boost::asio::buffer(rx.messages()), error);
+    if (!error) return true;
+    if (error) spdlog::warn("Send failed: {}", error.message());
+    return false;
   }
 
  private:
@@ -185,21 +132,12 @@ class TcpInterface final : public Interface {
   size_t _dev;
   std::unique_ptr<uint8_t[]> _ptr;
 
-  std::string _ip;
-  uint16_t _port;
-
   bool _run{false};
   std::thread _th;
 
-#if WIN32
-  UINT_PTR _socket{};
-  UINT_PTR _dst_socket{};
-#else
-  int _socket{-1};
-  int _dst_socket{-1};
-#endif
-  sockaddr_in _addr{};
-  sockaddr_in _dst_addr{};
+  boost::asio::io_service _io_service;
+  boost::asio::ip::tcp::acceptor _acc;
+  boost::asio::ip::tcp::socket _socket;
 };
 
 }  // namespace autd3::publish
