b2f9128b478756c24fb6991bada3f595c955d09e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-04-26 13:40:26 +0900

    fix capi

--- capi/link_remote_soem/c_api.cpp
+++ capi/link_remote_soem/c_api.cpp
@@ -16,7 +16,7 @@
 
 void AUTDLinkRemoteSOEM(void** out, const char* ip, const uint16_t port, const uint64_t timeout_ns) {
   const std::string ip_ = ip == nullptr ? std::string("") : std::string(ip);
-  auto soem_link = autd3::link::RemoteSOEM().ip(ip_).port(port).timeout(std::chrono::nanoseconds(timeout_ns)).build();
+  auto soem_link = autd3::link::RemoteSOEM(ip_, port).timeout(std::chrono::nanoseconds(timeout_ns)).build();
   auto* link = link_create(std::move(soem_link));
   *out = link;
 }
