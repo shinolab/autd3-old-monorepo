eb03d3bdca56700b528c0e7742d82f795f92dd1d
Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-04-26 10:49:58 +0900

    fix SOEMAUTDserver

--- dist/SOEMAUTDServer/tcp_interface.hpp
+++ dist/SOEMAUTDServer/tcp_interface.hpp
@@ -3,7 +3,7 @@
 // Created Date: 01/11/2022
 // Author: Shun Suzuki
 // -----
-// Last Modified: 25/04/2023
+// Last Modified: 26/04/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2022 Shun Suzuki. All rights reserved.
@@ -37,6 +37,7 @@
 #pragma clang diagnostic pop
 #endif
 
+#include <atomic>
 #include <memory>
 #include <string>
 #include <utility>
@@ -49,38 +50,24 @@ namespace autd3::publish {
 
 class TcpInterface final : public Interface {
  public:
-  TcpInterface(std::string ip, const uint16_t port, const size_t dev) noexcept
-      : _is_open(false),
-        _dev(dev),
-        _acc(_io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(ip), port)),
-        _socket(_io_service) {}
+  TcpInterface(std::string ip, const uint16_t port, const size_t dev) noexcept : _is_open(false), _dev(dev), _port(port) {}
   ~TcpInterface() override = default;
   TcpInterface(const TcpInterface& v) noexcept = delete;
   TcpInterface& operator=(const TcpInterface& obj) = delete;
   TcpInterface(TcpInterface&& obj) = delete;
   TcpInterface& operator=(TcpInterface&& obj) = delete;
 
-  void connect() override {
-    spdlog::info("Waiting for client connection...");
-    boost::system::error_code error;
-    _acc.accept(_socket, error);
-    if (error) throw std::runtime_error((boost::format("Cannot connect to client: %1%") % error.message()).str());
-    spdlog::info("Connected to client");
-
-    const auto size = driver::HEADER_SIZE + _dev * AUTD3::NUM_TRANS_IN_UNIT * sizeof(uint16_t);
-    _ptr = std::make_unique<uint8_t[]>(size);
-    std::memset(_ptr.get(), 0, size);
-
-    _run = true;
-    _th = std::thread([this, size] {
-      boost::asio::streambuf receive_buffer;
-      while (_run) {
-        boost::system::error_code e;
-        const auto len = read(_socket, receive_buffer, boost::asio::transfer_all(), e);
-        if (e) {
-          spdlog::warn("Receive failed: {}", e.message());
-          continue;
-        }
+  void session() {
+    spdlog::info("Connected to client: {}", _sock->remote_endpoint().address().to_string());
+    try {
+      for (;;) {
+        uint8_t buffer[65536];
+        boost::system::error_code error;
+        size_t len = _sock->read_some(boost::asio::buffer(buffer), error);
+        if (error == boost::asio::error::eof || error == boost::asio::error::connection_reset)
+          break;
+        else if (error)
+          throw boost::system::system_error(error);
         if (len < driver::HEADER_SIZE) {
           spdlog::warn("Received data size unknown: {}", len);
           continue;
@@ -89,27 +76,41 @@ class TcpInterface final : public Interface {
           spdlog::warn("Received data size unknown: {}", len);
           continue;
         }
-        const auto* buffer = boost::asio::buffer_cast<const uint8_t*>(receive_buffer.data());
         std::memcpy(_ptr.get(), buffer, len);
       }
-    });
-
-    _last_msg_id = 0;
-    _is_open = true;
-  }
-
-  void close() override {
-    _is_open = false;
-
-    if (_run) {
-      _run = false;
-      if (_th.joinable()) _th.join();
+    } catch (std::exception& e) {
+      spdlog::error("Exception in thread: {}", e.what());
     }
 
-    _socket.close();
+    spdlog::info("Disconnected from client");
   }
 
+  void connect() override {
+    const auto size = driver::HEADER_SIZE + _dev * AUTD3::NUM_TRANS_IN_UNIT * sizeof(uint16_t);
+    _ptr = std::make_unique<uint8_t[]>(size);
+    std::memset(_ptr.get(), 0, size);
+
+    std::thread([this]() {
+      boost::asio::io_context io_context;
+      boost::asio::ip::tcp::acceptor a(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), _port));
+      for (;;) {
+        _is_open.store(false);
+        spdlog::info("Waiting for client connection...");
+        _sock = std::make_shared<boost::asio::ip::tcp::socket>(a.accept());
+        _is_open.store(true);
+        session();
+      }
+    }).detach();
+
+    while (!is_open()) std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    _last_msg_id = 0;
+  }
+
+  void close() override {}
+
   bool tx(driver::TxDatagram& tx) override {
+    if (!is_open()) return false;
     const auto msg_id = _ptr[0];
     if (_last_msg_id == msg_id) return false;
     _last_msg_id = msg_id;
@@ -118,26 +119,27 @@ class TcpInterface final : public Interface {
   }
 
   bool rx(driver::RxDatagram& rx) override {
-    boost::system::error_code error;
-    write(_socket, boost::asio::buffer(rx.messages()), error);
-    if (!error) return true;
-    if (error) spdlog::warn("Send failed: {}", error.message());
-    return false;
+    if (!is_open()) return false;
+    try {
+      _sock->send(boost::asio::buffer(rx.messages(), rx.messages().size() * sizeof(driver::RxMessage)));
+    } catch (const std::exception&) {
+      return false;
+    }
+    return true;
   }
 
  private:
-  bool _is_open;
+  [[nodiscard]] bool is_open() const { return _is_open.load(); }
+
+  uint16_t _port;
+
+  std::atomic<bool> _is_open;
   uint8_t _last_msg_id{0};
 
   size_t _dev;
   std::unique_ptr<uint8_t[]> _ptr;
 
-  bool _run{false};
-  std::thread _th;
-
-  boost::asio::io_service _io_service;
-  boost::asio::ip::tcp::acceptor _acc;
-  boost::asio::ip::tcp::socket _socket;
+  std::shared_ptr<boost::asio::ip::tcp::socket> _sock{nullptr};
 };
 
 }  // namespace autd3::publish
