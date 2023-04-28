1ca2f6fbcaf7ab49514c855bd715103fb10fb70f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-04-28 13:38:29 +0900

    update python

--- python/pyautd3/native_methods/autd3capi_link_simulator.py
+++ python/pyautd3/native_methods/autd3capi_link_simulator.py
@@ -27,5 +27,17 @@ class NativeMethods(metaclass=Singleton):
             return
         self.dll = ctypes.CDLL(os.path.join(self.bin, f'{self.prefix}autd3capi-link-simulator{self.ext}'))
 
-        self.dll.AUTDLinkSimulator.argtypes = [ctypes.POINTER(ctypes.c_void_p), ctypes.c_uint64] 
+        self.dll.AUTDLinkSimulator.argtypes = [ctypes.POINTER(ctypes.c_void_p)] 
         self.dll.AUTDLinkSimulator.restype = None
+
+        self.dll.AUTDLinkSimulatorLogLevel.argtypes = [ctypes.c_void_p, ctypes.c_int32] 
+        self.dll.AUTDLinkSimulatorLogLevel.restype = None
+
+        self.dll.AUTDLinkSimulatorLogFunc.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p] 
+        self.dll.AUTDLinkSimulatorLogFunc.restype = None
+
+        self.dll.AUTDLinkSimulatorTimeout.argtypes = [ctypes.c_void_p, ctypes.c_uint64] 
+        self.dll.AUTDLinkSimulatorTimeout.restype = None
+
+        self.dll.AUTDLinkSimulatorBuild.argtypes = [ctypes.POINTER(ctypes.c_void_p), ctypes.c_void_p] 
+        self.dll.AUTDLinkSimulatorBuild.restype = None
