48ac559702a62036dccb77b43fed421c544bfb46
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-01 13:23:36 +0900

    add RemoteSImulator to capi

--- julia/src/NativeMethods/autd3capi-link-remote-soem.jl
+++ julia/src/NativeMethods/autd3capi-link-remote-soem.jl
@@ -33,8 +33,6 @@ end
 const _dll = joinpath(@__DIR__, get_bin_path(), "bin", get_lib_prefix() * "autd3capi-link-remote-soem" * get_lib_ext())
 
 autd_link_remote_soem(out, ip, port) = ccall((:AUTDLinkRemoteSOEM, _dll), Cvoid, (Ref{Ptr{Cvoid}}, Cstring, UInt16, ), out, ip, port);
-autd_link_remote_soem_server_ip_addr(remote_soem, server_ip_addr) = ccall((:AUTDLinkRemoteSOEMServerIpAddr, _dll), Cvoid, (Ptr{Cvoid}, Cstring, ), remote_soem, server_ip_addr);
-autd_link_remote_soem_client_ams_net_id(remote_soem, client_ams_net_id) = ccall((:AUTDLinkRemoteSOEMClientAmsNetId, _dll), Cvoid, (Ptr{Cvoid}, Cstring, ), remote_soem, client_ams_net_id);
 autd_link_remote_soem_log_level(remote_soem, level) = ccall((:AUTDLinkRemoteSOEMLogLevel, _dll), Cvoid, (Ptr{Cvoid}, Int32, ), remote_soem, level);
 autd_link_remote_soem_log_func(remote_soem, out_func, flush_func) = ccall((:AUTDLinkRemoteSOEMLogFunc, _dll), Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}, Ptr{Cvoid}, ), remote_soem, out_func, flush_func);
 autd_link_remote_soem_timeout(remote_soem, timeout_ns) = ccall((:AUTDLinkRemoteSOEMTimeout, _dll), Cvoid, (Ptr{Cvoid}, UInt64, ), remote_soem, timeout_ns);
