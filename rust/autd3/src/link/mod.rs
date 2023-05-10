f857a6d367955867538c8b3e3f3e8a106a155f21
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-10 11:18:37 +0900

    add debug link

---
+++ rust/autd3/src/link/mod.rs
@@ -0,0 +1,16 @@
+/*
+ * File: mod.rs
+ * Project: link
+ * Created Date: 09/05/2023
+ * Author: Shun Suzuki
+ * -----
+ * Last Modified: 09/05/2023
+ * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+ * -----
+ * Copyright (c) 2023 Shun Suzuki. All rights reserved.
+ *
+ */
+
+pub mod debug;
+
+pub use debug::Debug;
