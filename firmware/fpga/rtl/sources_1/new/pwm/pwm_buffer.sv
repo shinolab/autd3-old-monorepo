19507c80b27f10f77d83e3b6733c86b49821a051
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-15 13:06:46 +0900

    update pwm module

--- firmware/fpga/rtl/sources_1/new/pwm/pwm_buffer.sv
+++ firmware/fpga/rtl/sources_1/new/pwm/pwm_buffer.sv
@@ -1,43 +1,41 @@
 /*
- * File: generator.sv
+ * File: pwm_buffer.sv
  * Project: pwm
  * Created Date: 15/03/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 01/03/2023
+ * Last Modified: 15/05/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
- * 
+ *
  */
 
-
 `timescale 1ns / 1ps
-module pwm_generator #(
+module pwm_buffer #(
     parameter int WIDTH = 13
 ) (
     input var CLK,
+    input var [WIDTH-1:0] CYCLE_M1,
     input var [WIDTH-1:0] TIME_CNT,
-    input var [WIDTH-1:0] RISE,
-    input var [WIDTH-1:0] FALL,
-    output var PWM_OUT
+    input var [WIDTH-1:0] RISE_IN,
+    input var [WIDTH-1:0] FALL_IN,
+    output var [WIDTH-1:0] RISE_OUT,
+    output var [WIDTH-1:0] FALL_OUT
 );
 
   bit [WIDTH-1:0] t;
   bit [WIDTH-1:0] R;
   bit [WIDTH-1:0] F;
-  bit v;
 
   assign t = TIME_CNT;
-  assign R = RISE;
-  assign F = FALL;
-  assign PWM_OUT = v;
+  assign RISE_OUT = R;
+  assign FALL_OUT = F;
 
   always_ff @(posedge CLK) begin
-    if (R <= F) begin
-      v <= (R <= t) & (t < F);
-    end else begin
-      v <= (t < F) | (R <= t);
+    if (t == CYCLE_M1) begin
+      R <= RISE_IN;
+      F <= FALL_IN;
     end
   end
 
