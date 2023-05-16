64bbb552e7d8387bcff77ae3879d5ef7814bd707
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-16 11:01:40 +0900

    update modulator

--- firmware/fpga/rtl/sources_1/new/modulation/modulation_bus_if.sv
+++ firmware/fpga/rtl/sources_1/new/modulation/modulation_bus_if.sv
@@ -1,18 +1,18 @@
 /*
- * File: ms_bus_if.sv
+ * File: modulation_bus_if.sv
  * Project: modulation
  * Created Date: 24/03/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 28/07/2022
+ * Last Modified: 15/05/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
- * Copyright (c) 2022 Shun Suzuki. All rights reserved.
- * 
+ * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
+ *
  */
 
 `timescale 1ns / 1ps
-interface ms_bus_if ();
+interface modulation_bus_if ();
 
   bit [15:0] ADDR;
   bit [ 7:0] M;
