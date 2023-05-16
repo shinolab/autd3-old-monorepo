ba8534ce5c1e0db31f71237147e003fc565d6092
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-16 12:58:14 +0900

    update preconditioner

--- firmware/fpga/rtl/sim_1/new/pwm/sim_pwm_preconditioner.sv
+++ firmware/fpga/rtl/sim_1/new/pwm/sim_pwm_preconditioner.sv
@@ -4,7 +4,7 @@
  * Created Date: 15/03/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 15/05/2023
+ * Last Modified: 16/05/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
@@ -28,13 +28,16 @@ module sim_pwm_preconditioner ();
   localparam int DEPTH = 249;
   localparam int CYCLE = 4096;
 
-  bit [WIDTH-1:0] cycle[DEPTH];
-  bit [WIDTH-1:0] duty [DEPTH];
-  bit [WIDTH-1:0] phase[DEPTH];
+  bit [WIDTH-1:0] cycle [DEPTH];
+  bit [WIDTH-1:0] duty;
+  bit [WIDTH-1:0] phase;
 
-  bit [WIDTH-1:0] rise [DEPTH];
-  bit [WIDTH-1:0] fall [DEPTH];
+  bit [WIDTH-1:0] rise  [DEPTH];
+  bit [WIDTH-1:0] fall  [DEPTH];
   bit din_valid, dout_valid;
+
+  bit [WIDTH-1:0] duty_buf [DEPTH];
+  bit [WIDTH-1:0] phase_buf[DEPTH];
 
   pwm_preconditioner #(
       .WIDTH(WIDTH),
@@ -50,49 +53,38 @@ module sim_pwm_preconditioner ();
       .DOUT_VALID(dout_valid)
   );
 
-  task automatic wait_calc();
-    @(posedge CLK_20P48M);
-    din_valid = 1;
-
-    @(posedge CLK_20P48M);
-    din_valid = 0;
-
-    while (1) begin
-      @(posedge CLK_20P48M);
-      if (dout_valid) begin
-        break;
-      end
-    end
-
-    @(posedge CLK_20P48M);
-  endtask
-
   task automatic set(int idx, bit [WIDTH-1:0] c, bit [WIDTH-1:0] d, bit [WIDTH-1:0] p);
-    @(posedge CLK_20P48M);
-    cycle[idx] = c;
-    duty[idx]  = d;
-    phase[idx] = p;
-
-    wait_calc();
-
-    if ((rise[idx] != ((cycle[idx]-p-d/2+cycle[idx])%cycle[idx]))
-        & (fall[idx] == ((cycle[idx]-p+(d+1)/2)%cycle[idx]))) begin
-      $error("Failed at idx=%d, d=%d, p=%d, R=%d, F=%d", idx, d, p, rise[idx], fall[idx]);
-      $finish();
+    for (int i = 0; i < DEPTH; i++) begin
+      @(posedge CLK_20P48M);
+      din_valid = 1'b1;
+      if (i == idx) begin
+        duty  = d;
+        phase = p;
+      end else begin
+        duty  = 0;
+        phase = 0;
+      end
+      duty_buf[i]  = duty;
+      phase_buf[i] = phase;
     end
+    @(posedge CLK_20P48M);
+    din_valid = 1'b0;
   endtask
 
   task automatic set_random();
-    @(posedge CLK_20P48M);
-    din_valid = 1;
-    for (int j = 0; j < DEPTH; j++) begin
-      cycle[j] = sim_helper_random.range(8000, 2000);
-      duty[j]  = sim_helper_random.range(cycle[j], 0);
-      phase[j] = sim_helper_random.range(cycle[j], 0);
+    for (int i = 0; i < DEPTH; i++) begin
       @(posedge CLK_20P48M);
-      din_valid = 0;
+      din_valid = 1'b1;
+      duty = sim_helper_random.range(8000, 0);
+      phase = sim_helper_random.range(8000, 0);
+      duty_buf[i] = duty;
+      phase_buf[i] = phase;
     end
+    @(posedge CLK_20P48M);
+    din_valid = 1'b0;
+  endtask
 
+  task automatic check();
     while (1) begin
       @(posedge CLK_20P48M);
       if (dout_valid) begin
@@ -100,11 +92,10 @@ module sim_pwm_preconditioner ();
       end
     end
 
-    for (int j = 0; j < DEPTH; j++) begin
-      if ((rise[j] != ((cycle[j]-phase[j]-duty[j]/2+cycle[j])%cycle[j]))
-          | (fall[j] != ((cycle[j]-phase[j]+(duty[j]+1)/2)%cycle[j]))) begin
-        $error("Failed at %d: T=%d, d=%d, p=%d, R=%d, F=%d", j, cycle[j], duty[j], phase[j],
-               rise[j], fall[j]);
+    for (int i = 0; i < DEPTH; i++) begin
+      if ((rise[i] != ((cycle[i]-phase[i]-duty[i]/2+cycle[i])%cycle[i]))
+        & (fall[i] == ((cycle[i]-phase[i]+(duty[i]+1)/2)%cycle[i]))) begin
+        $error("Failed at idx=%d, d=%d, p=%d, R=%d, F=%d", i, duty[i], phase[i], rise[i], fall[i]);
         $finish();
       end
     end
@@ -112,24 +103,57 @@ module sim_pwm_preconditioner ();
 
   initial begin
     cycle = '{DEPTH{0}};
-    duty  = '{DEPTH{0}};
-    phase = '{DEPTH{0}};
+
     @(posedge locked);
 
-    set(0, CYCLE, CYCLE / 2, CYCLE / 2);  // normal, D=CYCLE/2
-    set(0, CYCLE, CYCLE, CYCLE / 2);  // normal, D=CYCLE
-    set(0, CYCLE, 0, CYCLE / 2);  // normal, D=0
-    set(0, CYCLE, CYCLE / 2, CYCLE / 2 - CYCLE / 4);  // normal, D=CYCLE/2, left edge
-    set(0, CYCLE, CYCLE / 2, CYCLE / 2 + CYCLE / 4);  // normal, D=CYCLE/2, right edge
-    set(0, CYCLE, CYCLE / 2, 0);  // over, D=CYCLE/2
-    set(0, CYCLE, CYCLE / 2, CYCLE);  // over, D=CYCLE/2
-    set(0, CYCLE, 0, CYCLE);  // over, D=0
+    fork
+      set(0, CYCLE, CYCLE / 2, CYCLE / 2);  // normal, D=CYCLE/2
+      check();
+    join
+
+    fork
+      set(0, CYCLE, CYCLE, CYCLE / 2);  // normal, D=CYCLE
+      check();
+    join
+
+    fork
+      set(0, CYCLE, 0, CYCLE / 2);  // normal, D=0
+      check();
+    join
+
+    fork
+      set(0, CYCLE, CYCLE / 2, CYCLE / 2 - CYCLE / 4);  // normal, D=CYCLE/2, left edge
+      check();
+    join
+
+    fork
+      set(0, CYCLE, CYCLE / 2, CYCLE / 2 + CYCLE / 4);  // normal, D=CYCLE/2, right edge
+      check();
+    join
+
+    fork
+      set(0, CYCLE, CYCLE / 2, 0);  // over, D=CYCLE/2
+      check();
+    join
+
+    fork
+      set(0, CYCLE, CYCLE / 2, CYCLE);  // over, D=CYCLE/2
+      check();
+    join
+
+    fork
+      set(0, CYCLE, 0, CYCLE);  // over, D=0
+      check();
+    join
 
     // at random
     sim_helper_random.init();
     for (int i = 0; i < 5000; i++) begin
       $display("check start @%d", i);
-      set_random();
+      fork
+        set_random();
+        check();
+      join
       $display("check finish @%d", i);
     end
 
