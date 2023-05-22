4c1c51266dc270dab53cb53b0352f922442c5bb1
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-22 11:13:58 +0900

    add remote twincat

--- src/autd3-link-twincat/src/lib.rs
+++ src/autd3-link-twincat/src/lib.rs
@@ -4,7 +4,7 @@
  * Created Date: 27/05/2021
  * Author: Shun Suzuki
  * -----
- * Last Modified: 31/05/2022
+ * Last Modified: 22/05/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2021 Shun Suzuki. All rights reserved.
@@ -12,7 +12,14 @@
  */
 
 mod error;
-mod native_methods;
-mod twincat_link;
 
-pub use twincat_link::TwinCAT;
+#[cfg(feature = "local")]
+mod local;
+#[cfg(feature = "remote")]
+mod remote;
+
+#[cfg(feature = "local")]
+pub use local::TwinCAT;
+
+#[cfg(feature = "remote")]
+pub use remote::RemoteTwinCAT;
