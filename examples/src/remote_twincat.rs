beb140fe1edfa967d66f99b3484e15c106934a5c
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-22 11:14:04 +0900

    update example

---
+++ examples/src/remote_twincat.rs
@@ -0,0 +1,36 @@
+/*
+ * File: remote_twincat.rs
+ * Project: src
+ * Created Date: 22/05/2023
+ * Author: Shun Suzuki
+ * -----
+ * Last Modified: 22/05/2023
+ * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+ * -----
+ * Copyright (c) 2023 Shun Suzuki. All rights reserved.
+ *
+ */
+
+mod test_runner;
+mod tests;
+
+use anyhow::Result;
+
+use autd3::prelude::*;
+use autd3_link_twincat::RemoteTwinCAT;
+
+fn main() -> Result<()> {
+    let geometry = Geometry::builder()
+        .add_device(AUTD3::new(Vector3::zeros(), Vector3::zeros()))
+        .build()?;
+
+    let link = RemoteTwinCAT::builder()
+        .server_ams_net_id("0.0.0.0.0.0")
+        .build()?;
+
+    let autd = Controller::open(geometry, link).expect("Failed to open");
+
+    run!(autd);
+
+    Ok(())
+}
