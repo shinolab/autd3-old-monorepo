b0cc39df7bc6f79328d9e4b3266b87321fa13798
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-24 11:31:55 +0900

    lint

--- examples/src/tests/audio_file.rs
+++ examples/src/tests/audio_file.rs
@@ -21,7 +21,7 @@ pub fn audio_file<T: Transducer, L: Link<T>>(
     let center = autd.geometry().center() + Vector3::new(0., 0., 150.0);
 
     let g = Focus::new(center);
-    const WAV_FILE: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/resources/sin150.wav");
+    const WAV_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/resources/sin150.wav");
     let m = autd3_modulation_audio_file::Wav::new(WAV_FILE)?;
 
     autd.send((m, g))
