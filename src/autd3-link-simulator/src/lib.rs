2e865d42765e830445699d788e012ea0a11a7af3
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-24 10:58:17 +0900

    update simulator link

--- src/autd3-link-simulator/src/lib.rs
+++ src/autd3-link-simulator/src/lib.rs
@@ -4,14 +4,13 @@
  * Created Date: 09/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 19/05/2023
+ * Last Modified: 23/05/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
  *
  */
 
-mod native_methods;
 mod simulator_link;
 
 pub use simulator_link::Simulator;
