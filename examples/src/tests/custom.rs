e28090f2d3bfd0acb35fc2a41b64d1ba2fefcfac
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-27 13:01:05 +0900

    update examples

--- examples/src/tests/custom.rs
+++ examples/src/tests/custom.rs
@@ -4,7 +4,7 @@
  * Created Date: 24/05/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 24/05/2023
+ * Last Modified: 27/05/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -65,7 +65,7 @@ impl Modulation for Burst {
 pub fn custom<T: Transducer, L: Link<T>>(
     autd: &mut Controller<T, L>,
 ) -> anyhow::Result<bool, AUTDError> {
-    autd.send(SilencerConfig::default())?;
+    autd.send(SilencerConfig::none())?;
 
     let g = Uniform::new();
     let m = Burst::new();
