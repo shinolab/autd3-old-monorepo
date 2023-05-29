681303bef20090b5e4bd06f61ecae9e6ba3611fe
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-29 18:39:37 +0900

    update cs

--- dotnet/unity/Assets/autd3/Scripts/NativeMethods/GeometryViewer.cs
+++ dotnet/unity/Assets/autd3/Scripts/NativeMethods/GeometryViewer.cs
@@ -16,7 +16,7 @@ namespace AUTD3Sharp
 
             [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)] public static extern IntPtr AUTDGeometryViewerVsync(IntPtr viewer, [MarshalAs(UnmanagedType.U1)] bool vsync);
 
-            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)] public static extern int AUTDGeometryViewerRun(IntPtr viewer, IntPtr cnt);
+            [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)] public static extern int AUTDGeometryViewerRun(IntPtr viewer, IntPtr geometry);
         }
     }
 }
