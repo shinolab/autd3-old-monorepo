1ecc66f88d80d5b874b107f8d9388033d915d418
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-30 15:09:24 +0900

    add release action

--- cpp/build.ps1
+++ cpp/build.ps1
@@ -3,7 +3,7 @@
 # Created Date: 28/05/2023
 # Author: Shun Suzuki
 # -----
-# Last Modified: 29/05/2023
+# Last Modified: 30/05/2023
 # Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 # -----
 # Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -16,8 +16,12 @@ cd capi
 cargo build --release --all
 cd ..
 mkdir cpp/lib > NUL 2>&1
+mkdir cpp/bin > NUL 2>&1
 foreach($dll in Get-ChildItem -Path capi/target/release | Where {$_.FullName -match "^(?!.*\.dll).*\.lib$"}){
     Copy-Item -Path $dll -Destination cpp/lib
+}
+foreach($dll in Get-ChildItem -Path capi/target/release | Where {$_.FullName -match ".*\.dll$"}){
+    Copy-Item -Path $dll -Destination cpp/bin
 }
 Copy-Item -Path ./src/autd3-link-soem/3rdparty/SOEM/oshw/win32/wpcap/Lib/x64  -Destination ./cpp/lib/wpcap -Recurse -Force
 popd
