fd1f49eb4f0321e75aa33a60bb1599e6b5d2cf2f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-30 15:09:24 +0900

    add release action

--- cpp/build.sh
+++ cpp/build.sh
@@ -5,19 +5,19 @@ script_dir="$( dirname -- "$( readlink -f -- "$0"; )"; )"
 
 pushd $script_dir
 cd ..
-mkdir -p cpp/lib
+mkdir -p cpp/bin
 cd capi
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   cargo build --release --all
   cd ..
-  cp ./capi/target/release/*.so cpp/lib
+  cp ./capi/target/release/*.so cpp/bin
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   cargo build --release --all --target=x86_64-apple-darwin
   cargo build --release --all --target=aarch64-apple-darwin
   cd ..
   for x64_file in `ls ./capi/target/x86_64-apple-darwin/release/*.dylib`; do
     file_basename=`basename $x64_file`
-    lipo -create $x64_file ./capi/target/aarch64-apple-darwin/release/$file_basename -output cpp/lib/$file_basename
+    lipo -create $x64_file ./capi/target/aarch64-apple-darwin/release/$file_basename -output cpp/bin/$file_basename
   done
 fi
 
