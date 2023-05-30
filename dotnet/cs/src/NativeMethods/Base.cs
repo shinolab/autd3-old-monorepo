e066923226b4d8e5ac0a288480c4617f7352579c
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-30 10:51:22 +0900

    update wrapper

--- dotnet/cs/src/NativeMethods/Base.cs
+++ dotnet/cs/src/NativeMethods/Base.cs
@@ -2,6 +2,10 @@
 using System;
 using System.Runtime.InteropServices;
 
+#if UNITY_2020_2_OR_NEWER
+#nullable enable
+#endif
+
 namespace AUTD3Sharp
 {
     namespace NativeMethods
@@ -203,4 +207,11 @@ namespace AUTD3Sharp
             [DllImport(DLL, CallingConvention = CallingConvention.Cdecl)] public static extern IntPtr AUTDLinkDebugBuild(IntPtr builder);
         }
     }
+
 }
+
+#if UNITY_2020_2_OR_NEWER
+#nullable disable
+#endif
+
+
