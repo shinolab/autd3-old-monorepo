c265fbc1ae441c8018f735e7b699cf4b2617cb17
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-05-31 10:48:35 +0900

    fix #65

--- dotnet/fs/example/GeometryViewer/Program.fs
+++ dotnet/fs/example/GeometryViewer/Program.fs
@@ -13,9 +13,8 @@ open AUTD3Sharp
 open AUTD3Sharp.Extra
 open AUTD3Sharp.Utils
 
-Geometry.Builder()
-        .AddDevice(Vector3d.zero, Vector3d.zero)
-        .AddDevice(Vector3d(0, 0, AUTD3.DeviceWidth), Vector3d(0, AUTD3.Pi / 2.0, 0))
-        .AddDevice(Vector3d(AUTD3.DeviceWidth, 0, AUTD3.DeviceWidth), Vector3d(0, AUTD3.Pi, 0))
-        .AddDevice(Vector3d(AUTD3.DeviceWidth, 0, 0), Vector3d(0, -AUTD3.Pi / 2.0, 0))
-        .Build() |> GeometryViewer().WindowSize(800, 600).Vsync(true).View
+Geometry.Builder().AddDevice(Vector3d.zero, Vector3d.zero)
+                    .AddDevice(Vector3d(0, 0, AUTD3.DeviceWidth), Vector3d(0, AUTD3.Pi / 2.0, 0))
+                    .AddDevice(Vector3d(AUTD3.DeviceWidth, 0, AUTD3.DeviceWidth), Vector3d(0, AUTD3.Pi, 0))
+                    .AddDevice(Vector3d(AUTD3.DeviceWidth, 0, 0), Vector3d(0, -AUTD3.Pi / 2.0, 0))
+                    .Build() |> GeometryViewer().WindowSize(800u, 600u).Vsync(true).Run |> exit
