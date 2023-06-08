218b93fb7e60b14bc73ea267b9733d75cdab812a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-06-08 13:56:25 +0900

    close #67

---
+++ src/autd3-backend-cuda/build.rs
@@ -0,0 +1,54 @@
+/*
+ * File: build.rs
+ * Project: autd3-backend-cuda
+ * Created Date: 06/06/2023
+ * Author: Shun Suzuki
+ * -----
+ * Last Modified: 08/06/2023
+ * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+ * -----
+ * Copyright (c) 2023 Shun Suzuki. All rights reserved.
+ *
+ */
+
+use cuda_config::*;
+
+fn main() {
+    let mut build = cc::Build::new();
+
+    #[cfg(feature = "single_float")]
+    build.define("AUTD3_USE_SINGLE_FLOAT", "1");
+
+    build
+        .cuda(true)
+        .flag("-cudart=shared")
+        .flag("-arch=sm_52")
+        .flag("-gencode=arch=compute_52,code=sm_52")
+        .flag("-gencode=arch=compute_60,code=sm_60")
+        .flag("-gencode=arch=compute_61,code=sm_61")
+        .flag("-gencode=arch=compute_70,code=sm_70")
+        .flag("-gencode=arch=compute_75,code=sm_75")
+        .flag("-gencode=arch=compute_80,code=sm_80")
+        .flag("-gencode=arch=compute_86,code=sm_86")
+        .flag("-gencode=arch=compute_87,code=sm_87")
+        .flag("-gencode=arch=compute_90,code=sm_90")
+        .flag("-gencode=arch=compute_90,code=compute_90")
+        .file("cuda_src/kernel.cu")
+        .compile("autd3_cuda_kernel");
+
+    if cfg!(target_os = "windows") {
+        println!(
+            "cargo:rustc-link-search=native={}",
+            find_cuda_windows().display()
+        );
+    } else {
+        for path in find_cuda() {
+            println!("cargo:rustc-link-search=native={}", path.display());
+        }
+    };
+
+    println!("cargo:rerun-if-changed=cuda_src/kernel.cu");
+    println!("cargo:rustc-link-lib=dylib=cusolver");
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=CUDA_LIBRARY_PATH");
+}
--- src/autd3-gain-holo/src/error.rs
+++ src/autd3-gain-holo/src/error.rs
@@ -4,7 +4,7 @@
  * Created Date: 29/05/2021
  * Author: Shun Suzuki
  * -----
- * Last Modified: 11/05/2023
+ * Last Modified: 06/06/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2021 Shun Suzuki. All rights reserved.
@@ -18,6 +18,8 @@ use thiserror::Error;
 pub enum HoloError {
     #[error("Failed to solve linear system")]
     SolveFailed,
+    #[error("{0}")]
+    BackendError(String),
 }
 
 impl From<HoloError> for AUTDInternalError {
