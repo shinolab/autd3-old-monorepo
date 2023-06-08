5bb0859fc13f576ca9adde2776b37e1bb52a8a29
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-06-08 13:56:25 +0900

    close #67

--- src/autd3-gain-holo/src/lib.rs
+++ src/autd3-gain-holo/src/lib.rs
@@ -4,7 +4,7 @@
  * Created Date: 28/05/2021
  * Author: Shun Suzuki
  * -----
- * Last Modified: 02/06/2023
+ * Last Modified: 07/06/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2021 Shun Suzuki. All rights reserved.
@@ -12,6 +12,7 @@
  */
 
 mod backend;
+mod backend_nalgebra;
 mod combinatorial;
 mod constraint;
 mod error;
@@ -22,8 +23,10 @@ mod matrix;
 mod nls;
 
 pub use backend::*;
+pub use backend_nalgebra::NalgebraBackend;
 pub use combinatorial::*;
 pub use constraint::*;
+pub use error::HoloError;
 pub use linear_synthesis::*;
 pub use matrix::*;
 pub use nls::*;
