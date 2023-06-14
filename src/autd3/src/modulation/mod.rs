b7a861ea88df70891cf5f1b3d0dc62dea50d9c2d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-06-15 05:52:54 +0900

    add transform mod

--- src/autd3/src/modulation/mod.rs
+++ src/autd3/src/modulation/mod.rs
@@ -4,7 +4,7 @@
  * Created Date: 31/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 14/06/2023
+ * Last Modified: 15/06/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
@@ -18,6 +18,7 @@ pub mod sine_legacy;
 pub mod sine_pressure;
 pub mod square;
 pub mod r#static;
+pub mod transform;
 
 pub use cache::Cache;
 pub use fir::FIR;
@@ -26,3 +27,4 @@ pub use sine::Sine;
 pub use sine_legacy::SineLegacy;
 pub use sine_pressure::SinePressure;
 pub use square::Square;
+pub use transform::Transform;
