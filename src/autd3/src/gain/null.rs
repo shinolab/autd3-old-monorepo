5a87a1d0cb77ec9a371c14530c64b05b6c79ed07
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-06-20 01:40:53 +0900

    add tests for gain

--- src/autd3/src/gain/null.rs
+++ src/autd3/src/gain/null.rs
@@ -4,7 +4,7 @@
  * Created Date: 01/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 20/05/2023
+ * Last Modified: 19/06/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
@@ -34,5 +34,32 @@ impl Null {
 impl<T: Transducer> Gain<T> for Null {
     fn calc(&mut self, geometry: &Geometry<T>) -> Result<Vec<Drive>, AUTDInternalError> {
         Ok(Self::transform(geometry, |_| Drive { phase: 0., amp: 0. }))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use autd3_core::autd3_device::AUTD3;
+    use autd3_core::geometry::{LegacyTransducer, Vector3};
+
+    use super::*;
+
+    use crate::tests::GeometryBuilder;
+
+    #[test]
+    fn test_null() {
+        let geometry = GeometryBuilder::<LegacyTransducer>::new()
+            .add_device(AUTD3::new(Vector3::zeros(), Vector3::zeros()))
+            .build()
+            .unwrap();
+
+        let mut null_gain = Null::new();
+
+        let drives = null_gain.calc(&geometry).unwrap();
+
+        for drive in drives {
+            assert_eq!(drive.phase, 0.0);
+            assert_eq!(drive.amp, 0.0);
+        }
     }
 }
