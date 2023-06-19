102113c154134f6965d89cb381b9e68697c0d0d8
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-06-20 01:29:55 +0900

    rn impl trait

--- src/autd3/src/modulation/transform.rs
+++ src/autd3/src/modulation/transform.rs
@@ -4,7 +4,7 @@
  * Created Date: 15/06/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 15/06/2023
+ * Last Modified: 20/06/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -15,19 +15,19 @@ use autd3_core::{error::AUTDInternalError, float, modulation::Modulation};
 use autd3_traits::Modulation;
 
 #[derive(Modulation)]
-pub struct ModulationTransform<M: Modulation, F: Fn(usize, &float) -> float> {
+pub struct TransformImpl<M: Modulation, F: Fn(usize, &float) -> float> {
     m: M,
     freq_div: u32,
     f: F,
 }
 
 pub trait Transform<M: Modulation> {
-    fn with_transform<F: Fn(usize, &float) -> float>(self, f: F) -> ModulationTransform<M, F>;
+    fn with_transform<F: Fn(usize, &float) -> float>(self, f: F) -> TransformImpl<M, F>;
 }
 
 impl<M: Modulation> Transform<M> for M {
-    fn with_transform<F: Fn(usize, &float) -> float>(self, f: F) -> ModulationTransform<M, F> {
-        ModulationTransform {
+    fn with_transform<F: Fn(usize, &float) -> float>(self, f: F) -> TransformImpl<M, F> {
+        TransformImpl {
             freq_div: self.sampling_frequency_division(),
             f,
             m: self,
@@ -35,7 +35,7 @@ impl<M: Modulation> Transform<M> for M {
     }
 }
 
-impl<M: Modulation, F: Fn(usize, &float) -> float> Modulation for ModulationTransform<M, F> {
+impl<M: Modulation, F: Fn(usize, &float) -> float> Modulation for TransformImpl<M, F> {
     fn calc(&mut self) -> Result<Vec<float>, AUTDInternalError> {
         let m = self.m.calc()?;
         Ok(m.iter().enumerate().map(|(i, x)| (self.f)(i, x)).collect())
