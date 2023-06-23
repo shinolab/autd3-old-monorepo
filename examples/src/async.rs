2c3ff48123b43fbf9cb8147988d72ce876e73fb7
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-06-23 12:02:28 +0900

    add send_async

---
+++ examples/src/async.rs
@@ -0,0 +1,36 @@
+/*
+ * File: async.rs
+ * Project: src
+ * Created Date: 23/06/2023
+ * Author: Shun Suzuki
+ * -----
+ * Last Modified: 23/06/2023
+ * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+ * -----
+ * Copyright (c) 2023 Shun Suzuki. All rights reserved.
+ *
+ */
+
+use anyhow::Result;
+
+use autd3::link::Debug;
+use autd3::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut autd = Controller::builder()
+        .add_device(AUTD3::new(Vector3::zeros(), Vector3::zeros()))
+        .open_with(Debug::new())?;
+
+    autd.send_async(Clear::new()).await?;
+    autd.send_async(Synchronize::new()).await?;
+
+    let center = autd.geometry().center() + Vector3::new(0., 0., 150.0 * MILLIMETER);
+
+    let g = Focus::new(center);
+    let m = Square::new(150);
+
+    autd.send_async((m, g)).await?;
+
+    Ok(())
+}
