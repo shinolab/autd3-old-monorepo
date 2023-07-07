25f97c46f885de3083d0ca241ca4a0512ea90711
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-07 16:54:12 +0900

    start server develop

---
+++ server/src/components/svelte/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import LeftPanel from "./LeftPanel.svelte";
+	import RightPanel from "./RightPanel.svelte";
+</script>
+
+<div>
+	<LeftPanel />
+	<RightPanel />
+</div>
+
+<style>
+	div {
+		display: flex;
+		width: 100%;
+		align-items: flex-start;
+		gap: 10px;
+		flex-shrink: 0;
+
+		padding: 10px;
+
+		height: 100vh;
+		box-sizing: border-box;
+	}
+</style>
---
+++ server/src/components/svelte/UI/utils/IpInput.svelte
@@ -0,0 +1,35 @@
+<!--
+File: IpInput.svelte
+Project: AUTD server
+Created Date: 06/07/2023
+Author: Shun Suzuki
+-----
+Last Modified: 06/07/2023
+Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+-----
+Copyright (c) 2023 Shun Suzuki. All rights reserved.
+
+-->
+
+<script>
+    export let id = "";
+    export let value = "";
+</script>
+
+<input
+    {id}
+    type="text"
+    pattern="\d{(1, 3)}\.\d{(1, 3)}\.\d{(1, 3)}\.\d{(1, 3)}"
+    placeholder="127.0.0.1"
+    required
+    bind:value
+/>
+
+<style>
+    input {
+        border-radius: 3px;
+        border: 1px solid var(--color-border-interactive-muted, #2b659b);
+        background: var(--color-background-base-default, #101923);
+        color: var(--color-text-base-default, #ffffff);
+    }
+</style>
