5bdbec565399eab9c3f6820dbdf611d828ea33ca
Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-10 11:01:34 +0900

    fix mac universal build

--- server/src-tauri/build.rs
+++ server/src-tauri/build.rs
@@ -26,49 +26,128 @@ fn main() {
         manifest_dir.join("../SOEMAUTDServer").display()
     );
 
-    Command::new("cargo")
-        .current_dir(manifest_dir.join("../simulator"))
-        .args(["build", "--release"])
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-
-    Command::new("cargo")
-        .current_dir(manifest_dir.join("../SOEMAUTDServer"))
-        .args(["build", "--release"])
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-
     let ext = if cfg!(target_os = "windows") {
         ".exe"
     } else {
         ""
     };
 
-    std::fs::copy(
-        manifest_dir.join(format!("../simulator/target/release/simulator{}", ext)),
-        manifest_dir.join(format!(
-            "simulator-{}{}",
-            std::env::var("TARGET").unwrap(),
-            ext
-        )),
-    )
-    .unwrap();
-    std::fs::copy(
-        manifest_dir.join(format!(
-            "../SOEMAUTDServer/target/release/SOEMAUTDServer{}",
-            ext
-        )),
-        manifest_dir.join(format!(
-            "SOEMAUTDServer-{}{}",
-            std::env::var("TARGET").unwrap(),
-            ext
-        )),
-    )
-    .unwrap();
+    if cfg!(target_os = "macos") {
+        Command::new("cargo")
+            .current_dir(manifest_dir.join("../simulator"))
+            .args(["build", "--release", "--target=x86_64-apple-darwin"])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+        Command::new("cargo")
+            .current_dir(manifest_dir.join("../simulator"))
+            .args(["build", "--release", "--target=aarch64-apple-darwin"])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+        Command::new("cargo")
+            .current_dir(manifest_dir.join("../SOEMAUTDServer"))
+            .args(["build", "--release", "--target=x86_64-apple-darwin"])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+        Command::new("cargo")
+            .current_dir(manifest_dir.join("../SOEMAUTDServer"))
+            .args(["build", "--release", "--target=aarch64-apple-darwin"])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+
+        std::fs::copy(
+            manifest_dir.join("../simulator/target/x86_64-apple-darwin/release/simulator"),
+            manifest_dir.join("simulator-x86_64-apple-darwin"),
+        )
+        .unwrap();
+        std::fs::copy(
+            manifest_dir.join("../simulator/target/aarch64-apple-darwin/release/simulator"),
+            manifest_dir.join("simulator-aarch64-apple-darwin"),
+        )
+        .unwrap();
+        Command::new("lipo")
+            .current_dir(manifest_dir)
+            .args([
+                "-create",
+                "simulator-x86_64-apple-darwin",
+                "simulator-aarch64-apple-darwin",
+                "-output",
+                "simulator-universal-apple-darwin",
+            ])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+        std::fs::copy(
+            manifest_dir
+                .join("../SOEMAUTDServer/target/x86_64-apple-darwin/release/SOEMAUTDServer"),
+            manifest_dir.join("SOEMAUTDServer-x86_64-apple-darwin"),
+        )
+        .unwrap();
+        std::fs::copy(
+            manifest_dir
+                .join("../SOEMAUTDServer/target/aarch64-apple-darwin/release/SOEMAUTDServer"),
+            manifest_dir.join("SOEMAUTDServer-aarch64-apple-darwin"),
+        )
+        .unwrap();
+        Command::new("lipo")
+            .current_dir(manifest_dir)
+            .args([
+                "-create",
+                "SOEMAUTDServer-x86_64-apple-darwin",
+                "SOEMAUTDServer-aarch64-apple-darwin",
+                "-output",
+                "SOEMAUTDServer-universal-apple-darwin",
+            ])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+    } else {
+        Command::new("cargo")
+            .current_dir(manifest_dir.join("../simulator"))
+            .args(["build", "--release"])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+        Command::new("cargo")
+            .current_dir(manifest_dir.join("../SOEMAUTDServer"))
+            .args(["build", "--release"])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+
+        std::fs::copy(
+            manifest_dir.join(format!("../simulator/target/release/simulator{}", ext)),
+            manifest_dir.join(format!(
+                "simulator-{}{}",
+                std::env::var("TARGET").unwrap(),
+                ext
+            )),
+        )
+        .unwrap();
+        std::fs::copy(
+            manifest_dir.join(format!(
+                "../SOEMAUTDServer/target/release/SOEMAUTDServer{}",
+                ext
+            )),
+            manifest_dir.join(format!(
+                "SOEMAUTDServer-{}{}",
+                std::env::var("TARGET").unwrap(),
+                ext
+            )),
+        )
+        .unwrap();
+    };
 
     tauri_build::build()
 }
