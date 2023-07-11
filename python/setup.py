e406d7f4e06cdf15d931e137d55822fa1dc2309a
Mingxin Zhang <m.zhang@hapis.k.u-tokyo.ac.jp>
2023-07-12 03:10:35 +0900

    #93 and #94

--- python/setup.py
+++ python/setup.py
@@ -19,10 +19,11 @@ import tarfile
 import os
 import sys
 import itertools
+import platform
 
 
 def _get_version():
-    with open("README.md", "r") as f:
+    with open("README.md", "r", encoding='utf-8') as f:
         for line in f.readlines():
             if line.startswith("version: "):
                 return line.replace("version: ", "").strip()
@@ -41,7 +42,7 @@ def _set_package_version(version):
         f.write(init_py)
 
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 _set_package_version(_get_version())
@@ -66,40 +67,41 @@ def download_bin():
     asset_base_url = "https://github.com/shinolab/autd3/releases/download/"
     version = f"v{_VERSION_TRIPLE}"
 
-    targets = [
-        Target("win", "x64", "dll", "zip"),
-        Target("macos", "universal", "dylib", "tar.gz"),
-        Target("linux", "x64", "so", "tar.gz"),
-    ]
+    system_name = platform.system()
+    if system_name == 'Windows':
+        target = Target("win", "x64", "dll", "zip")
+    elif system_name == 'Darwin':
+        target = Target("macos", "universal", "dylib", "tar.gz")
+    elif system_name == 'Linux':
+        target = Target("linux", "x64", "so", "tar.gz")
 
-    for target in targets:
-        url = f"{asset_base_url}{version}/autd3-{version}-{target.os_name}-{target.arch}.{target.archive_ext}"
+    url = f"{asset_base_url}{version}/autd3-{version}-{target.os_name}-{target.arch}.{target.archive_ext}"
 
-        tmp_archive_path = f"tmp.{target.archive_ext}"
+    tmp_archive_path = f"tmp.{target.archive_ext}"
 
-        res = requests.get(url, stream=True)
-        with open(tmp_archive_path, "wb") as fp:
-            shutil.copyfileobj(res.raw, fp)
+    res = requests.get(url, stream=True)
+    with open(tmp_archive_path, "wb") as fp:
+        shutil.copyfileobj(res.raw, fp)
 
-        if target.archive_ext == "zip":
-            with zipfile.ZipFile(tmp_archive_path) as f:
-                for info in f.infolist():
-                    if info.filename.startswith("bin") and info.filename.endswith(
-                        target.bin_ext
-                    ):
-                        f.extract(info, ".")
-        elif target.archive_ext == "tar.gz":
-            with tarfile.open(tmp_archive_path) as f:
-                libraries = []
-                for i in f.getmembers():
-                    if i.name.startswith("bin") and i.name.endswith(target.bin_ext):
-                        libraries.append(i)
-                f.extractall(path=".", members=libraries)
+    if target.archive_ext == "zip":
+        with zipfile.ZipFile(tmp_archive_path) as f:
+            for info in f.infolist():
+                if info.filename.startswith("bin") and info.filename.endswith(
+                    target.bin_ext
+                ):
+                    f.extract(info, ".")
+    elif target.archive_ext == "tar.gz":
+        with tarfile.open(tmp_archive_path) as f:
+            libraries = []
+            for i in f.getmembers():
+                if i.name.startswith("bin") and i.name.endswith(target.bin_ext):
+                    libraries.append(i)
+            f.extractall(path=".", members=libraries)
 
-        os.makedirs(f"pyautd3/bin/{target.os_name}_{target.arch}", exist_ok=True)
-        for f in glob.glob("./bin/*"):
-            shutil.move(f, f"pyautd3/bin/{target.os_name}_{target.arch}")
-        os.remove(tmp_archive_path)
+    os.makedirs(f"pyautd3/bin/{target.os_name}_{target.arch}", exist_ok=True)
+    for f in glob.glob("./bin/*"):
+        shutil.move(f, f"pyautd3/bin/{target.os_name}_{target.arch}")
+    os.remove(tmp_archive_path)
 
 
 skip_download_bin = False
@@ -149,6 +151,9 @@ setuptools.setup(
     include_package_data=True,
     package_dir={"pyautd3": "pyautd3"},
     packages=packages,
+    install_requires=[
+        'numpy',
+    ],
     python_requires=">=3.9",
     data_files=data_files,
 )
