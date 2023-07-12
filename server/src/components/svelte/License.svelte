760117c2810403f66034191f76f89e1bc0327cd6
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-12 14:11:52 +0900

    fix #100: add license and third party license

---
+++ server/src/components/svelte/License.svelte
@@ -0,0 +1,48 @@
+<!--
+File: License.svelte
+Project: autd-server
+Created Date: 12/07/2023
+Author: Shun Suzuki
+-----
+Last Modified: 12/07/2023
+Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+-----
+Copyright (c) 2023 Shun Suzuki. All rights reserved.
+
+-->
+
+<div>
+    <h1>AUTD Server</h1>
+    <p>Copyright (c) 2023 Shun Suzuki. All rights reserved.</p>
+    <p>
+        AUTD Server is MIT licensed. For the licenses of the programs used by
+        AUTD Server, please see Third party.
+    </p>
+    <p>
+        AUTD Server is distributed at https://github.com/shinolab/autd3. For
+        details, please refer to this repository.
+    </p>
+</div>
+
+<style>
+    div {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: left;
+        color: #ffffff;
+    }
+
+    h1 {
+        font-size: 2em;
+        margin: 0;
+    }
+
+    p {
+        font-size: 1em;
+        margin: 1em 0 0 0;
+    }
+</style>
