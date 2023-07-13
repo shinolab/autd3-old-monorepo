ab57e466d02a3b994730ddf27d523df899fc9958
Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-13 14:44:40 +0900

    fix for mac

---
+++ src/autd3-link-soem/src/local/soem_bindings/mod.rs
@@ -0,0 +1,28 @@
+/*
+ * File: mod.rs
+ * Project: soem_bindings
+ * Created Date: 13/07/2023
+ * Author: Shun Suzuki
+ * -----
+ * Last Modified: 13/07/2023
+ * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+ * -----
+ * Copyright (c) 2023 Shun Suzuki. All rights reserved.
+ *
+ */
+
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+#![allow(clippy::upper_case_acronyms)]
+#![allow(deref_nullptr)]
+#[cfg(target_os = "windows")]
+mod win32;
+#[cfg(target_os = "windows")]
+pub use win32::*;
+
+#[cfg(target_os = "macos")]
+mod macos;
+#[cfg(target_os = "macos")]
+pub use macos::*;
