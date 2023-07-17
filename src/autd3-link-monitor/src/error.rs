f12826729a9d9d180d7c7b8759de3df10adbbd9f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-17 09:44:04 +0900

    impl PlottersBackend

--- src/autd3-link-monitor/src/error.rs
+++ src/autd3-link-monitor/src/error.rs
@@ -4,7 +4,7 @@
  * Created Date: 14/06/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 16/07/2023
+ * Last Modified: 17/07/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -14,6 +14,7 @@
 use autd3_core::error::AUTDInternalError;
 use thiserror::Error;
 
+#[cfg(feature = "plotters")]
 use plotters::drawing::DrawingAreaErrorKind;
 
 #[derive(Error, Debug)]
@@ -23,12 +24,16 @@ pub enum MonitorError {
     PyO3Error(pyo3::PyErr),
     #[error("Plot range is invalid")]
     InvalidPlotRange,
+    #[cfg(feature = "plotters")]
     #[error("{0}")]
-    BitmapDrawingAreaError(
-        DrawingAreaErrorKind<
-            <plotters::prelude::BitMapBackend<'static> as plotters::prelude::DrawingBackend>::ErrorType,
-        >,
-    ),
+    DrawingAreaError(String),
+    #[error("Not supported operation")]
+    NotSupported,
+    #[error("{0}")]
+    IoError(std::io::Error),
+    #[cfg(feature = "plotters")]
+    #[error("{0}")]
+    BitMapBackendError(plotters_bitmap::BitMapBackendError),
 }
 
 impl From<MonitorError> for AUTDInternalError {
@@ -37,15 +42,17 @@ impl From<MonitorError> for AUTDInternalError {
     }
 }
 
-impl From<
-DrawingAreaErrorKind<
-<plotters::prelude::BitMapBackend<'static> as plotters::prelude::DrawingBackend>::ErrorType,
-> 
-> for MonitorError {
-    fn from(value: DrawingAreaErrorKind<
-        <plotters::prelude::BitMapBackend<'static> as plotters::prelude::DrawingBackend>::ErrorType,
-        >) -> Self {
-        Self::BitmapDrawingAreaError(value)
+#[cfg(feature = "plotters")]
+impl<E: std::error::Error + Send + Sync> From<DrawingAreaErrorKind<E>> for MonitorError {
+    fn from(value: DrawingAreaErrorKind<E>) -> Self {
+        Self::DrawingAreaError(value.to_string())
+    }
+}
+
+#[cfg(feature = "plotters")]
+impl From<plotters_bitmap::BitMapBackendError> for MonitorError {
+    fn from(value: plotters_bitmap::BitMapBackendError) -> Self {
+        Self::BitMapBackendError(value)
     }
 }
 
@@ -53,5 +60,11 @@ DrawingAreaErrorKind<
 impl From<pyo3::PyErr> for MonitorError {
     fn from(value: pyo3::PyErr) -> Self {
         Self::PyO3Error(value)
+    }
+}
+
+impl From<std::io::Error> for MonitorError {
+    fn from(value: std::io::Error) -> Self {
+        Self::IoError(value)
     }
 }
