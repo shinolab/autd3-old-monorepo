4d3177539e6cd04e6f3cfcb44072e7cd87702da9
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-19 13:22:42 +0900

    add AUTDModulationWavWithSamplingFrequencyDivision

--- capi/autd3capi-modulation-audio-file/src/lib.rs
+++ capi/autd3capi-modulation-audio-file/src/lib.rs
@@ -4,7 +4,7 @@
  * Created Date: 27/05/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 02/06/2023
+ * Last Modified: 19/07/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -15,7 +15,7 @@
 
 use std::ffi::{c_char, CStr};
 
-use autd3capi_def::{common::*, ModulationPtr};
+use autd3capi_def::{common::*, take_mod, ModulationPtr};
 
 use autd3_modulation_audio_file::Wav;
 
@@ -25,4 +25,13 @@ pub unsafe extern "C" fn AUTDModulationWav(path: *const c_char, err: *mut c_char
     let path = try_or_return!(CStr::from_ptr(path).to_str(), err, ModulationPtr(NULL));
     let m = try_or_return!(Wav::new(path), err, ModulationPtr(NULL));
     ModulationPtr::new(m)
+}
+
+#[no_mangle]
+#[must_use]
+pub unsafe extern "C" fn AUTDModulationWavWithSamplingFrequencyDivision(
+    m: ModulationPtr,
+    div: u32,
+) -> ModulationPtr {
+    ModulationPtr::new(take_mod!(m, Wav).with_sampling_frequency_division(div))
 }
