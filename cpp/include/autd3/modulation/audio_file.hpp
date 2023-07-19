a9066712ffcdb424234c5743738ea2f1c2d8c57d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-19 13:22:42 +0900

    add AUTDModulationWavWithSamplingFrequencyDivision

--- cpp/include/autd3/modulation/audio_file.hpp
+++ cpp/include/autd3/modulation/audio_file.hpp
@@ -3,7 +3,7 @@
 // Created Date: 29/05/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 04/06/2023
+// Last Modified: 19/07/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -23,15 +23,26 @@ class Wav final : public internal::Modulation {
  public:
   explicit Wav(std::filesystem::path path) : _path(std::move(path)) {}
 
+  Wav with_sampling_frequency_division(const uint32_t div) {
+    _freq_div = div;
+    return *this;
+  }
+
+  Wav with_sampling_frequency(const double freq) {
+    return with_sampling_frequency_division(static_cast<uint32_t>(static_cast<double>(internal::native_methods::FPGA_SUB_CLK_FREQ) / freq));
+  }
+
   [[nodiscard]] internal::native_methods::ModulationPtr modulation_ptr() const override {
     char err[256]{};
-    const auto ptr = internal::native_methods::AUTDModulationWav(_path.string().c_str(), err);
+    auto ptr = internal::native_methods::AUTDModulationWav(_path.string().c_str(), err);
     if (ptr._0 == nullptr) throw internal::AUTDException(err);
+    if (_freq_div.has_value()) ptr = AUTDModulationWavWithSamplingFrequencyDivision(ptr, _freq_div.value());
     return ptr;
   }
 
  private:
   std::filesystem::path _path;
+  std::optional<uint32_t> _freq_div;
 };
 
 }  // namespace autd3::modulation
