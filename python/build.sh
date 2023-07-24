18465cdb68e1230e5a59562c870b4a3983458a8a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-24 15:44:36 +0900

    fix python build

--- python/build.sh
+++ python/build.sh
@@ -14,20 +14,20 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     cargo build --release --all
   fi
   cd ..
-  mkdir -p python/pyautd3/bin/linux_x64
-  cp ./capi/target/release/*.so python/pyautd3/bin/linux_x64
+  mkdir -p python/pyautd3/bin
+  cp ./capi/target/release/*.so python/pyautd3/bin
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   cargo build --release --all --exclude autd3capi-backend-cuda --target=x86_64-apple-darwin
   cargo build --release --all --exclude autd3capi-backend-cuda --target=aarch64-apple-darwin
   cd ..
-  mkdir -p python/pyautd3/bin/macos_universal
+  mkdir -p python/pyautd3/bin
   for x64_file in `ls ./capi/target/x86_64-apple-darwin/release/*.dylib`; do
     file_basename=`basename $x64_file`
-    lipo -create $x64_file ./capi/target/aarch64-apple-darwin/release/$file_basename -output python/pyautd3/bin/macos_universal/$file_basename
+    lipo -create $x64_file ./capi/target/aarch64-apple-darwin/release/$file_basename -output python/pyautd3/bin/$file_basename
   done
 fi
 
 cd python
-python setup.py sdist bdist_wheel True
+python -m build
 
 popd
