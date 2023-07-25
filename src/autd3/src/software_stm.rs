240c7f2b4f16683f35e61d911a0b700b0f0dfa28
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-26 07:11:57 +0900

    osaltimer duration

--- src/autd3/src/software_stm.rs
+++ src/autd3/src/software_stm.rs
@@ -4,7 +4,7 @@
  * Created Date: 23/06/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 24/07/2023
+ * Last Modified: 26/07/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -176,7 +176,7 @@ impl<
                         i: i.clone(),
                         now: now.clone(),
                     },
-                    interval.as_nanos() as _,
+                    interval,
                 ) {
                     Ok(handle) => handle,
                     Err(e) => {
