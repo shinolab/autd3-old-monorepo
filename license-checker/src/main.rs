81cbd003a93ef9ff8152a609d295295814309f30
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-07-27 08:36:28 +0900

    update server license

--- license-checker/src/main.rs
+++ license-checker/src/main.rs
@@ -23,6 +23,7 @@ use license_file::Package;
 
 mod diff;
 mod license_file;
+mod npm;
 
 fn check<P>(
     path: P,
@@ -101,9 +102,6 @@ where
                 .map(|license| format!(" ({})", license))
                 .unwrap_or_default()
         )?;
-        if let Some(authors) = dependency.authors {
-            writeln!(writer, "{}", authors)?;
-        }
         if let Some(rep) = dependency.repository {
             writeln!(writer, "{}", rep)?;
         }
@@ -196,12 +194,119 @@ where
     Ok(())
 }
 
+fn check_npm<P>(path: P, filename: &str) -> anyhow::Result<()>
+where
+    P: Into<PathBuf>,
+{
+    let path: PathBuf = path.into();
+
+    let dependencies = npm::glob_node_modules(&path)?;
+
+    let mut licenses = std::collections::HashSet::new();
+    for dependency in &dependencies {
+        licenses.insert(dependency.license.to_owned());
+    }
+
+    let old = path.parent().unwrap().join(format!("{}.txt", filename));
+    let new = path.parent().unwrap().join(format!("{}-new.txt", filename));
+
+    let mut writer = BufWriter::new(File::create(&new)?);
+
+    writeln!(writer, "THIRD-PARTY SOFTWARE NOTICES AND INFORMATION")?;
+    writeln!(writer)?;
+    writeln!(
+        writer,
+        "This software includes the following third-party components."
+    )?;
+    writeln!(
+        writer,
+        "The license terms for each of these components are provided later in this notice."
+    )?;
+    writeln!(writer)?;
+    writeln!(writer)?;
+
+    for dependency in dependencies {
+        writeln!(
+            writer,
+            "---------------------------------------------------------"
+        )?;
+        writeln!(writer)?;
+
+        writeln!(
+            writer,
+            "{} {} {}",
+            dependency.name, dependency.version, dependency.license
+        )?;
+        writeln!(writer, "{}", dependency.repository)?;
+    }
+
+    writeln!(writer)?;
+
+    for entry in glob::glob(concat!(env!("CARGO_MANIFEST_DIR"), "/licenses/*.txt"))? {
+        let entry = entry?;
+        let path = Path::new(&entry);
+        let name = path.file_stem().unwrap().to_str().unwrap();
+
+        if !licenses.iter().any(|license| license.contains(name)) {
+            continue;
+        }
+
+        writeln!(
+            writer,
+            "---------------------------------------------------------"
+        )?;
+        writeln!(writer)?;
+        writeln!(writer, "{}", name)?;
+        writeln!(writer)?;
+        writeln!(writer, "---")?;
+        writeln!(writer)?;
+
+        let mut file_content = String::new();
+        fs::File::open(path)
+            .map(BufReader::new)?
+            .read_to_string(&mut file_content)?;
+        writer.write_all(file_content.as_bytes())?;
+    }
+    writeln!(
+        writer,
+        "---------------------------------------------------------"
+    )?;
+
+    writer.flush()?;
+
+    if old.exists() {
+        let mut old_license = String::new();
+        fs::File::open(&old)
+            .map(BufReader::new)?
+            .read_to_string(&mut old_license)?;
+
+        let mut new_license = String::new();
+        fs::File::open(&new)
+            .map(BufReader::new)?
+            .read_to_string(&mut new_license)?;
+
+        if diff::show_diff(&old_license, &new_license) {
+            return Err(anyhow::anyhow!(
+                "ThirdPartyNotice.txt is not up to date. Please check {} manually.",
+                new.canonicalize().unwrap().to_str().unwrap()
+            ));
+        }
+
+        fs::remove_file(&old)?;
+    }
+
+    std::fs::rename(new, old)?;
+
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
     let license_file_map = license_file::load_license_file_map()?;
 
     let sdr_rs_dep = (
         "MIT",
-        r"@Adam Greig <adam@adamgreig.com>/sdr 0.7.0 (MIT)
+        r"sdr 0.7.0 (MIT)
+https://github.com/adamgreig/sdr-rs
 Modification of the original version by Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>",
     );
     let notofont_dep = ("OFL", "Noto Sans v2.012 (OFL)");
@@ -215,14 +320,14 @@ Modification of the original version by Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.j
 
     check(
         Path::new(env!("CARGO_MANIFEST_DIR")).join("../server/simulator/Cargo.toml"),
-        "AUTD-Simulator-ThirdPartyNotice",
+        "ThirdPartyNotice",
         &license_file_map,
         &[sdr_rs_dep, notofont_dep],
     )?;
 
     check(
         Path::new(env!("CARGO_MANIFEST_DIR")).join("../server/SOEMAUTDServer/Cargo.toml"),
-        "SOEMAUTDServer-ThirdPartyNotice",
+        "ThirdPartyNotice",
         &license_file_map,
         &[sdr_rs_dep],
     )?;
@@ -232,6 +337,11 @@ Modification of the original version by Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.j
         "ThirdPartyNotice",
         &license_file_map,
         &[sdr_rs_dep],
+    )?;
+
+    check_npm(
+        Path::new(env!("CARGO_MANIFEST_DIR")).join("../server/node_modules"),
+        "ThirdPartyNotice",
     )?;
 
     Ok(())
