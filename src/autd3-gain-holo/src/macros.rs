2a6e87339a297b6d671edf980aadd77b70e987af
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-08-01 13:28:06 +0900

    improve gain holo performance

--- src/autd3-gain-holo/src/macros.rs
+++ src/autd3-gain-holo/src/macros.rs
@@ -4,7 +4,7 @@
  * Created Date: 28/05/2021
  * Author: Shun Suzuki
  * -----
- * Last Modified: 24/05/2023
+ * Last Modified: 01/08/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2021 Shun Suzuki. All rights reserved.
@@ -13,7 +13,7 @@
 
 use crate::MatrixXc;
 use autd3_core::{
-    acoustics::{propagate, Sphere},
+    acoustics::{propagate_tr, Sphere},
     geometry::{Geometry, Transducer, Vector3},
 };
 
@@ -31,15 +31,8 @@ pub fn generate_propagation_matrix<T: Transducer>(
         m,
         num_trans,
         geometry.transducers().flat_map(|trans| {
-            foci.iter().map(move |fp| {
-                propagate::<Sphere>(
-                    trans.position(),
-                    &trans.z_direction(),
-                    attenuation,
-                    trans.wavenumber(sound_speed),
-                    fp,
-                )
-            })
+            foci.iter()
+                .map(move |fp| propagate_tr::<Sphere, T>(trans, attenuation, sound_speed, fp))
         }),
     )
 }
