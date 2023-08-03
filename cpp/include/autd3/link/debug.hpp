60c91c4d992461748e89e1c738f44969fa10f3cf
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-08-04 07:03:19 +0900

    doc link

--- cpp/include/autd3/link/debug.hpp
+++ cpp/include/autd3/link/debug.hpp
@@ -3,7 +3,7 @@
 // Created Date: 29/05/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 03/06/2023
+// Last Modified: 04/08/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -19,15 +19,32 @@
 
 namespace autd3::link {
 
+/**
+ * @brief Link for debugging
+ */
 class Debug : public internal::Link {
  public:
   Debug() : Link(internal::native_methods::AUTDLinkDebug()) {}
 
+  /**
+   * @brief Set log level
+   *
+   * @param level log level
+   * @return Debug
+   */
   Debug with_log_level(const internal::native_methods::Level level) {
     _ptr = AUTDLinkDebugWithLogLevel(_ptr, level);
     return *this;
   }
 
+  /**
+   * @brief Set log function
+   * @details By default, the logger will display log messages on the console.
+   *
+   * @param out output callback
+   * @param flush flush callback
+   * @return Debug
+   */
   Debug with_log_func(const internal::LogOutCallback out, const internal::LogFlushCallback flush) {
     _ptr = AUTDLinkDebugWithLogFunc(_ptr, reinterpret_cast<void*>(out), reinterpret_cast<void*>(flush));
     return *this;
