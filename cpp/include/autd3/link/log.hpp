c5366813ef21cfcee5d34d127dc2115bd313fa97
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-08-04 07:03:19 +0900

    doc link

--- cpp/include/autd3/link/log.hpp
+++ cpp/include/autd3/link/log.hpp
@@ -3,7 +3,7 @@
 // Created Date: 22/06/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 22/06/2023
+// Last Modified: 04/08/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -19,6 +19,9 @@
 
 namespace autd3::link {
 
+/**
+ * @brief Link for logging
+ */
 class Log : public internal::Link {
  public:
   template <class L>
@@ -26,11 +29,25 @@ class Log : public internal::Link {
     static_assert(std::is_base_of_v<Link, std::remove_reference_t<L>>, "This is not a Link");
   }
 
+  /**
+   * @brief Set log level
+   *
+   * @param level log level
+   * @return Log
+   */
   Log with_log_level(const internal::native_methods::Level level) {
     _ptr = AUTDLinkLogWithLogLevel(_ptr, level);
     return *this;
   }
 
+  /**
+   * @brief Set log function
+   * @details By default, the logger will display log messages on the console.
+   *
+   * @param out output callback
+   * @param flush flush callback
+   * @return Log
+   */
   Log with_log_func(const internal::LogOutCallback out, const internal::LogFlushCallback flush) {
     _ptr = AUTDLinkLogWithLogFunc(_ptr, reinterpret_cast<void*>(out), reinterpret_cast<void*>(flush));
     return *this;
