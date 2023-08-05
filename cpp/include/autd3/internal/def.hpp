2b93e05b0fcfb7db79a4317b1f8d8d647bcbb75f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-08-06 05:47:53 +0900

    close #89

--- cpp/include/autd3/internal/def.hpp
+++ cpp/include/autd3/internal/def.hpp
@@ -3,7 +3,7 @@
 // Created Date: 29/05/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 03/06/2023
+// Last Modified: 06/08/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -27,8 +27,14 @@ using Quaternion = Eigen::Quaternion<double>;
 using Affine3 = Eigen::Transform<double, 3, Eigen::Affine>;
 
 #if __cplusplus >= 202002L
+/**
+ * @brief Mathematical constant pi
+ */
 constexpr double pi = std::numbers::pi_v<double>;
 #else
+/**
+ * @brief Mathematical constant pi
+ */
 constexpr double pi = 3.141592653589793238462643383279502884L;
 #endif
 
--- cpp/include/autd3/internal/silencer.hpp
+++ cpp/include/autd3/internal/silencer.hpp
@@ -3,7 +3,7 @@
 // Created Date: 29/05/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 04/06/2023
+// Last Modified: 06/08/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -16,11 +16,22 @@
 
 namespace autd3::internal {
 
+/**
+ * @brief Header to configure silencer
+ */
 class SilencerConfig final : public Header {
  public:
   SilencerConfig() noexcept : SilencerConfig(10) {}
+  /**
+   * @brief Constructor
+   *
+   * @param step Update step of silencer. The smaller `step` is, the quieter the output is.
+   */
   explicit SilencerConfig(const uint16_t step) noexcept : Header(), _step(step) {}
 
+  /**
+   * @brief Disable silencer
+   */
   static SilencerConfig none() noexcept { return SilencerConfig(0xFFFF); }
 
   [[nodiscard]] native_methods::DatagramHeaderPtr ptr() const override { return native_methods::AUTDCreateSilencer(_step); }
