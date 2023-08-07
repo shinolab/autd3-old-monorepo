8bcd00ef058ae91550fc08137830dc675ab8d947
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-08-08 07:31:43 +0900

    rm backend, add LinAlgBackend (naive and gs only)

--- src/autd3-gain-holo/src/linear_synthesis/gs.rs
+++ src/autd3-gain-holo/src/linear_synthesis/gs.rs
@@ -4,7 +4,7 @@
  * Created Date: 29/05/2021
  * Author: Shun Suzuki
  * -----
- * Last Modified: 18/07/2023
+ * Last Modified: 08/08/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2021 Shun Suzuki. All rights reserved.
@@ -13,7 +13,7 @@
 
 use std::rc::Rc;
 
-use crate::{constraint::Constraint, impl_holo, macros::generate_propagation_matrix, Backend};
+use crate::{constraint::Constraint, impl_holo, Complex, LinAlgBackend, Trans};
 use autd3_core::{
     error::AUTDInternalError,
     float,
@@ -29,7 +29,7 @@ use nalgebra::ComplexField;
 /// Reference
 /// * Asier Marzo and Bruce W Drinkwater. Holographic acoustic tweezers.Proceedings of theNational Academy of Sciences, 116(1):84–89, 2019.
 #[derive(Gain)]
-pub struct GS<B: Backend + 'static> {
+pub struct GS<B: LinAlgBackend + 'static> {
     foci: Vec<Vector3>,
     amps: Vec<float>,
     repeat: usize,
@@ -39,7 +39,7 @@ pub struct GS<B: Backend + 'static> {
 
 impl_holo!(B, GS<B>);
 
-impl<B: Backend + 'static> GS<B> {
+impl<B: LinAlgBackend + 'static> GS<B> {
     pub fn new(backend: Rc<B>) -> Self {
         Self {
             foci: vec![],
@@ -59,10 +59,49 @@ impl<B: Backend + 'static> GS<B> {
     }
 }
 
-impl<B: Backend + 'static, T: Transducer> Gain<T> for GS<B> {
+impl<B: LinAlgBackend + 'static, T: Transducer> Gain<T> for GS<B> {
     fn calc(&self, geometry: &Geometry<T>) -> Result<Vec<Drive>, AUTDInternalError> {
-        let g = generate_propagation_matrix(geometry, &self.foci);
-        let q = self.backend.gs(self.repeat, &self.amps, g)?;
+        let m = geometry.num_transducers();
+        let n = self.foci.len();
+
+        let g = self
+            .backend
+            .generate_propagation_matrix(geometry, &self.foci);
+
+        let amps = self.backend.make_complex_v(&self.amps);
+
+        let ones = vec![1.; m];
+        let q0 = self.backend.make_complex_v(&ones);
+        let mut q = self.backend.make_complex_v(&ones);
+
+        let mut p = self.backend.alloc_zeros_cv(n);
+        for _ in 0..self.repeat {
+            self.backend.c_gemv(
+                Trans::NoTrans,
+                Complex::new(1., 0.),
+                &g,
+                &q,
+                Complex::new(0., 0.),
+                &mut p,
+            );
+            self.backend.normalize_cv(&mut p);
+            self.backend.hadamard_product_cv(&amps, &mut p);
+
+            self.backend.c_gemv(
+                Trans::ConjTrans,
+                Complex::new(1., 0.),
+                &g,
+                &p,
+                Complex::new(0., 0.),
+                &mut q,
+            );
+
+            self.backend.normalize_cv(&mut q);
+            self.backend.hadamard_product_cv(&q0, &mut q);
+        }
+
+        let q = self.backend.to_host_cv(q);
+
         let max_coefficient = q.camax().abs();
         Ok(geometry
             .transducers()
