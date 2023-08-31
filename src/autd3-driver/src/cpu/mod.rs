0b137016e3d4af94317370e66b61cb9577e54728
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-08-31 15:41:27 +0900

    update driver/fpga and cpu

--- src/autd3-driver/src/cpu/mod.rs
+++ src/autd3-driver/src/cpu/mod.rs
@@ -4,21 +4,17 @@
  * Created Date: 02/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 19/05/2023
+ * Last Modified: 31/08/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
  *
  */
 
-mod body;
-mod cpu_flag;
 mod datagram;
 mod ec_config;
 mod header;
 
-pub use body::*;
-pub use cpu_flag::*;
 pub use datagram::*;
 pub use ec_config::*;
 pub use header::*;
