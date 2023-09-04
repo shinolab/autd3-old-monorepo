68692cb2059144f65a83d37a4cacc35ed91536b5
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-09-04 14:03:41 +0900

    rm unused

--- src/autd3-driver/src/cpu/header.rs
+++ src/autd3-driver/src/cpu/header.rs
@@ -4,7 +4,7 @@
  * Created Date: 02/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 31/08/2023
+ * Last Modified: 04/09/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
@@ -12,15 +12,6 @@
  */
 
 use crate::fpga::FPGAControlFlags;
-
-pub const MSG_CLEAR: u8 = 0x00;
-pub const MSG_RD_CPU_VERSION: u8 = 0x01;
-pub const MSG_RD_CPU_VERSION_MINOR: u8 = 0x02;
-pub const MSG_RD_FPGA_VERSION: u8 = 0x03;
-pub const MSG_RD_FPGA_VERSION_MINOR: u8 = 0x04;
-pub const MSG_RD_FPGA_FUNCTION: u8 = 0x05;
-pub const MSG_BEGIN: u8 = 0x10;
-pub const MSG_END: u8 = 0xF0;
 
 #[derive(Clone, Copy)]
 #[repr(C)]
