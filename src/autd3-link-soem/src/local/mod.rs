6bb14548d6b171c05ea3663cc84a6d007af0734e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-09-05 13:59:51 +0900

    update soem link

--- src/autd3-link-soem/src/local/mod.rs
+++ src/autd3-link-soem/src/local/mod.rs
@@ -4,7 +4,7 @@
  * Created Date: 21/05/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 26/07/2023
+ * Last Modified: 05/09/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -18,6 +18,6 @@ mod iomap;
 mod link_soem;
 mod soem_bindings;
 
-pub use autd3_core::sync_mode::SyncMode;
+pub use autd3_driver::sync_mode::SyncMode;
 pub use ethernet_adapters::EthernetAdapters;
 pub use link_soem::SOEM;
--- src/examples/src/remote_soem.rs
+++ src/examples/src/remote_soem.rs
@@ -4,13 +4,14 @@
  * Created Date: 27/04/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 28/06/2023
+ * Last Modified: 05/09/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
  *
  */
 
+#[macro_use]
 mod test_runner;
 mod tests;
 
@@ -20,9 +21,9 @@ use autd3::prelude::*;
 use autd3_link_soem::RemoteSOEM;
 
 fn main() -> Result<()> {
-    let autd = Controller::builder()
+    let mut autd = Controller::builder()
         .add_device(AUTD3::new(Vector3::zeros(), Vector3::zeros()))
         .open_with(RemoteSOEM::new("127.0.0.1:8080".parse()?)?)?;
 
-    test_runner::run(autd)
+    run!(autd)
 }
