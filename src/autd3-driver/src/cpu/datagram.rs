f6e1ef0437a574a5df22206c7dedc4c9ee4d40e0
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-09-05 10:15:43 +0900

    fix txdatagram

--- src/autd3-driver/src/cpu/datagram.rs
+++ src/autd3-driver/src/cpu/datagram.rs
@@ -13,50 +13,37 @@
 
 use std::ops::{Deref, DerefMut};
 
-use crate::{
-    cpu::{Header, EC_OUTPUT_FRAME_SIZE},
-    geometry::{Geometry, Transducer},
-};
+use crate::cpu::{Header, EC_OUTPUT_FRAME_SIZE};
 
 #[derive(Clone)]
 pub struct TxDatagram {
     data: Vec<u8>,
-    data_pointer: Vec<usize>,
-    device_map: Vec<usize>,
+    num_devices: usize,
 }
 
 impl TxDatagram {
-    pub fn new<T: Transducer>(geometry: &Geometry<T>) -> Self {
-        let device_map = geometry
-            .iter()
-            .map(|dev| dev.num_transducers())
-            .collect::<Vec<_>>();
-        let data_pointer = [0usize]
-            .iter()
-            .chain(device_map.iter())
-            .scan(0, |state, tr_num| {
-                *state += std::mem::size_of::<u16>() * tr_num;
-                Some(*state)
-            })
-            .collect::<Vec<_>>();
+    pub fn new(num_devices: usize) -> Self {
         Self {
-            data: vec![0x00; (EC_OUTPUT_FRAME_SIZE) * device_map.len()],
-            data_pointer,
-            device_map,
+            data: vec![0x00; (EC_OUTPUT_FRAME_SIZE) * num_devices],
+            num_devices,
         }
     }
 
-    pub fn data(&self) -> &[u8] {
+    pub fn all_data(&self) -> &[u8] {
         &self.data
     }
 
+    pub fn data(&self, i: usize) -> &[u8] {
+        &self.data[i * EC_OUTPUT_FRAME_SIZE..(i + 1) * EC_OUTPUT_FRAME_SIZE]
+    }
+
     pub fn headers(&self) -> impl Iterator<Item = &Header> {
-        (0..self.device_map.len()).map(|i| self.header(i))
+        (0..self.num_devices).map(|i| self.header(i))
     }
 
     pub fn header(&self, i: usize) -> &Header {
         unsafe {
-            (self.data[self.data_pointer[i]..].as_ptr() as *const Header)
+            (self.data[i * EC_OUTPUT_FRAME_SIZE..].as_ptr() as *const Header)
                 .as_ref()
                 .unwrap()
         }
@@ -64,24 +51,24 @@ impl TxDatagram {
 
     pub fn header_mut(&mut self, i: usize) -> &mut Header {
         unsafe {
-            (self.data[self.data_pointer[i]..].as_mut_ptr() as *mut Header)
+            (self.data[i * EC_OUTPUT_FRAME_SIZE..].as_mut_ptr() as *mut Header)
                 .as_mut()
                 .unwrap()
         }
     }
 
     pub fn payload(&self, i: usize) -> &[u8] {
-        &self.data[self.data_pointer[i] + std::mem::size_of::<Header>()
-            ..self.data_pointer[i] + EC_OUTPUT_FRAME_SIZE]
+        &self.data[i * EC_OUTPUT_FRAME_SIZE + std::mem::size_of::<Header>()
+            ..(i + 1) * EC_OUTPUT_FRAME_SIZE]
     }
 
     pub fn payload_mut(&mut self, i: usize) -> &mut [u8] {
-        &mut self.data[self.data_pointer[i] + std::mem::size_of::<Header>()
-            ..self.data_pointer[i] + EC_OUTPUT_FRAME_SIZE]
+        &mut self.data[i * EC_OUTPUT_FRAME_SIZE + std::mem::size_of::<Header>()
+            ..(i + 1) * EC_OUTPUT_FRAME_SIZE]
     }
 
     pub fn payloads(&self) -> impl Iterator<Item = &[u8]> {
-        (0..self.device_map.len()).map(|i| self.payload(i))
+        (0..self.num_devices).map(|i| self.payload(i))
     }
 
     pub fn copy_from(&mut self, src: &TxDatagram) {
@@ -132,12 +119,9 @@ impl RxDatagram {
     }
 
     pub fn copy_from_iter<I: IntoIterator<Item = RxMessage>>(&mut self, src: I) {
-        self.data
-            .iter_mut()
-            .zip(src)
-            .for_each(|(dst, src)| {
-                *dst = src;
-            });
+        self.data.iter_mut().zip(src).for_each(|(dst, src)| {
+            *dst = src;
+        });
     }
 }
 
