213cb9afc2a14e4a766d01e3385479c7ae85eb93
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-09-10 10:33:39 +0900

    error on firmware info failed

--- src/autd3/src/error.rs
+++ src/autd3/src/error.rs
@@ -4,7 +4,7 @@
  * Created Date: 02/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 01/09/2023
+ * Last Modified: 10/09/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
@@ -14,10 +14,35 @@
 use autd3_driver::error::AUTDInternalError;
 use thiserror::Error;
 
+pub struct ReadFirmwareInfoState(pub Vec<bool>);
+
+impl std::fmt::Display for ReadFirmwareInfoState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Read firmware info failed: {}",
+            self.0
+                .iter()
+                .enumerate()
+                .filter_map(|(i, b)| if *b { None } else { Some(i.to_string()) })
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    }
+}
+
+impl std::fmt::Debug for ReadFirmwareInfoState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        <Self as std::fmt::Display>::fmt(&self, f)
+    }
+}
+
 #[derive(Error, Debug)]
 pub enum AUTDError {
     #[error("Device id ({0}) is specified, but only {1} AUTDs are connected.")]
     GroupedOutOfRange(usize, usize),
+    #[error("{0}")]
+    ReadFirmwareInfoFailed(ReadFirmwareInfoState),
     #[error("{0}")]
     Internal(AUTDInternalError),
 }
