23829e299a1e4a0b3ba69890bd02b8e8a175d74e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-09-12 08:03:32 +0900

    change builder api

--- src/examples/src/freq_config.rs
+++ src/examples/src/freq_config.rs
@@ -4,7 +4,7 @@
  * Created Date: 24/05/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 05/09/2023
+ * Last Modified: 12/09/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -17,7 +17,8 @@ use autd3::link::Debug;
 use autd3::prelude::*;
 
 fn main() -> Result<()> {
-    let mut autd = Controller::advanced_builder()
+    let mut autd = Controller::builder()
+        .advanced()
         .add_device(AUTD3::new(Vector3::zeros(), Vector3::zeros()))
         .open_with(Debug::new().with_log_level(LevelFilter::MoreSevereEqual(Level::Debug)))?;
 
