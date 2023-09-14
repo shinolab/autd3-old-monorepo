cc946be2026d4342f87c2513444933e63d64ead9
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-09-14 12:23:37 +0900

    add op handler test

--- src/autd3-driver/src/operation/mod.rs
+++ src/autd3-driver/src/operation/mod.rs
@@ -4,7 +4,7 @@
  * Created Date: 08/01/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 13/09/2023
+ * Last Modified: 14/09/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -193,5 +193,127 @@ impl OperationHandler {
         op.commit(dev);
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{
+        cpu::{Header, EC_OUTPUT_FRAME_SIZE},
+        geometry::{LegacyTransducer, UnitQuaternion, Vector3},
+    };
+
+    use super::*;
+
+    struct OperationMock {
+        pub initialized: HashMap<usize, bool>,
+        pub pack_size: HashMap<usize, usize>,
+        pub required_size: HashMap<usize, usize>,
+        pub num_frames: HashMap<usize, usize>,
+    }
+
+    impl<T: Transducer> Operation<T> for OperationMock {
+        fn init(&mut self, geometry: &Geometry<T>) -> Result<(), AUTDInternalError> {
+            self.initialized = geometry
+                .devices()
+                .map(|dev| (dev.idx(), true))
+                .collect::<HashMap<_, _>>();
+            Ok(())
+        }
+
+        fn required_size(&self, device: &Device<T>) -> usize {
+            self.required_size[&device.idx()]
+        }
+
+        fn pack(&mut self, device: &Device<T>, _: &mut [u8]) -> Result<usize, AUTDInternalError> {
+            Ok(self.pack_size[&device.idx()])
+        }
+
+        fn commit(&mut self, device: &Device<T>) {
+            *self.num_frames.get_mut(&device.idx()).unwrap() -= 1;
+        }
+
+        fn remains(&self, device: &Device<T>) -> usize {
+            self.num_frames[&device.idx()]
+        }
+    }
+
+    #[test]
+    fn op_handler_test() {
+        let geometry = Geometry::new(vec![Device::new(
+            0,
+            vec![LegacyTransducer::new(
+                0,
+                Vector3::zeros(),
+                UnitQuaternion::identity(),
+            )],
+        )]);
+
+        let mut op1 = OperationMock {
+            initialized: Default::default(),
+            pack_size: Default::default(),
+            required_size: Default::default(),
+            num_frames: Default::default(),
+        };
+        op1.pack_size.insert(0, 1);
+        op1.required_size.insert(0, 2);
+        op1.num_frames.insert(0, 3);
+
+        let mut op2 = OperationMock {
+            initialized: Default::default(),
+            pack_size: Default::default(),
+            required_size: Default::default(),
+            num_frames: Default::default(),
+        };
+        op2.pack_size.insert(0, 1);
+        op2.required_size.insert(0, 2);
+        op2.num_frames.insert(0, 3);
+
+        OperationHandler::init(&mut op1, &mut op2, &geometry).unwrap();
+
+        assert!(op1.initialized[&0]);
+        assert!(op2.initialized[&0]);
+        assert!(!OperationHandler::is_finished(
+            &mut op1, &mut op2, &geometry
+        ));
+
+        let mut tx = TxDatagram::new(1);
+
+        OperationHandler::pack(&mut op1, &mut op2, &geometry, &mut tx).unwrap();
+        assert_eq!(op1.num_frames[&0], 2);
+        assert_eq!(op2.num_frames[&0], 2);
+        assert!(!OperationHandler::is_finished(
+            &mut op1, &mut op2, &geometry
+        ));
+
+        op1.pack_size.insert(
+            0,
+            EC_OUTPUT_FRAME_SIZE - std::mem::size_of::<Header>() - op2.required_size[&0],
+        );
+
+        OperationHandler::pack(&mut op1, &mut op2, &geometry, &mut tx).unwrap();
+        assert_eq!(op1.num_frames[&0], 1);
+        assert_eq!(op2.num_frames[&0], 1);
+        assert!(!OperationHandler::is_finished(
+            &mut op1, &mut op2, &geometry
+        ));
+
+        op1.pack_size.insert(
+            0,
+            EC_OUTPUT_FRAME_SIZE - std::mem::size_of::<Header>() - op2.required_size[&0] + 1,
+        );
+        OperationHandler::pack(&mut op1, &mut op2, &geometry, &mut tx).unwrap();
+        assert_eq!(op1.num_frames[&0], 0);
+        assert_eq!(op2.num_frames[&0], 1);
+        assert!(!OperationHandler::is_finished(
+            &mut op1, &mut op2, &geometry
+        ));
+
+        OperationHandler::pack(&mut op1, &mut op2, &geometry, &mut tx).unwrap();
+        assert_eq!(op1.num_frames[&0], 0);
+        assert_eq!(op2.num_frames[&0], 0);
+        assert!(OperationHandler::is_finished(&mut op1, &mut op2, &geometry));
     }
 }
