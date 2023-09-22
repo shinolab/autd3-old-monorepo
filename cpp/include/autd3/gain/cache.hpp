6d1a457612ec54e6b9d4da5a0965f569c9635df6
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-09-22 09:44:39 +0900

    fix #177

--- cpp/include/autd3/gain/cache.hpp
+++ cpp/include/autd3/gain/cache.hpp
@@ -3,7 +3,7 @@
 // Created Date: 13/09/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 20/09/2023
+// Last Modified: 22/09/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -29,7 +29,9 @@ namespace autd3::gain {
 template <class G>
 class Cache final : public internal::Gain {
  public:
-  explicit Cache(G g) : _g(std::move(g)) { static_assert(std::is_base_of_v<Gain, G>, "This is not Gain"); }
+  explicit Cache(G g) : _g(std::move(g)), _cache(std::make_shared<std::unordered_map<size_t, std::vector<internal::native_methods::Drive>>>()) {
+    static_assert(std::is_base_of_v<Gain, G>, "This is not Gain");
+  }
 
   [[nodiscard]] internal::native_methods::GainPtr gain_ptr(const internal::Geometry& geometry) const override {
     std::vector<uint32_t> device_indices;
@@ -37,8 +39,8 @@ class Cache final : public internal::Gain {
     std::transform(geometry.cbegin(), geometry.cend(), std::back_inserter(device_indices),
                    [](const internal::Device& dev) { return static_cast<uint32_t>(dev.idx()); });
 
-    if (_cache.size() != device_indices.size() ||
-        std::any_of(device_indices.begin(), device_indices.end(), [this](const uint32_t idx) { return _cache.find(idx) == _cache.end(); })) {
+    if (_cache->size() != device_indices.size() ||
+        std::any_of(device_indices.begin(), device_indices.end(), [this](const uint32_t idx) { return _cache->find(idx) == _cache->end(); })) {
       char err[256]{};
       auto res = internal::native_methods::AUTDGainCalc(_g.gain_ptr(geometry), geometry.ptr(), err);
       if (res._0 == nullptr) throw internal::AUTDException(err);
@@ -46,21 +48,21 @@ class Cache final : public internal::Gain {
         std::vector<internal::native_methods::Drive> drives;
         drives.resize(dev.num_transducers());
         internal::native_methods::AUTDGainCalcGetResult(res, drives.data(), static_cast<uint32_t>(dev.idx()));
-        _cache.emplace(dev.idx(), std::move(drives));
+        _cache->emplace(dev.idx(), std::move(drives));
       }
       internal::native_methods::AUTDGainCalcFreeResult(res);
     }
 
     return std::accumulate(geometry.cbegin(), geometry.cend(), internal::native_methods::AUTDGainCustom(),
                            [this](const internal::native_methods::GainPtr acc, const internal::Device& dev) {
-                             return AUTDGainCustomSet(acc, static_cast<uint32_t>(dev.idx()), _cache[dev.idx()].data(),
-                                                      static_cast<uint32_t>(_cache[dev.idx()].size()));
+                             return AUTDGainCustomSet(acc, static_cast<uint32_t>(dev.idx()), _cache->at(dev.idx()).data(),
+                                                      static_cast<uint32_t>(_cache->at(dev.idx()).size()));
                            });
   }
 
  private:
   G _g;
-  mutable std::unordered_map<size_t, std::vector<internal::native_methods::Drive>> _cache;
+  mutable std::shared_ptr<std::unordered_map<size_t, std::vector<internal::native_methods::Drive>>> _cache;
 };
 
 }  // namespace autd3::gain
