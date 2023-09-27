8d28ccf3d2b22b8d1e6a61be32c0438def6599d7
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-09-27 10:57:55 +0900

    c++20

--- cpp/examples/tests/stm.hpp
+++ cpp/examples/tests/stm.hpp
@@ -3,7 +3,7 @@
 // Created Date: 08/09/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 15/09/2023
+// Last Modified: 27/09/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -12,11 +12,9 @@
 #pragma once
 
 #include <autd3.hpp>
-
-#if __cplusplus >= 202002L
 #include <ranges>
+
 using namespace std::ranges::views;
-#endif
 
 inline void focus_stm(autd3::Controller& autd) {
   auto silencer = autd3::Silencer::disable();
@@ -27,20 +25,12 @@ inline void focus_stm(autd3::Controller& autd) {
   const autd3::Vector3 center = autd.geometry().center() + autd3::Vector3(0.0, 0.0, 150.0);
   constexpr size_t points_num = 200;
   constexpr auto radius = 30.0;
-#if __cplusplus >= 202002L
   auto stm = autd3::FocusSTM(1).add_foci_from_iter(iota(0) | take(points_num) | transform([&](auto i) {
                                                      const auto theta = 2.0 * autd3::pi * static_cast<double>(i) / static_cast<double>(points_num);
                                                      autd3::Vector3 p =
                                                          center + autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0);
                                                      return p;
                                                    }));
-#else
-  autd3::FocusSTM stm(1);
-  for (size_t i = 0; i < points_num; i++) {
-    const auto theta = 2.0 * autd3::pi * static_cast<double>(i) / static_cast<double>(points_num);
-    stm.add_focus(center + autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0));
-  }
-#endif
 
   std::cout << "Actual frequency is " << stm.frequency() << " Hz\n";
 
@@ -56,20 +46,11 @@ inline void gain_stm(autd3::Controller& autd) {
   const autd3::Vector3 center = autd.geometry().center() + autd3::Vector3(0.0, 0.0, 150.0);
   constexpr size_t points_num = 50;
   constexpr auto radius = 30.0;
-#if __cplusplus >= 202002L
   auto stm = autd3::GainSTM(1).add_gains_from_iter(iota(0) | take(points_num) | transform([&](auto i) {
                                                      const auto theta = 2.0 * autd3::pi * static_cast<double>(i) / static_cast<double>(points_num);
                                                      return autd3::gain::Focus(center +
                                                                                autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0));
                                                    }));
-#else
-  autd3::GainSTM stm(1);
-  for (size_t i = 0; i < points_num; i++) {
-    const auto theta = 2.0 * autd3::pi * static_cast<double>(i) / static_cast<double>(points_num);
-    autd3::gain::Focus g(center + autd3::Vector3(radius * std::cos(theta), radius * std::sin(theta), 0.0));
-    stm.add_gain(g);
-  }
-#endif
 
   std::cout << "Actual frequency is " << stm.frequency() << " Hz\n";
   autd.send(m, stm);
