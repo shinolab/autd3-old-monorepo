5b2935ff3324fae88722b66386028b7544af32ac
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-09-28 08:51:32 +0900

    fix glb path

--- dotnet/unity/build.ps1
+++ dotnet/unity/build.ps1
@@ -3,7 +3,7 @@
 # Created Date: 28/05/2023
 # Author: Shun Suzuki
 # -----
-# Last Modified: 24/09/2023
+# Last Modified: 28/09/2023
 # Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 # -----
 # Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -49,7 +49,7 @@ cargo build --release --features "left_handed use_meter"
 cd ../..
 Copy-Item -Path server/src-tauri/target/release/simulator.exe -Destination ./dotnet/unity/Assets/Editor/autd_simulator.exe
 mkdir ./dotnet/unity/Assets/Editor/assets > NUL 2>&1
-Copy-Item -Path server/src-tauri/target/release/assets/autd3.glb -Destination ./dotnet/unity/Assets/Editor/assets/autd3.glb
+Copy-Item -Path server/simulator/assets/autd3.glb -Destination ./dotnet/unity/Assets/Editor/assets/autd3.glb
 
 Add-Content -Path ./dotnet/unity/Assets/LICENSE.md -Value ""
 Add-Content -Path ./dotnet/unity/Assets/LICENSE.md -Value "========================================================="
