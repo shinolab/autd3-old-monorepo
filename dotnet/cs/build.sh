e801629ffadc4fa0e192050d6a321738db01422f
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-10-11 10:29:35 +0900

    refactor github actions

--- dotnet/cs/build.sh
+++ dotnet/cs/build.sh
@@ -10,6 +10,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   cargo build --release --all
   cd ..
   cp ./capi/target/release/*.so ./dotnet/cs/src/native/linux/x64
+  cp ./capi/target/release/*.so ./dotnet/cs/tests
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   cargo build --release --all --exclude autd3capi-backend-cuda --target=x86_64-apple-darwin
   cargo build --release --all --exclude autd3capi-backend-cuda --target=aarch64-apple-darwin
@@ -17,6 +18,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   for x64_file in `ls ./capi/target/x86_64-apple-darwin/release/*.dylib`; do
     file_basename=`basename $x64_file`
     lipo -create $x64_file ./capi/target/aarch64-apple-darwin/release/$file_basename -output ./dotnet/cs/src/native/osx/universal/$file_basename
+    cp ./dotnet/cs/src/native/osx/universal/$file_basename ./dotnet/cs/tests
   done
 fi
 
