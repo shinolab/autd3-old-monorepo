924b7235ec5278ca01db77176f193d8f14129ddf
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-10-18 14:44:30 +0900

    add CorrectedAmplitude

--- src/autd3-driver/src/common/mod.rs
+++ src/autd3-driver/src/common/mod.rs
@@ -4,7 +4,7 @@
  * Created Date: 14/10/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 14/10/2023
+ * Last Modified: 18/10/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -12,9 +12,11 @@
  */
 
 mod amplitude;
+mod corrected_amplitude;
 mod drive;
 mod duty_ratio;
 
 pub use amplitude::Amplitude;
+pub use corrected_amplitude::CorrectedAmplitude;
 pub use drive::Drive;
 pub use duty_ratio::DutyRatio;
