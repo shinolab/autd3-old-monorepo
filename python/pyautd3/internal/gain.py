71b6a422d3946e1d5b2d8f77c3d910d7eb01c4b5
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-10-27 09:40:17 +0900

    refactor gains

--- python/pyautd3/internal/gain.py
+++ python/pyautd3/internal/gain.py
@@ -1,4 +1,4 @@
-'''
+"""
 File: gain.py
 Project: internal
 Created Date: 29/08/2023
@@ -9,25 +9,27 @@ Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 -----
 Copyright (c) 2023 Shun Suzuki. All rights reserved.
 
-'''
+"""
 
 
 from abc import ABCMeta, abstractmethod
 
-from pyautd3.autd import Datagram
 from pyautd3.geometry import Geometry
-
 from pyautd3.native_methods.autd3capi import NativeMethods as Base
 from pyautd3.native_methods.autd3capi_def import DatagramPtr, GainPtr
 
+from .datagram import Datagram
+
+__all__ = []  # type: ignore[var-annotated]
+
 
 class IGain(Datagram, metaclass=ABCMeta):
-    def __init__(self):
+    def __init__(self: "IGain") -> None:
         super().__init__()
 
-    def _ptr(self, geometry: Geometry) -> DatagramPtr:
-        return Base().gain_into_datagram(self.gain_ptr(geometry))
+    def _ptr(self: "IGain", geometry: Geometry) -> DatagramPtr:
+        return Base().gain_into_datagram(self._gain_ptr(geometry))
 
     @abstractmethod
-    def gain_ptr(self, geometry: Geometry) -> GainPtr:
+    def _gain_ptr(self: "IGain", geometry: Geometry) -> GainPtr:
         pass
