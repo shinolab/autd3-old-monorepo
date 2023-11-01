e0a34b3c9723916ed10059cf5eed89c1208faeea
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-11-01 10:23:49 +0900

    update controller

--- firmware/src/fpga/rtl/sim_1/new/helper/sim_helper_clk.sv
+++ firmware/src/fpga/rtl/sim_1/new/helper/sim_helper_clk.sv
@@ -4,17 +4,16 @@
  * Created Date: 15/03/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 12/12/2022
+ * Last Modified: 01/11/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022 Shun Suzuki. All rights reserved.
- * 
+ *
  */
 
 
 `timescale 1ns / 1ps
 module sim_helper_clk (
-    output var CLK_163P84M,
     output var CLK_20P48M,
     output var LOCKED,
     output var [63:0] SYS_TIME
@@ -22,20 +21,18 @@ module sim_helper_clk (
 
   bit MRCC_25P6M;
 
-  bit clk_163P84M, clk_20P48M;
+  bit clk_20P48M;
   bit locked;
   bit reset;
   bit [63:0] sys_time;
 
   ultrasound_cnt_clk_gen ultrasound_cnt_clk_gen (
       .clk_in1(MRCC_25P6M),
-      .reset(reset),
-      .locked(locked),
-      .clk_out1(clk_163P84M),
-      .clk_out2(clk_20P48M)
+      .reset  (reset),
+      .locked (locked),
+      .clk_out(clk_20P48M)
   );
 
-  assign CLK_163P84M = clk_163P84M;
   assign CLK_20P48M = clk_20P48M;
   assign LOCKED = locked;
   assign SYS_TIME = sys_time;
@@ -56,7 +53,7 @@ module sim_helper_clk (
     #19.532 MRCC_25P6M = !MRCC_25P6M;
   end
 
-  always @(posedge CLK_163P84M) begin
+  always @(posedge clk_20P48M) begin
     sys_time = sys_time + 1;
   end
 
