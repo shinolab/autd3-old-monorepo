aa2bb450629e879d335e9ffd3935ee02c3b579d1
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-11-05 10:04:48 +0900

    cpu v4.0.0

--- firmware/src/cpu/src/app.c
+++ firmware/src/cpu/src/app.c
@@ -4,7 +4,7 @@
  * Created Date: 22/04/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 12/09/2023
+ * Last Modified: 03/11/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
@@ -14,27 +14,27 @@
 #include "app.h"
 
 #include "iodefine.h"
+#include "kernel.h"
 #include "params.h"
 #include "utils.h"
-#include "kernel.h"
 
-#define CPU_VERSION_MAJOR (0x8A) /* v3.0 */
-#define CPU_VERSION_MINOR (0x02)
+#define CPU_VERSION_MAJOR (0x8B) /* v4.0 */
+#define CPU_VERSION_MINOR (0x00)
 
-#define MOD_BUF_SEGMENT_SIZE_WIDTH (15)
-#define MOD_BUF_SEGMENT_SIZE (1 << MOD_BUF_SEGMENT_SIZE_WIDTH)
-#define MOD_BUF_SEGMENT_SIZE_MASK (MOD_BUF_SEGMENT_SIZE - 1)
+#define MOD_BUF_PAGE_SIZE_WIDTH (15)
+#define MOD_BUF_PAGE_SIZE (1 << MOD_BUF_PAGE_SIZE_WIDTH)
+#define MOD_BUF_PAGE_SIZE_MASK (MOD_BUF_PAGE_SIZE - 1)
 
-#define FOCUS_STM_BUF_SEGMENT_SIZE_WIDTH (11)
-#define FOCUS_STM_BUF_SEGMENT_SIZE (1 << FOCUS_STM_BUF_SEGMENT_SIZE_WIDTH)
-#define FOCUS_STM_BUF_SEGMENT_SIZE_MASK (FOCUS_STM_BUF_SEGMENT_SIZE - 1)
+#define FOCUS_STM_BUF_PAGE_SIZE_WIDTH (11)
+#define FOCUS_STM_BUF_PAGE_SIZE (1 << FOCUS_STM_BUF_PAGE_SIZE_WIDTH)
+#define FOCUS_STM_BUF_PAGE_SIZE_MASK (FOCUS_STM_BUF_PAGE_SIZE - 1)
 
-#define GAIN_STM_BUF_SEGMENT_SIZE_WIDTH (5)
-#define GAIN_STM_BUF_SEGMENT_SIZE (1 << GAIN_STM_BUF_SEGMENT_SIZE_WIDTH)
-#define GAIN_STM_BUF_SEGMENT_SIZE_MASK (GAIN_STM_BUF_SEGMENT_SIZE - 1)
-#define GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_WIDTH (6)
-#define GAIN_STM_LEGACY_BUF_SEGMENT_SIZE (1 << GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_WIDTH)
-#define GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK (GAIN_STM_LEGACY_BUF_SEGMENT_SIZE - 1)
+#define GAIN_STM_BUF_PAGE_SIZE_WIDTH (5)
+#define GAIN_STM_BUF_PAGE_SIZE (1 << GAIN_STM_BUF_PAGE_SIZE_WIDTH)
+#define GAIN_STM_BUF_PAGE_SIZE_MASK (GAIN_STM_BUF_PAGE_SIZE - 1)
+#define GAIN_STM_LEGACY_BUF_PAGE_SIZE_WIDTH (6)
+#define GAIN_STM_LEGACY_BUF_PAGE_SIZE (1 << GAIN_STM_LEGACY_BUF_PAGE_SIZE_WIDTH)
+#define GAIN_STM_LEGACY_BUF_PAGE_SIZE_MASK (GAIN_STM_LEGACY_BUF_PAGE_SIZE - 1)
 
 #define WDT_CNT_MAX (1000)
 
@@ -46,10 +46,6 @@ volatile uint8_t _write_cursor;
 volatile uint8_t _read_cursor;
 
 static volatile RX_STR _data;
-
-inline static void word_cpy(uint16_t* dst, uint16_t* src, uint32_t cnt) {
-  while (cnt-- > 0) *dst++ = *src++;
-}
 
 bool_t push(const volatile uint16_t* p_data) {
   uint32_t next;
@@ -104,8 +100,6 @@ static volatile uint8_t _ack = 0;
 static volatile uint8_t _rx_data = 0;
 static volatile bool_t _read_fpga_info;
 
-static volatile uint16_t _cycle[TRANS_NUM];
-
 static volatile uint32_t _mod_cycle = 0;
 
 static volatile uint32_t _stm_cycle = 0;
@@ -126,18 +120,13 @@ inline static uint64_t get_next_sync0() {
   return next_sync0;
 }
 
-void synchronize(const volatile uint8_t* p_data) {
-  const volatile uint16_t* cycle = (const volatile uint16_t*)p_data;
+void synchronize() {
   volatile uint64_t next_sync0;
   volatile uint16_t flag;
-
-  bram_cpy_volatile(BRAM_SELECT_CONTROLLER, BRAM_ADDR_CYCLE_BASE, cycle, TRANS_NUM);
 
   next_sync0 = get_next_sync0();
   bram_cpy_volatile(BRAM_SELECT_CONTROLLER, BRAM_ADDR_EC_SYNC_TIME_0, (volatile uint16_t*)&next_sync0, sizeof(uint64_t) >> 1);
   bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_CTL_FLAG, _fpga_flags_internal | _fpga_flags | CTL_FLAG_SYNC);
-
-  memcpy_volatile(_cycle, cycle, TRANS_NUM * sizeof(uint16_t));
 
   while (true) {
     flag = bram_read(BRAM_SELECT_CONTROLLER, BRAM_ADDR_CTL_FLAG);
@@ -145,14 +134,14 @@ void synchronize(const volatile uint8_t* p_data) {
   }
 }
 
-inline static void change_mod_segment(uint16_t segment) {
+inline static void change_mod_page(uint16_t page) {
   asm("dmb");
-  bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_MOD_MEM_SEGMENT, segment);
+  bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_MOD_MEM_PAGE, page);
   asm("dmb");
 }
 
 void write_mod(const volatile uint8_t* p_data) {
-  uint32_t segment_capacity;
+  uint32_t page_capacity;
   uint32_t freq_div;
 
   uint8_t flag = p_data[1];
@@ -162,7 +151,7 @@ void write_mod(const volatile uint8_t* p_data) {
   const uint16_t* data;
   if ((flag & MODULATION_FLAG_BEGIN) == MODULATION_FLAG_BEGIN) {
     _mod_cycle = 0;
-    change_mod_segment(0);
+    change_mod_page(0);
     freq_div = *((const uint32_t*)&p_data[4]);
     bram_cpy(BRAM_SELECT_CONTROLLER, BRAM_ADDR_MOD_FREQ_DIV_0, (uint16_t*)&freq_div, sizeof(uint32_t) >> 1);
     data = (const uint16_t*)(&p_data[8]);
@@ -170,17 +159,17 @@ void write_mod(const volatile uint8_t* p_data) {
     data = (const uint16_t*)(&p_data[4]);
   }
 
-  segment_capacity = (_mod_cycle & ~MOD_BUF_SEGMENT_SIZE_MASK) + MOD_BUF_SEGMENT_SIZE - _mod_cycle;
-  if (write <= segment_capacity) {
-    bram_cpy(BRAM_SELECT_MOD, (_mod_cycle & MOD_BUF_SEGMENT_SIZE_MASK) >> 1, data, (write + 1) >> 1);
+  page_capacity = (_mod_cycle & ~MOD_BUF_PAGE_SIZE_MASK) + MOD_BUF_PAGE_SIZE - _mod_cycle;
+  if (write <= page_capacity) {
+    bram_cpy(BRAM_SELECT_MOD, (_mod_cycle & MOD_BUF_PAGE_SIZE_MASK) >> 1, data, (write + 1) >> 1);
     _mod_cycle += write;
   } else {
-    bram_cpy(BRAM_SELECT_MOD, (_mod_cycle & MOD_BUF_SEGMENT_SIZE_MASK) >> 1, data, segment_capacity >> 1);
-    _mod_cycle += segment_capacity;
-    data += segment_capacity;
-    change_mod_segment((_mod_cycle & ~MOD_BUF_SEGMENT_SIZE_MASK) >> MOD_BUF_SEGMENT_SIZE_WIDTH);
-    bram_cpy(BRAM_SELECT_MOD, (_mod_cycle & MOD_BUF_SEGMENT_SIZE_MASK) >> 1, data, (write - segment_capacity + 1) >> 1);
-    _mod_cycle += write - segment_capacity;
+    bram_cpy(BRAM_SELECT_MOD, (_mod_cycle & MOD_BUF_PAGE_SIZE_MASK) >> 1, data, page_capacity >> 1);
+    _mod_cycle += page_capacity;
+    data += page_capacity;
+    change_mod_page((_mod_cycle & ~MOD_BUF_PAGE_SIZE_MASK) >> MOD_BUF_PAGE_SIZE_WIDTH);
+    bram_cpy(BRAM_SELECT_MOD, (_mod_cycle & MOD_BUF_PAGE_SIZE_MASK) >> 1, data, (write - page_capacity + 1) >> 1);
+    _mod_cycle += write - page_capacity;
   }
 
   if ((flag & MODULATION_FLAG_END) == MODULATION_FLAG_END) {
@@ -218,39 +207,19 @@ static void write_filter(const volatile uint8_t* p_data) {
 }
 
 static void write_gain(const volatile uint8_t* p_data) {
-  uint8_t flag = p_data[1];
-  const uint16_t* src = (const uint16_t*)(&p_data[2]);
+  volatile const uint16_t* src = (volatile const uint16_t*)(&p_data[2]);
   volatile uint16_t* base = (volatile uint16_t*)FPGA_BASE;
   uint16_t addr = get_addr(BRAM_SELECT_NORMAL, 0);
   uint32_t cnt = TRANS_NUM;
   volatile uint16_t* dst = &base[addr];
 
   _fpga_flags_internal &= ~CTL_FLAG_OP_MODE;
-  if ((flag & GAIN_FLAG_LEGACY) == GAIN_FLAG_LEGACY) {
-    while (cnt--) {
-      *dst = *src++;
-      dst += 2;
-    }
-    _fpga_flags_internal |= CTL_FLAG_LEGACY_MODE;
-  } else if ((flag & GAIN_FLAG_DUTY) == GAIN_FLAG_DUTY) {
-    dst++;
-    while (cnt--) {
-      *dst = *src++;
-      dst += 2;
-    }
-    _fpga_flags_internal &= ~CTL_FLAG_LEGACY_MODE;
-  } else {
-    while (cnt--) {
-      *dst = *src++;
-      dst += 2;
-    }
-    _fpga_flags_internal &= ~CTL_FLAG_LEGACY_MODE;
-  }
+  word_cpy_volatile(dst, src, cnt);
 }
 
-inline static void change_stm_segment(uint16_t segment) {
+inline static void change_stm_page(uint16_t page) {
   asm("dmb");
-  bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_STM_MEM_SEGMENT, segment);
+  bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_STM_MEM_PAGE, page);
   asm("dmb");
 }
 
@@ -267,12 +236,12 @@ static void write_focus_stm(const volatile uint8_t* p_data) {
   uint16_t start_idx;
   uint16_t finish_idx;
   uint32_t cnt;
-  uint32_t segment_capacity;
+  uint32_t page_capacity;
 
   size = *((const uint16_t*)&p_data[2]);
   if ((flag & FOCUS_STM_FLAG_BEGIN) == FOCUS_STM_FLAG_BEGIN) {
     _stm_cycle = 0;
-    change_stm_segment(0);
+    change_stm_page(0);
 
     freq_div = *((const uint32_t*)&p_data[4]);
     sound_speed = *((const uint32_t*)&p_data[8]);
@@ -300,10 +269,10 @@ static void write_focus_stm(const volatile uint8_t* p_data) {
     src = (const uint16_t*)(&p_data[4]);
   }
 
-  segment_capacity = (_stm_cycle & ~FOCUS_STM_BUF_SEGMENT_SIZE_MASK) + FOCUS_STM_BUF_SEGMENT_SIZE - _stm_cycle;
-  if (size <= segment_capacity) {
+  page_capacity = (_stm_cycle & ~FOCUS_STM_BUF_PAGE_SIZE_MASK) + FOCUS_STM_BUF_PAGE_SIZE - _stm_cycle;
+  if (size <= page_capacity) {
     cnt = size;
-    addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & FOCUS_STM_BUF_SEGMENT_SIZE_MASK) << 3);
+    addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & FOCUS_STM_BUF_PAGE_SIZE_MASK) << 3);
     dst = &base[addr];
     while (cnt--) {
       *dst++ = *src++;
@@ -314,8 +283,8 @@ static void write_focus_stm(const volatile uint8_t* p_data) {
     }
     _stm_cycle += size;
   } else {
-    cnt = segment_capacity;
-    addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & FOCUS_STM_BUF_SEGMENT_SIZE_MASK) << 3);
+    cnt = page_capacity;
+    addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & FOCUS_STM_BUF_PAGE_SIZE_MASK) << 3);
     dst = &base[addr];
     while (cnt--) {
       *dst++ = *src++;
@@ -324,12 +293,12 @@ static void write_focus_stm(const volatile uint8_t* p_data) {
       *dst++ = *src++;
       dst += 4;
     }
-    _stm_cycle += segment_capacity;
+    _stm_cycle += page_capacity;
 
-    change_stm_segment((_stm_cycle & ~FOCUS_STM_BUF_SEGMENT_SIZE_MASK) >> FOCUS_STM_BUF_SEGMENT_SIZE_WIDTH);
+    change_stm_page((_stm_cycle & ~FOCUS_STM_BUF_PAGE_SIZE_MASK) >> FOCUS_STM_BUF_PAGE_SIZE_WIDTH);
 
-    cnt = size - segment_capacity;
-    addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & FOCUS_STM_BUF_SEGMENT_SIZE_MASK) << 3);
+    cnt = size - page_capacity;
+    addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & FOCUS_STM_BUF_PAGE_SIZE_MASK) << 3);
     dst = &base[addr];
     while (cnt--) {
       *dst++ = *src++;
@@ -338,7 +307,7 @@ static void write_focus_stm(const volatile uint8_t* p_data) {
       *dst++ = *src++;
       dst += 4;
     }
-    _stm_cycle += size - segment_capacity;
+    _stm_cycle += size - page_capacity;
   }
 
   if ((flag & FOCUS_STM_FLAG_END) == FOCUS_STM_FLAG_END) {
@@ -348,7 +317,7 @@ static void write_focus_stm(const volatile uint8_t* p_data) {
   }
 }
 
-static void write_gain_stm_legacy(const volatile uint8_t* p_data) {
+static void write_gain_stm(const volatile uint8_t* p_data) {
   volatile uint16_t* base = (volatile uint16_t*)FPGA_BASE;
 
   uint8_t flag = p_data[1];
@@ -365,7 +334,7 @@ static void write_gain_stm_legacy(const volatile uint8_t* p_data) {
 
   if ((flag & GAIN_STM_FLAG_BEGIN) == GAIN_STM_FLAG_BEGIN) {
     _stm_cycle = 0;
-    change_stm_segment(0);
+    change_stm_page(0);
 
     _gain_stm_mode = *((const uint16_t*)&p_data[2]);
 
@@ -394,7 +363,7 @@ static void write_gain_stm_legacy(const volatile uint8_t* p_data) {
   }
 
   src = src_base;
-  addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK) << 8);
+  addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_PAGE_SIZE_MASK) << 8);
 
   switch (_gain_stm_mode) {
     case GAIN_STM_MODE_DUTY_PHASE_FULL:
@@ -411,7 +380,7 @@ static void write_gain_stm_legacy(const volatile uint8_t* p_data) {
 
       if (send > 1) {
         src = src_base;
-        addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK) << 8);
+        addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_PAGE_SIZE_MASK) << 8);
         dst = &base[addr];
         cnt = TRANS_NUM;
         while (cnt--) *dst++ = 0xFF00 | (((*src++) >> 8) & 0x00FF);
@@ -429,7 +398,7 @@ static void write_gain_stm_legacy(const volatile uint8_t* p_data) {
 
       if (send > 1) {
         src = src_base;
-        addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK) << 8);
+        addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_PAGE_SIZE_MASK) << 8);
         dst = &base[addr];
         cnt = TRANS_NUM;
         while (cnt--) {
@@ -441,7 +410,7 @@ static void write_gain_stm_legacy(const volatile uint8_t* p_data) {
 
       if (send > 2) {
         src = src_base;
-        addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK) << 8);
+        addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_PAGE_SIZE_MASK) << 8);
         dst = &base[addr];
         cnt = TRANS_NUM;
         while (cnt--) {
@@ -453,7 +422,7 @@ static void write_gain_stm_legacy(const volatile uint8_t* p_data) {
 
       if (send > 3) {
         src = src_base;
-        addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK) << 8);
+        addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_LEGACY_BUF_PAGE_SIZE_MASK) << 8);
         dst = &base[addr];
         cnt = TRANS_NUM;
         while (cnt--) {
@@ -467,115 +436,18 @@ static void write_gain_stm_legacy(const volatile uint8_t* p_data) {
       break;
   }
 
-  if ((_stm_cycle & GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK) == 0)
-    change_stm_segment((_stm_cycle & ~GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK) >> GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_WIDTH);
+  if ((_stm_cycle & GAIN_STM_LEGACY_BUF_PAGE_SIZE_MASK) == 0)
+    change_stm_page((_stm_cycle & ~GAIN_STM_LEGACY_BUF_PAGE_SIZE_MASK) >> GAIN_STM_LEGACY_BUF_PAGE_SIZE_WIDTH);
 
   if ((flag & GAIN_STM_FLAG_END) == GAIN_STM_FLAG_END) {
-    _fpga_flags_internal |= CTL_FLAG_LEGACY_MODE;
     _fpga_flags_internal |= CTL_FLAG_OP_MODE;
     _fpga_flags_internal |= CTL_FLAG_STM_GAIN_MODE;
     bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_STM_CYCLE, max(1, _stm_cycle) - 1);
-  }
-}
-
-static void write_gain_stm_advanced(const volatile uint8_t* p_data) {
-  volatile uint16_t* base = (volatile uint16_t*)FPGA_BASE;
-
-  uint8_t flag = p_data[1];
-
-  uint16_t addr;
-  volatile uint16_t* dst;
-  const volatile uint16_t *src, *src_base;
-  uint32_t freq_div;
-  uint16_t start_idx;
-  uint16_t finish_idx;
-  uint32_t cnt;
-
-  if ((flag & GAIN_STM_FLAG_BEGIN) == GAIN_STM_FLAG_BEGIN) {
-    _stm_cycle = 0;
-    change_stm_segment(0);
-
-    _gain_stm_mode = *((const uint16_t*)&p_data[2]);
-
-    freq_div = *((const uint32_t*)&p_data[4]);
-    bram_cpy(BRAM_SELECT_CONTROLLER, BRAM_ADDR_STM_FREQ_DIV_0, (uint16_t*)&freq_div, sizeof(uint32_t) >> 1);
-
-    start_idx = *((const uint16_t*)&p_data[8]);
-    finish_idx = *((const uint16_t*)&p_data[10]);
-    bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_STM_START_IDX, start_idx);
-    bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_STM_FINISH_IDX, finish_idx);
-
-    if ((flag & GAIN_STM_FLAG_USE_START_IDX) == GAIN_STM_FLAG_USE_START_IDX) {
-      _fpga_flags_internal |= CTL_FLAG_USE_STM_START_IDX;
-    } else {
-      _fpga_flags_internal &= ~CTL_FLAG_USE_STM_START_IDX;
-    }
-    if ((flag & GAIN_STM_FLAG_USE_FINISH_IDX) == GAIN_STM_FLAG_USE_FINISH_IDX) {
-      _fpga_flags_internal |= CTL_FLAG_USE_STM_FINISH_IDX;
-    } else {
-      _fpga_flags_internal &= ~CTL_FLAG_USE_STM_FINISH_IDX;
-    }
-
-    src_base = (const uint16_t*)(&p_data[12]);
-  } else {
-    src_base = (const uint16_t*)(&p_data[2]);
-  }
-
-  src = src_base;
-  addr = get_addr(BRAM_SELECT_STM, (_stm_cycle & GAIN_STM_BUF_SEGMENT_SIZE_MASK) << 9);
-
-  switch (_gain_stm_mode) {
-    case GAIN_STM_MODE_DUTY_PHASE_FULL:
-      if ((flag & GAIN_STM_FLAG_DUTY) == GAIN_STM_FLAG_DUTY) {
-        dst = &base[addr] + 1;
-        _stm_cycle += 1;
-      } else {
-        dst = &base[addr];
-      }
-      cnt = TRANS_NUM;
-      while (cnt--) {
-        *dst = *src++;
-        dst += 2;
-      }
-      break;
-    case GAIN_STM_MODE_PHASE_FULL:
-      dst = &base[addr];
-      cnt = 0;
-      while (cnt++ < TRANS_NUM) {
-        *dst++ = *src++;
-        *dst++ = _cycle[cnt] >> 1;
-      }
-      _stm_cycle += 1;
-      break;
-    case GAIN_STM_MODE_PHASE_HALF:
-      // Not supported in advanced mode
-      break;
-    default:
-      break;
-  }
-
-  if ((_stm_cycle & GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK) == 0)
-    change_stm_segment((_stm_cycle & ~GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_MASK) >> GAIN_STM_LEGACY_BUF_SEGMENT_SIZE_WIDTH);
-
-  if ((flag & GAIN_STM_FLAG_END) == GAIN_STM_FLAG_END) {
-    _fpga_flags_internal &= ~CTL_FLAG_LEGACY_MODE;
-    _fpga_flags_internal |= CTL_FLAG_OP_MODE;
-    _fpga_flags_internal |= CTL_FLAG_STM_GAIN_MODE;
-    bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_STM_CYCLE, max(1, _stm_cycle) - 1);
-  }
-}
-
-static void write_gain_stm(const volatile uint8_t* p_data) {
-  uint8_t flag = p_data[1];
-  if ((flag & GAIN_STM_FLAG_LEGACY) == GAIN_STM_FLAG_LEGACY) {
-    write_gain_stm_legacy(p_data);
-  } else {
-    write_gain_stm_advanced(p_data);
   }
 }
 
 static void clear(void) {
-  uint32_t freq_div_4k = 40960;
+  uint32_t freq_div_4k = 5120;
 
   _read_fpga_info = false;
 
@@ -590,7 +462,7 @@ static void clear(void) {
   _mod_cycle = 2;
   bram_write(BRAM_SELECT_CONTROLLER, BRAM_ADDR_MOD_CYCLE, max(1, _mod_cycle) - 1);
   bram_cpy(BRAM_SELECT_CONTROLLER, BRAM_ADDR_MOD_FREQ_DIV_0, (uint16_t*)&freq_div_4k, sizeof(uint32_t) >> 1);
-  change_mod_segment(0);
+  change_mod_page(0);
   bram_write(BRAM_SELECT_MOD, 0, 0x0000);
 
   bram_set(BRAM_SELECT_NORMAL, 0, 0x0000, TRANS_NUM << 1);
@@ -616,7 +488,7 @@ void handle_payload(uint8_t tag, const volatile uint8_t* p_data) {
       clear();
       break;
     case TAG_SYNC:
-      synchronize(p_data + 2);
+      synchronize();
       break;
     case TAG_FIRM_INFO:
       switch (p_data[1]) {
