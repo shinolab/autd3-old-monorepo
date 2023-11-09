5ba9ca4c0fad2afc69d0649a37ec0f623416fdff
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-11-09 15:02:14 +0900

    fix python flag example

--- python/example/samples/flag.py
+++ python/example/samples/flag.py
@@ -12,6 +12,7 @@ Copyright (c) 2023 Shun Suzuki. All rights reserved.
 """
 
 
+import asyncio
 import threading
 
 from pyautd3 import Controller, UpdateFlags
@@ -29,23 +30,25 @@ async def flag(autd: Controller) -> None:
 
     fin = False
 
-    async def f() -> None:
-        prompts = ["-", "/", "|", "\\"]
-        prompts_idx = 0
-        while not fin:
-            states = await autd.fpga_info()
-            print(f"{prompts[(prompts_idx // 1000) % len(prompts)]} FPGA Status...")
-            print("\n".join([str(state) for state in states]))
-            print(f"\x1b[{len(states) + 1}A", end="")
-            prompts_idx += 1
+    def f() -> None:
+        nonlocal fin
+        print("press any key stop checking FPGA status...")
+        _ = input()
+        fin = True
 
     th = threading.Thread(target=f)
     th.start()
 
-    print("press any key stop checking FPGA status...")
-    _ = input()
+    prompts = ["-", "/", "|", "\\"]
+    prompts_idx = 0
+    while not fin:
+        states = await autd.fpga_info()
+        print(f"{prompts[(prompts_idx // 1000) % len(prompts)]} FPGA Status...")
+        print("\n".join([f"\x1b[0K[{i}]: thermo = {state.is_thermal_assert()}" for i, state in enumerate(states)]))
+        print(f"\x1b[{len(states) + 1}A", end="")
+        prompts_idx += 1
+    print("\x1b[1F\x1b[0J")
 
-    fin = True
     th.join()
 
     for dev in autd.geometry:
