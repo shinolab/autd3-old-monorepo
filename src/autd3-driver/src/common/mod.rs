d3f3cde0983611b3193b6548cf9ee5bbfb25f090
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-11-22 10:56:06 +0900

    update firmware-emulator

--- src/autd3-driver/src/common/mod.rs
+++ src/autd3-driver/src/common/mod.rs
@@ -4,7 +4,7 @@
  * Created Date: 14/10/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 21/11/2023
+ * Last Modified: 22/11/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -13,7 +13,6 @@
 
 mod drive;
 mod emit_intensity;
-pub mod pulse_width;
 mod sampling_config;
 
 pub use drive::Drive;
--- src/autd3-firmware-emulator/src/fpga/params.rs
+++ src/autd3-firmware-emulator/src/fpga/params.rs
@@ -4,19 +4,15 @@
  * Created Date: 07/05/2022
  * Author: Shun Suzuki
  * -----
- * Last Modified: 06/11/2023
+ * Last Modified: 22/11/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2022-2023 Shun Suzuki. All rights reserved.
  *
  */
 
-use autd3_driver::defined::{float, METER};
-
 pub const VERSION_NUM_MAJOR: u8 = 0x8B;
 pub const VERSION_NUM_MINOR: u8 = 0x00;
-
-pub const TRANS_SIZE_FIXED_POINT_UNIT: float = 40000. / METER;
 
 pub const BRAM_SELECT_CONTROLLER: u16 = 0x0;
 pub const BRAM_SELECT_MOD: u16 = 0x1;
@@ -37,7 +33,8 @@ pub const ADDR_MOD_FREQ_DIV_0: usize = 0x0022;
 pub const ADDR_MOD_FREQ_DIV_1: usize = 0x0023;
 pub const ADDR_VERSION_NUM: usize = 0x0030;
 pub const ADDR_VERSION_NUM_MINOR: usize = 0x0031;
-pub const ADDR_SILENT_STEP: usize = 0x0040;
+pub const ADDR_SILENT_STEP_INTENSITY: usize = 0x0040;
+pub const ADDR_SILENT_STEP_PHASE: usize = 0x0041;
 pub const ADDR_STM_ADDR_OFFSET: usize = 0x0050;
 pub const ADDR_STM_CYCLE: usize = 0x0051;
 pub const ADDR_STM_FREQ_DIV_0: usize = 0x0052;
@@ -47,8 +44,6 @@ pub const ADDR_SOUND_SPEED_1: usize = 0x0055;
 pub const ADDR_STM_START_IDX: usize = 0x0056;
 pub const ADDR_STM_FINISH_IDX: usize = 0x0057;
 pub const ADDR_MOD_DELAY_BASE: usize = 0x0200;
-pub const ADDR_FILTER_DUTY_BASE: usize = 0x0300;
-pub const ADDR_FILTER_PHASE_BASE: usize = 0x0400;
 
 pub const CTL_REG_FORCE_FAN_BIT: u16 = 0;
 pub const CTL_REG_OP_MODE_BIT: u16 = 9;
@@ -56,15 +51,5 @@ pub const CTL_REG_STM_GAIN_MODE_BIT: u16 = 10;
 pub const CTL_FLAG_USE_STM_FINISH_IDX_BIT: u16 = 11;
 pub const CTL_FLAG_USE_STM_START_IDX_BIT: u16 = 12;
 
-pub const ENABLED_STM_BIT: u8 = 0x01;
-pub const ENABLED_MODULATOR_BIT: u8 = 0x02;
-pub const ENABLED_SILENCER_BIT: u8 = 0x04;
-pub const ENABLED_MOD_DELAY_BIT: u8 = 0x08;
-pub const ENABLED_FILTER_BIT: u8 = 0x10;
 pub const ENABLED_EMULATOR_BIT: u8 = 0x80;
-pub const ENABLED_FEATURES_BITS: u8 = ENABLED_MOD_DELAY_BIT
-    | ENABLED_STM_BIT
-    | ENABLED_MODULATOR_BIT
-    | ENABLED_SILENCER_BIT
-    | ENABLED_FILTER_BIT
-    | ENABLED_EMULATOR_BIT;
+pub const ENABLED_FEATURES_BITS: u8 = ENABLED_EMULATOR_BIT;
