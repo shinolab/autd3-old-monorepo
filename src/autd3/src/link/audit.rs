8ecb71c1cd6906049acf11d1d367c8288121d09a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-11-27 15:40:02 +0900

    fix features

--- src/autd3/src/link/audit.rs
+++ src/autd3/src/link/audit.rs
@@ -4,7 +4,7 @@
  * Created Date: 14/09/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 09/11/2023
+ * Last Modified: 27/11/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -46,7 +46,6 @@ impl AuditBuilder {
     }
 }
 
-// #[async_trait::async_trait]
 impl LinkSyncBuilder for AuditBuilder {
     type L = Audit;
 
