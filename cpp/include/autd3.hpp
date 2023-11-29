d2e4e9ee4759e11e28efcb7f320c762345776ae6
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-11-29 17:35:34 +0900

    fix #227

--- cpp/include/autd3.hpp
+++ cpp/include/autd3.hpp
@@ -3,7 +3,7 @@
 // Created Date: 29/05/2023
 // Author: Shun Suzuki
 // -----
-// Last Modified: 26/11/2023
+// Last Modified: 29/11/2023
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -73,6 +73,7 @@ using internal::SamplingConfiguration;
 using modulation::Modulation;
 
 using internal::Clear;
+using internal::ConfigureDebugOutoutIdx;
 using internal::ConfigureModDelay;
 using internal::Silencer;
 using internal::Synchronize;
