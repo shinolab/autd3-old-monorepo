02b5761a3e294e69adf47a142abd087eb00f2f3c
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-11-29 15:45:56 +0900

    v17.0.3

--- cpp/include/autd3.hpp
+++ cpp/include/autd3.hpp
@@ -54,7 +54,7 @@ constexpr double pi = internal::pi;
 /**
  * @brief AUTD3 software version
  */
-static inline std::string version = "17.0.2";
+static inline std::string version = "17.0.3";
 
 using internal::deg;
 using internal::Device;
