e1616378abcf69547d3f1a521398705eeb9076b5
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-12-06 15:59:57 +0900

    fix python examples

--- python/example/remote_soem.py
+++ python/example/remote_soem.py
@@ -14,7 +14,7 @@ Copyright (c) 2023 Shun Suzuki. All rights reserved.
 
 import asyncio
 
-from samples import runner
+from samples import runner  # type: ignore[import-not-found]
 
 from pyautd3 import AUTD3, Controller
 from pyautd3.link.soem import RemoteSOEM
